7b2f709e7cd7f978a0759e4b319ca7540e86abe9
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-21 23:42:01 -0500

    Create Strings.js

---
+++ Expressions/Strings.js
@@ -0,0 +1,8 @@
+console.log("A1")
+console.log("ABC111222333")
+console.log("That's what the lines are for.")
+console.log('Useful for quoting, "Cory"')
+
+console.log("Cat" + "Dog")
+console.log("cat".toUpperCase())
+console.log("DOG".toLowerCase())
