27901028dc963b9c7bb3a4245a7c1b94e753d0ec
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-22 00:18:01 -0500

    Update Strings.js

--- Expressions/Strings.js
+++ Expressions/Strings.js
@@ -1,8 +1,10 @@
-console.log("A1")
-console.log("ABC111222333")
-console.log("That's what the lines are for.")
-console.log('Useful for quoting, "Cory"')
+console.log("A1");
+console.log("ABC111222333");
+console.log("That's what the lines are for.");
+console.log('Useful for quoting, "Cory"');
 
-console.log("Cat" + "Dog")
-console.log("cat".toUpperCase())
-console.log("DOG".toLowerCase())
+console.log("Cat" + "Dog");
+console.log("cat".toUpperCase());
+console.log("DOG".toLowerCase());
+
+console.log('apple' === 'apple');
