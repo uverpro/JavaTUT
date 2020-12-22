1873a70f10fd1f8a5ac07521bf43f7b68d84d422
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-22 01:44:19 -0500

    Create Variables.js

---
+++ Variables/Variables.js
@@ -0,0 +1,12 @@
+// Variables allow you to assign a value
+
+let color = "green"; // let keyword. "=" assighnemt operator.
+console.log(color); // green
+console.log(color + "!"); // green!
+
+let number = 85;
+console.log(number); // 85
+console.log(number + 1); // 86
+console.log(number); // 85
+number = number + 1;
+console.log(number); // 86
