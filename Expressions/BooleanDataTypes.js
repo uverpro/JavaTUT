5c1c647e3187c8ca75ea073b4b0f25682d743b7d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-22 00:37:01 -0500

    Update BooleanDataTypes.js

--- Expressions/BooleanDataTypes.js
+++ Expressions/BooleanDataTypes.js
@@ -34,13 +34,13 @@ console.log(43 <= 42); // false
 console.log(43 >= 43); // true
 console.log(43 <= 43); // true
 
-console.log('apple' === 'apple') // true
-console.log('apple' === 'Apple') // false
+console.log('apple' === 'apple'); // true
+console.log('apple' === 'Apple'); // false
 
 // > or < in Strings measure the order of characters in alphabetical order
-console.log('cat' < 'dog') // true
-console.log('cat' < 'cats') // true
-console.log('cat' < 'apple') // false
+console.log('cat' < 'dog'); // true
+console.log('cat' < 'cats'); // true
+console.log('cat' < 'apple'); // false
 
 // === "strict" equality
 // == "loose" equality
