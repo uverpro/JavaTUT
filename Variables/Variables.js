7f4ed4c6d113f459c4270ae88b2f1de905e64037
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-22 01:55:05 -0500

    Update Variables.js

--- Variables/Variables.js
+++ Variables/Variables.js
@@ -8,5 +8,31 @@ let number = 85;
 console.log(number); // 85
 console.log(number + 1); // 86
 console.log(number); // 85
+// +=, -=, *=, /=
 number = number + 1;
 console.log(number); // 86
+number += 1;
+console.log(number); // 87
+number += 12;
+console.log(number); // 99
+number -= 9;
+console.log(number); // 90
+number *= 10;
+console.log(number); // 900
+number /= 9;
+console.log(number); // 100
+
+let i = 0;
+console.log(i);
+i++; // i += 1
+console.log(i);
+
+let x = 0;
+console.log(x); // 0
+x--;
+console.log(x); // -1
+
+let y;
+console.log(y); // undefined
+
+// undefined is the default value in Javascript
