5f3d8fa40435cbf58edcf2f1a0a31fc5f57f0447
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-22 01:25:38 -0500

    Update String-Expressions.js

--- Expressions/Expressions-Ex/String-Expressions.js
+++ Expressions/Expressions-Ex/String-Expressions.js
@@ -1,4 +1,4 @@
-console.log("River" + " " + "Town"); // River Town
+console.log("River " + "Town"); // River Town
 console.log("cat" + "dog"); // catdog
 console.log("New" + " " + "York"); // New York
 console.log("runner's knee" + "!"); // runner's knee!
