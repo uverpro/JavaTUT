5c520842d4ae463f6e2384e50a789ee43e4dc998
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-22 00:55:10 -0500

    Update Number-Expressions.js

--- Expressions/Expressions-Ex/Number-Expressions.js
+++ Expressions/Expressions-Ex/Number-Expressions.js
@@ -1,12 +1,12 @@
-console.log(2 + 3);
-console.log()
-console.log()
-console.log()
+console.log(2 + 3); // 5
+console.log(10 - 15); // -5
+console.log(4 + 1 - 5); // 0
+console.log(4 * 3); // 12
 
-console.log()
-console.log()
-console.log()
-console.log()
+console.log(7 / 2); // 3.5
+console.log(4 + 2 * 3) // 10
+console.log((4 + 2) * 3) // 18
+console.log(5 % 2); // 1
 
 console.log()
 console.log()
