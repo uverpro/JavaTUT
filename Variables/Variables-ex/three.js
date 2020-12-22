42ed9703b4cf911b9547dd9d86440e862b382932
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-22 02:30:24 -0500

    Create three.js

---
+++ Variables/Variables-ex/three.js
@@ -0,0 +1,9 @@
+let qty = 15 % 4;
+console.log(qty); // 3
+
+let num = 38 + 3;
+num++;
+console.log(num + " is a great number"); // 42 is a great number
+
+let isNumEven = num % 2 === 0;
+console.log(num + " is even? " + isNumEven) // 42 is even? true
