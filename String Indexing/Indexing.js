4ff498a32f55b529e99acbf9dd8cf91845c2bdff
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-22 19:06:03 -0500

    Update Indexing.js

--- String Indexing/Indexing.js
+++ String Indexing/Indexing.js
@@ -17,4 +17,14 @@ console.log(str[5]); // s
 console.log(str[6]); // undefined
 console.log(str[-1]); // undefined
 console.log([str.length - 1]); // [ 5 ]
-console.log(str[str.length - 1]) // s
+console.log(str[str.length - 1]); // s
+
+// ".length -1" will always return the last character in a String.
+let String = "Evenflow"
+console.log(String[String.length - 1]); // w
+console.log(String[String.length]) // undefined
+// The "length" of "Evenflow" is 8. The "indecies" is 0, 1, 2, 3, 4, 5, 6, 7
+// (8 - 1) = [7] = w
+
+let strng = 'cats'
+str.indexOf('a')
