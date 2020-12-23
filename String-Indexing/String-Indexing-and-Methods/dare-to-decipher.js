81ecc2afd0b72a3f5078e8cb6889ccdf736e6640
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-22 23:17:11 -0500

    Update dare-to-decipher.js

--- String-Indexing/String-Indexing-and-Methods/dare-to-decipher.js
+++ String-Indexing/String-Indexing-and-Methods/dare-to-decipher.js
@@ -10,7 +10,7 @@ console.log(phrase.indexOf(char)); // 8
 console.log(phrase.slice(2, 8)); // at's a
 
 console.log("abcdefg".slice(1, 3)); // bc
-console.log("abcdefg".slice(2)); // c
-console.log("abcdefg".slice(4)); // e
+console.log("abcdefg".slice(2)); // cdefg
+console.log("abcdefg".slice(4)); // efg
 console.log("abcdefg".slice(2, -1)); // cdef
 console.log("abcdefg".slice(2, -2)); // cde
