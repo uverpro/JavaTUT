4fcb100d94e1a0b6ce946917043ddb9919f7eace
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-23 22:22:55 -0500

    Update zero.js

--- Conditionals/Conditional-Exercises/zero.js
+++ Conditionals/Conditional-Exercises/zero.js
@@ -1,8 +1,23 @@
-// Snippet 0-1
-// if (false) {
-//     console.log("foo");
-// }
+//Snippet 0-1
+if (false) {
+    console.log("foo");
+}
 
-// if (true) {
-//     console.log("bar"); // will return true because a Conditional is by default true because JAVA
-// }
+if (true) {
+    console.log("bar"); // will return true because a Conditional is by default true because JAVA
+}
+
+
+
+//Snippet 0-2
+if (false || false) {
+    console.log("boop")
+}
+
+if (true || false) {
+    console.log("beep") // see line 7
+}
+
+
+
+//Snippet 0-3
