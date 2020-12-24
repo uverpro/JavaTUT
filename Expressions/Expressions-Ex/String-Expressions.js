6c60e0166a1fdf2b7059279bdd975c718d9f4464
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-23 22:19:09 -0500

    Delete String-Expressions.js

--- Expressions/Expressions-Ex/String-Expressions.js
+++
@@ -1,5 +0,0 @@
-console.log("River " + "Town"); // River Town
-console.log("cat" + "dog"); // catdog
-console.log("New" + " " + "York"); // New York
-console.log("runner's knee" + "!"); // runner's knee!
-console.log("man" + "bear" + "pig"); // manbearpig
