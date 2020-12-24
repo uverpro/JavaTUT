01f5a0615f9a118ef46125dccb628a01b60ad375
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-23 23:21:34 -0500

    Update two.js

---
+++ Conditionals/Conditional-Exercises/two.js
@@ -0,0 +1,8 @@
+//Snippet 2-1
+let nonsense = "blog trust fund tattooed williamsburg poke roof party";
+// console.log(nonsense.indexOf("ok")) // 39
+let hasOk = nonsense.indexOf("ok") > -1;
+
+if (hasOk) {
+    console.log("yeet");
+}
