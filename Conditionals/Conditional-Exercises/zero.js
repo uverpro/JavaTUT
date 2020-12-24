7200ae75ca2f9ac1c925cd57eb0607f0c39fb6dd
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-23 22:18:50 -0500

    Create zero.js

---
+++ Conditionals/Conditional-Exercises/zero.js
@@ -0,0 +1,8 @@
+// Snippet 0-1
+// if (false) {
+//     console.log("foo");
+// }
+
+// if (true) {
+//     console.log("bar"); // will return true because a Conditional is by default true because JAVA
+// }
