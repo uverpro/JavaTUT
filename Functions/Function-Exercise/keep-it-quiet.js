fe2a5e2c6cfb1841d16e1c33119066c4f8630bc7
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-24 21:01:36 -0500

    Update keep-it-quiet.js

--- Functions/Function-Exercise/keep-it-quiet.js
+++ Functions/Function-Exercise/keep-it-quiet.js
@@ -1,3 +1,6 @@
+// Write a function `keepItQuiet` that accepts a string as an argument. The function should return the
+// lowercase version of the string with 3 periods added to the end of it.
+
 let keepItQuiet = function(str) { // Defined the function and used "str" as a parameter for a string argument
     let shh = str.toLowerCase(); // will return a lower case version of the string with 3 periods added to the end of it
     return shh + "...";
