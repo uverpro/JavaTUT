bc72ceb41a09d285bd234ec25284130f64c72948
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-24 21:01:33 -0500

    Update is-div-by-4.js

--- Functions/Function-Exercise/is-div-by-4.js
+++ Functions/Function-Exercise/is-div-by-4.js
@@ -1,3 +1,6 @@
+// Write a function `isDivBy4` that accepts a number as an argument. The function should return a
+// boolean indicating whether or not the number is divisible by 4.
+
 let isDivBy4 = function(num) { // the "if" & "else" Statements are in the first brackets
     if (num % 4 === 0) { // every Return Function has its own curly brackets
     return true;
