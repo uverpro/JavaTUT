21b10dc18f6465a5fc7a977201c1681db634b6ed
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-25 18:52:59 -0500

    Update wacky-word.js

---
+++ Functions/B_Function-Exercise/wacky-word.js
@@ -0,0 +1,14 @@
+// Write a function `wackyWord` that accepts two strings as arguments. The function should return a new
+// string containing the first three characters of the first string concatenated with the last two
+// character of the second string.
+
+let wackyWord = function (str1, str2) {
+    return str1.slice(0, 3) + str2.slice(-2);
+    // return str1.slice(0, 3) + (str2[str2.length - 2] + str2[str2.length - 1]); // first lol
+
+};
+
+console.log(wackyWord("very", "kindly")); // verly
+console.log(wackyWord("forever", "sick")); // forck
+console.log(wackyWord("cellar", "door")); // celor
+console.log(wackyWord("bagel", "sweep")); // bagep
