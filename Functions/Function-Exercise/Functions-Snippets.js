fe677d85bfe67d983b90a33e898eb1f8dc067f94
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-24 19:09:14 -0500

    Update Functions-Snippets.js

---
+++ Functions/Function-Exercise/Functions-Snippets.js
@@ -0,0 +1,47 @@
+//Snippet 0-1
+let greet = function() {
+    console.log("Hey");
+    console.log("Programmers!");
+};
+
+let whistle = function() {
+    console.log("DOOT");
+};
+
+console.log("First");
+console.log("Second");
+greet();
+console.log("Third");
+console.log("Fourth");
+whistle();
+
+// First
+// Second
+// Hey
+// Programmers!
+// Third
+// Fourth
+// DOOT
+
+
+
+//Snippet 0-2
+let howMany = function() {
+    return 42;
+};
+
+console.log(howMany); // [Function: howMany] just printing the name of the function because there are no arguments
+console.log(howMany()); // 42
+
+const theAnswer = howMany();
+console.log(theAnswer); // 42
+
+let howMuch = function() {
+    5;
+};
+console.log(howMuch()); // undefined because there's no assigned value within the code of the function
+
+// [Function: howMany]
+// 42
+// 42
+// undefined
