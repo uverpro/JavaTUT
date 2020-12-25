dccf19961844185e2755fcbadffd6601fc62ac2d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-24 19:38:23 -0500

    Update Functions-Snippets.js

--- Functions/Function-Exercise/Functions-Snippets.js
+++ Functions/Function-Exercise/Functions-Snippets.js
@@ -45,3 +45,31 @@ console.log(howMuch()); // undefined because there's no Return Function within t
 // 42
 // 42
 // undefined
+
+
+
+//Snippet 0-3
+let average = function(num1, num2) {
+    console.log("Calculating..."); // <-- Will print everytime Return Function is called
+    return (num1 + num2) / 2;
+};
+
+console.log(average(5, 10)); // 7.5
+console.log(average(20, 26)); // 23
+console.log(average(50, 100) + 2); // 77
+
+let a = 21 + 3;
+let b = 20;
+let n = average(a, b);
+console.log (average(n, 18)); // 20
+// (average((((21 + 3) + 20) / 2 + 18) / 2)) = (((24 + 20) / 2 + 18) / 2) = ((44 / 2 + 18) / 2) = (22 + 18) / 2 = 40 / 2 = 20
+
+// Calculating...  |
+// 7.5             | First Call (Line 57)
+// Calculating...  |
+// 23              | Second Call (Line 58)
+// Calculating...  |
+// 77              | Third Call (Line 59)
+// Calculating...  | Called from the "n" argument
+// Calculating...  |
+// 20              | "Fourth" Call (Line 64)
