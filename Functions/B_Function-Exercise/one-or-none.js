e6ad24f34a7fdc3789eb6690ed086365d5766aa5
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-25 00:40:10 -0500

    Create one-or-none.js

---
+++ Functions/B_Function-Exercise/one-or-none.js
@@ -0,0 +1,18 @@
+// Write a function `oneOrNone` that accepts two booleans as arguments. The function should return true
+// if exactly one of the arguments is true. If BOTH arguments are true, then it should return false.
+
+let oneOrNone = function (arg1, arg2) {
+    if (arg1 === true && arg2 === false) {
+        return true
+    } else if (arg1 === false && arg2 === true) {
+        return true
+    } else {
+        return false
+    }
+};
+
+
+console.log(oneOrNone(false, false)); // false
+console.log(oneOrNone(true, false)); // true
+console.log(oneOrNone(false, true)); // true
+console.log(oneOrNone(true, true)); // false
