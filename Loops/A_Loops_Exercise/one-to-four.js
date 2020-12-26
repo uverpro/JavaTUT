f596090e60db5b1596c8d8f4ee0723c14868416d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 01:48:53 -0500

    Create one-to-four.js

---
+++ Loops/A_Loops_Exercise/one-to-four.js
@@ -0,0 +1,2 @@
+// Write a function `oneToFour` that prints all whole numbers from one to four, inclusive. The function
+// takes in no arguments and doesn't need to return any value. It should just print to the terminal.
