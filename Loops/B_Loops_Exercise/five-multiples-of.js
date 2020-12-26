9ed08f20b0c155ca09002622776cab0508d85973
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 16:46:24 -0500

    Update five-multiples-of.js

--- Loops/B_Loops_Exercise/five-multiples-of.js
+++ Loops/B_Loops_Exercise/five-multiples-of.js
@@ -1,3 +1,14 @@
 // Write a function named `fiveMultiplesOf` that accepts a number as an argument. The function should
 // print out the first five multiples of the given number. The function doesn't need to return any
 // value. It should just print to the terminal.
+
+let fiveMultiplesOf = function (num) {
+    for (i = num; i <= (5 * num); i += num) {
+        console.log(i);
+    }
+};
+
+fiveMultiplesOf(7)
+// 7, 14, 21, 28, 35
+fiveMultiplesOf(3)
+// 3, 6, 9, 12, 15
