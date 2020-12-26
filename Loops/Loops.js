627a0681342c8ce82dae8aa75e455a1632a36a94
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-25 22:43:54 -0500

    Update Loops.js

--- Loops/Loops.js
+++ Loops/Loops.js
@@ -4,3 +4,12 @@ console.log("I am Hungry");
 console.log("I am Hungry");
 console.log("I am Hungry");
 // What if I want to run this command "x" amount of times?
+//for (let count = 0; count <= 4; count++) {}; // Use a "for loop"
+    // Memorize these three steps of a "for loop".
+        // 1. Variable Decleration.
+        // 2. Boolean expression that is true as long as I want the loop to run. In this example, 0 through 4.
+        // 3. Incremental increase. In this example, hit each number, increment by 1.
+
+for (let count = 0; count <= 4; count++) {
+
+};
