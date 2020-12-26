03679f908f7c07d6be661a6cf25d6c2152d1600f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-25 20:22:51 -0500

    Update in-range.js

--- Functions/C_Function-Exercise/in-range.js
+++ Functions/C_Function-Exercise/in-range.js
@@ -2,7 +2,17 @@
 // return a boolean indicating if `n` is between `min` and `max` inclusive.
 
 let inRange = function (min, max, n) {
-
+    if (n >= min && n <= max) {
+        return true
+    } else {
+        return false
+    }
 };
 
 console.log(inRange(5, 13, 8)); // true
+console.log(inRange(5, 13, 29)); // false
+console.log(inRange(100, 125, 100)); // true
+console.log(inRange(100, 125, 99)); // false
+console.log(inRange(40, 45, 44)); // true
+console.log(inRange(40, 45, 45)); // true
+console.log(inRange(40, 45, 46)); // false
