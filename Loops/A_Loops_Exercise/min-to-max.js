f3d3139be20ac01e4edaf73e0d409ac96737c4e6
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 02:18:36 -0500

    Update min-to-max.js

--- Loops/A_Loops_Exercise/min-to-max.js
+++ Loops/A_Loops_Exercise/min-to-max.js
@@ -1,3 +1,13 @@
 // Write a function `minToMax(min, max)` that accepts two numbers as arguments. The function should
 // print all numbers from min to max inclusive. The function doesn't need to return any value. It
 // should just print to the terminal.
+
+let minToMax = function (min, max) {
+
+};
+
+minToMax(5, 9);
+
+minToMax(11, 13);
+
+minToMax(20, 11);
