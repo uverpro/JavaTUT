3827216a7a15ca726ef8715593fe6c0b2dae6e75
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 03:30:23 -0500

    Create evens.js

---
+++ Loops/A_Loops_Exercise/evens.js
@@ -0,0 +1,8 @@
+// Write a function `evens(max)` that accepts a max number as an argument. The function should print
+// all positive even numbers that are less than the max.
+
+
+
+evens(11);
+
+evens(8);
