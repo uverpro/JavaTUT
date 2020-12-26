d934368daa93e3be36a6d1bcceab90db32b32aab
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 02:21:37 -0500

    Update min-to-max.js

--- Loops/A_Loops_Exercise/min-to-max.js
+++ Loops/A_Loops_Exercise/min-to-max.js
@@ -3,11 +3,13 @@
 // should just print to the terminal.
 
 let minToMax = function (min, max) {
-
+    for (let i = min; i <= max; i++) {
+        console.log(i);
+    }
 };
 
 minToMax(5, 9);
-
+// 5, 6, 7, 8, 9
 minToMax(11, 13);
-
+// 11, 12, 13
 minToMax(20, 11);
