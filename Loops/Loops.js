f5f1b403d7d1e9b7b15f4fa4c0138dfc703cab5c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-25 23:09:36 -0500

    Update Loops.js

--- Loops/Loops.js
+++ Loops/Loops.js
@@ -16,15 +16,31 @@ for (let count = 0; count <= 4; count++) {
 // Start at 0.
 // Is 0 <= 4?
 // So long as statement is true, it will run the code.
+// ITERATION. Each time code is ran in a "for loop" it's referred to as an ITERATION. A SINGLE CYCLE.
 // Incremental increase by one.
 // Is 1 <= 4?
 // It will repeat until "count = 5"
 // 5 is not <= 4, so it will return false and stop running the code inside the "for loop"
 
-console.log("Start");
 
-for (let count = 0; count <= 9; count++) {
+
+console.log("Start");
+for (let i = 0; i <= 9; i++) {
     console.log("In the Loop");
 }; // will print 10x
-
 console.log("End");
+
+
+
+for (let i = 10; i >= 0; i--) { // for (let i = 10; i >= 0; i++) WOULD RUN FOREVER
+    console.log(i);
+}; // will "count down" from 10 to 0
+console.log("LIFT OFF!");
+
+
+
+console.log("Let's count by two's!")
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i);
+};
+console.log("GOOD JERB!");
