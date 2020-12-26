3936e62635543ef38037d3c38d1dfd4638022610
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 18:54:48 -0500

    Create string-repeater.js

---
+++ Loops/B_Loops_Exercise/string-repeater.js
@@ -0,0 +1,2 @@
+// Write a function named `stringRepeater(str, num)` that accepts a string and a number as arguments.
+// The function should return a new string consisting of the `str` repeated `num` number of times.
