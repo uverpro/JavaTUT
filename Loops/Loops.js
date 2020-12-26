77bbb84b893e78ff8c6a634ee32c0db2a254f70f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-25 22:34:23 -0500

    Create Loops.js

---
+++ Loops/Loops.js
@@ -0,0 +1,6 @@
+console.log("I am Hungry");
+console.log("I am Hungry");
+console.log("I am Hungry");
+console.log("I am Hungry");
+console.log("I am Hungry");
+// What if I want to run this command "x" amount of times?
