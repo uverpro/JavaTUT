9ee16591e596a1a0f1a5d62a7ccdd200fd1e4c99
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-25 22:59:04 -0500

    Update Loops.js

--- Loops/Loops.js
+++ Loops/Loops.js
@@ -11,5 +11,20 @@ console.log("I am Hungry");
         // 3. Incremental increase. In this example, hit each number, increment by 1.
 
 for (let count = 0; count <= 4; count++) {
-
+    console.log("I am Hungry"); // Will print 5 times AKA "0-4"
 };
+// Start at 0.
+// Is 0 <= 4?
+// So long as statement is true, it will run the code.
+// Incremental increase by one.
+// Is 1 <= 4?
+// It will repeat until "count = 5"
+// 5 is not <= 4, so it will return false and stop running the code inside the "for loop"
+
+console.log("Start");
+
+for (let count = 0; count <= 9; count++) {
+    console.log("In the Loop");
+}; // will print 10x
+
+console.log("End");
