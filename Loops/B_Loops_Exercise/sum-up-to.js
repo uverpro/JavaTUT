06d0ec35a23f7a5cc0f2900a760a614d03a5d7b2
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 17:01:41 -0500

    Create sum-up-to.js

---
+++ Loops/B_Loops_Exercise/sum-up-to.js
@@ -0,0 +1,12 @@
+// Write a function named `sumUpTo(max)` that accepts a max number as an argument. The function should
+// return the total sum of all whole numbers from 1 to the max, inclusive.
+//
+// For example, sumUpTo(4) should return 10 because 1 + 2 + 3 + 4 = 10.
+
+let sumUpTo = function (max) {
+    for (let i = 1; i <= max; i++) {
+        return max += 1;
+    }
+}
+
+sumUpTo(4);
