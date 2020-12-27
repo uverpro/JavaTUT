e64a2bd8b623b27b3d9358add81e7f3641456470
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 22:05:09 -0500

    Create reverse-iterate.js

---
+++ Loops/C_Loops_Exercise/reverse-iterate.js
@@ -0,0 +1,4 @@
+// Write a function `reverseIterate` that accepts a string as an argument.
+// The function should print the characters of the string one by one, in reverse order.
+//The function doesn't need to return any value.
+// It should just print to the terminal.
