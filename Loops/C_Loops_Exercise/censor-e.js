6e2c2c26b354dfcab8d880cd3c19bb960b4c7771
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-27 00:26:56 -0500

    Create censor-e.js

---
+++ Loops/C_Loops_Exercise/censor-e.js
@@ -0,0 +1,2 @@
+// Write a function `censorE` that accepts a string as an argument.
+//The function should return the a new version of string where all characters that are 'e's are replaced with '*'s.
