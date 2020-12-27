4dfdeca6f83a105f7e09a5e7c7d1020f1a343488
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 23:46:09 -0500

    Update raise-power.js

--- Loops/C_Loops_Exercise/raise-power.js
+++ Loops/C_Loops_Exercise/raise-power.js
@@ -1,15 +1,5 @@
-
-let removeCapitals = function (str) {
-    let capstr = "";
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i]; // assign the str[i] a variable
-        if (char === char.toUpperCase()) {  //  if (str[i] === str[i].toLowerCase()) {
-        capstr -= char //  capstr += str[i]
-         }
-        }
-    return capstr;
-    }
-
-console.log(removeCapitals("fOrEver")); // frver
-console.log(removeCapitals("raiNCoat")); // raioat
-console.log(removeCapitals("cElLAr Door")); // clr oor
+// Write a function `raisePower(base, exponent)` that accepts two numbers, `base` and `exponent`. The
+// function should return `base` raised to the `exponent` power.
+//
+// For example, raisePower(2, 5) should return 32 because 2 * 2 * 2 * 2 * 2 = 32
+// For example, raisePower(4, 3) should return 64 because 4 * 4 * 4 = 64
