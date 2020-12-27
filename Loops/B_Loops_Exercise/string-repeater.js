8da5de1cb044917aa63123990c2d7ec241682b2f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 20:25:05 -0500

    Update string-repeater.js

--- Loops/B_Loops_Exercise/string-repeater.js
+++ Loops/B_Loops_Exercise/string-repeater.js
@@ -14,3 +14,17 @@ let stringRepeater = function (str, num) {
 console.log(stringRepeater("q", 4)); // qqqq
 console.log(stringRepeater("go", 2));  // gogo
 console.log(stringRepeater("tac", 3)); // tactactac
+
+// declare the function & parameters
+// declare variable = empty string aka "" (same as 0 in a number problem)
+// run the for loop
+// is 1 <= 2? TRUE
+// "" + "go"
+// run the loop
+// is 2 <= 2? TRUE
+// "go" + "go"
+// run the for loop
+// is 3 <= 2? FALSE.
+// end loop
+// return "gogo"
+// A FOR LOOP is just to run the code below a certain number of times
