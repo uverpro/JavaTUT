2a69cf6f505734af66cfb4ee03d5938ac6e53cb2
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 21:32:51 -0500

    Update divisible-range.js

--- Loops/C_Loops_Exercise/divisible-range.js
+++ Loops/C_Loops_Exercise/divisible-range.js
@@ -1,2 +1,9 @@
 // Write a function `divisibleRange(min, max, num)` that accepts three numbers as arguments.
 // The function should print all numbers between `min` and `max` (exclusive) that are also divisible by num.
+
+let divisibleRange = function (min, max, num) {
+
+};
+
+divisibleRange(17, 40, 9);
+// 18, 27, 36
