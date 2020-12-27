5883d3e744a9a8553d7ca4458258ff599ceb53ec
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-27 03:03:36 -0500

    Update Arrays.js

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -1,3 +1,4 @@
-let fruits = ["apple", "mango", "kiwi"];
+let fruits = ["apple", "mango", "kiwi", "grapes"];
 console.log(fruits); // Will print the ELEMENTS of the Array
-console.log(fruits.length); // WILL print 3 because there are 3 ELEMENTS in the Array
+console.log(fruits.length); // WILL print 4 because there are 3 ELEMENTS in the Array
+console.log(fruits[0]); // WILL print "apple" because ELEMENTS in array begin at "0", same as indices
