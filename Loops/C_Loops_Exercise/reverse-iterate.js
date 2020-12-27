0fc39ba59cfd45a1ce4c334ce7ab86f5cddc5ce5
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 22:37:37 -0500

    Update reverse-iterate.js

--- Loops/C_Loops_Exercise/reverse-iterate.js
+++ Loops/C_Loops_Exercise/reverse-iterate.js
@@ -5,7 +5,7 @@
 
 let reverseIterate = function (str) {
     let stringy = ""
-    for (let i = str.length - 1; i < str.length; i--) {
+    for (let i = str.length - 1; i >= 0; i--) {
         stringy += i;
         console.log(str[i]);
     }
