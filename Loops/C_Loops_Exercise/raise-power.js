d7f292d7b0a6728dc83e0c75ac1183e8075d60bf
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 23:48:03 -0500

    Update raise-power.js

--- Loops/C_Loops_Exercise/raise-power.js
+++ Loops/C_Loops_Exercise/raise-power.js
@@ -3,3 +3,9 @@
 //
 // For example, raisePower(2, 5) should return 32 because 2 * 2 * 2 * 2 * 2 = 32
 // For example, raisePower(4, 3) should return 64 because 4 * 4 * 4 = 64
+
+
+console.log(raisePower(2, 5));
+console.log(raisePower(4, 3));
+console.log(raisePower(10, 4));
+console.log(raisePower(7, 2));
