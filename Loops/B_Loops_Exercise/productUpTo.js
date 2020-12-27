ffdf9b4e76696714f6acd8d4fd10f8c492123160
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 20:25:08 -0500

    Create productUpTo.js

---
+++ Loops/B_Loops_Exercise/productUpTo.js
@@ -0,0 +1,5 @@
+// Write a function named `productUpTo(max)` that accepts a max number as an argument. The function
+// should return the total product of all whole numbers from 1 to the max, inclusive. A product is a
+// number obtained from multiplying numbers together.
+//
+// For example, productUpTo(4) should return 24 because 1 * 2 * 3 * 4 = 24
