8d1dbff8cf84f806cb71b97122f5f3c6a915a59a
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-27 03:01:45 -0500

    Update Arrays.js

---
+++ Arrays/Arrays.js
@@ -0,0 +1,3 @@
+let fruits = ["apple", "mango", "kiwi"];
+console.log(fruits); // Will print the ELEMENTS of the Array
+console.log(fruits.length); // WILL print 3 because there are 3 ELEMENTS in the Array
