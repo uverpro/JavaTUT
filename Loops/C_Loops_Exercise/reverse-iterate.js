230850096e95cc48d0d711b20d1c7c72f299cc0d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-26 22:18:34 -0500

    Update reverse-iterate.js

--- Loops/C_Loops_Exercise/reverse-iterate.js
+++ Loops/C_Loops_Exercise/reverse-iterate.js
@@ -2,3 +2,13 @@
 // The function should print the characters of the string one by one, in reverse order.
 //The function doesn't need to return any value.
 // It should just print to the terminal.
+
+let reverseIterate = function (str) {
+    let stringy = ""
+    for (let i = str.length - 1; i < str.length; i--) {
+        stringy += i;
+        console.log(str[i]);
+    }
+};
+
+reverseIterate("carrot");
