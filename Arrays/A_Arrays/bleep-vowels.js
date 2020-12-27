ee33efe5956f43edc1aa181fb4c8acbf007ab949
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-27 05:53:27 -0500

    Update bleep-vowels.js

--- Arrays/A_Arrays/bleep-vowels.js
+++ Arrays/A_Arrays/bleep-vowels.js
@@ -2,24 +2,6 @@
 // The function should return a new string where all vowels are replaced with `*`s.
 // Vowels are the letters a, e, i, o, u.
 
-// function bleepVowels (str) {
-//     let bleep = "";
-//     for (i = 0; i < str.length; i++) {
-//         let chars = str[i]; // console.log(str[i])
-//         if (chars === "a" || "e" || "i" || "o" || "u") {
-//             bleep += "*"
-//         } else {
-//             bleep += chars
-//         }
-//     }
-//     return bleep
-// }
-
-// console.log(bleepVowels("skateboard")); // 'sk*t*b**rd'
-// console.log(bleepVowels("slipper")); // 'sl*pp*r'
-// console.log(bleepVowels("range")); // 'r*ng*'
-// console.log(bleepVowels("brisk morning")); // 'br*sk m*rn*ng'
-
 //Alvin's Solution w/ Array
 
 function bleepVowels (str) {
