29e22289b49580a48fe11f071e974828c35d63dd
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-27 03:05:37 -0500

    Update Arrays.js

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -1,4 +1,12 @@
+
+
+
 let fruits = ["apple", "mango", "kiwi", "grapes"];
 console.log(fruits); // Will print the ELEMENTS of the Array
 console.log(fruits.length); // WILL print 4 because there are 3 ELEMENTS in the Array
 console.log(fruits[0]); // WILL print "apple" because ELEMENTS in array begin at "0", same as indices
+
+
+
+let stuff = [true, 42, "dog"]; // Can put anything into an Array. Booleans, numbers, strings.
+console.log(stuff);
