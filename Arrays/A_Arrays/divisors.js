84c600d48c144d3530f4c18b1ffe33b79f05cd04
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-28 00:33:41 -0500

    Create divisors.js

---
+++ Arrays/A_Arrays/divisors.js
@@ -0,0 +1,10 @@
+// Write a function `divisors` that accepts a number as an argument.
+//The function should return an array containing all positive numbers that can divide into the argument.
+
+function divisors (number) {
+
+};
+
+console.log(divisors(15)); // [1, 3, 5, 15]
+console.log(divisors(7)); // [1, 7]
+console.log(divisors(24)); // [1, 2, 3, 4, 6, 8, 12, 24]
