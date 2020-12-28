7ac22909924f38eebbb7ccae67f6d5da70834991
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-28 00:07:45 -0500

    Update strings-to-lengths.js

--- Arrays/A_Arrays/strings-to-lengths.js
+++ Arrays/A_Arrays/strings-to-lengths.js
@@ -1,2 +1,11 @@
 // Write a function `stringsToLengths` that accepts an array of strings as an argument.
 //The function should return a new array containing the lengths of the elements of the original array.
+
+function stringsToLengths (array)
+
+
+console.log(stringsToLengths(["belly", "echo", "irony", "pickled"]));
+// [5, 4, 5, 7]
+
+console.log(stringsToLengths(["on", "off", "handmade"]));
+// [2, 3, 8]
