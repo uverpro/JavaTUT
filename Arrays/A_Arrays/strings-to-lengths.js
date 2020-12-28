1a06b94e996b751f975f20512d9523aa8adce851
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-28 00:26:00 -0500

    Update strings-to-lengths.js

--- Arrays/A_Arrays/strings-to-lengths.js
+++ Arrays/A_Arrays/strings-to-lengths.js
@@ -6,6 +6,8 @@ function stringsToLengths (words) {
     for (i = 0; i < words.length; i++) {
         let nums = words[i];    //console.log(array[i]);
         lengths.push(nums.length); // words[i].length WORKS. No need for conditionals!
+        // lengths.push puts "()" into new array
+        // words[i].length = words[0].length = belly.length = 5
     }
     return lengths;
 };
