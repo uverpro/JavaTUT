f456a041cc1acf6879ea1073bcfadd9c14d8295b
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-28 00:07:05 -0500

    Create strings-to-lengths.js

---
+++ Arrays/A_Arrays/strings-to-lengths.js
@@ -0,0 +1,2 @@
+// Write a function `stringsToLengths` that accepts an array of strings as an argument.
+//The function should return a new array containing the lengths of the elements of the original array.
