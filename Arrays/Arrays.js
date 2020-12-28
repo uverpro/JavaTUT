bc73a2445a925eb4fa330b62c9756aad85bce562
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2020-12-28 03:30:16 -0500

    Update Arrays.js

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -105,9 +105,10 @@ console.log(superfans);
 // If the array is empty it will return null
 
 function smallestNum (arraynums) {
-    let smallest = nums[0]; // Create a new variable. The default value is the first index in the array.
-    for (let i = 0; i < arraynums.length; i++) {
-        console.log(arraynums[i]); // 5, 6, 4, 3, 7 // console.log(i); (0, 1, 3, 4,)
+    let smallest = arraynums[0]; // Create a new variable. The default value is the first index in the array.
+    for (let i = 0; i < arraynums.length; i++) { // change i to default value of "1" so as to not compare index 0 (5) against itself.
+        // console.log(arraynums[i]); // 5, 6, 4, 3, 7 // console.log(i); (0, 1, 3, 4,)
+        let num = arraynums[i];
     }
 };
 
