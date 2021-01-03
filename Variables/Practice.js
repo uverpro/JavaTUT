6647f89c0636bdf30a7daa21c72caefd045fb16e
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-03 06:33:17 -0500

    Update Practice.js

---
+++ Variables/Practice.js
@@ -0,0 +1,19 @@
+let apple;
+console.log(apple); // undefined
+
+apple = 5;
+console.log(apple); // 5
+
+apple + 1;
+console.log(apple); // 5
+
+apple += 1;
+console.log(apple); // 6
+
+let banana = apple;
+console.log(banana); // und
+
+banana = banana / 2;
+console.log(banana); // undefined
+
+console.log(apple); // 6
