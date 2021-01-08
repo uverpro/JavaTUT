c4be774956095ec3ee7adaa93646e8bf23b9c87d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-08 17:59:37 -0500

    Update five-multiples-of.js

--- Loops/B_Loops_Exercise/five-multiples-of.js
+++ Loops/B_Loops_Exercise/five-multiples-of.js
@@ -12,3 +12,8 @@ fiveMultiplesOf(7)
 // 7, 14, 21, 28, 35
 fiveMultiplesOf(3)
 // 3, 6, 9, 12, 15
+
+
+// for (i = num, i <= (5 * num); i += num)
+// for (i = 7; while 7 <= 35; 7 += 7)
+// console.log 7, 7+7, 14+7, 21+7, 28+7
