3c3247c137ea00fd8e239e892df94cf0af21aa57
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-09 11:42:03 -0500

    Update how-many-es.js

--- Loops/A_Loops_Exercise/how-many-es.js
+++ Loops/A_Loops_Exercise/how-many-es.js
@@ -5,7 +5,7 @@ function howManyEs (str) {
             count += 1;
         }
         return count;
-    }
+    };
 
     console.log(howManyEs("To be, or not to be, that is the question.")); // 4
     console.log(howManyEs("eeeeee o, hi ho!")); // 6
