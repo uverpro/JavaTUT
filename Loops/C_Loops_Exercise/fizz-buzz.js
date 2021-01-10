f7f2dcd8c765963256145e659c9e03828b9231a4
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-10 14:22:14 -0500

    Update fizz-buzz.js

--- Loops/C_Loops_Exercise/fizz-buzz.js
+++ Loops/C_Loops_Exercise/fizz-buzz.js
@@ -1,4 +1,4 @@
-// Write a function `fizzBuzz` that accepts a max number as an argument.
+ // Write a function `fizzBuzz` that accepts a max number as an argument.
 //The function should print all numbers less than or equal to max that are divisible by either 3 or 5
 //but not both 3 and 5.
 //The function doesn't need to return any value. It should just print to the terminal.
