6a67d8cc58a5a50e98e59751cafba8087d1962c6
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-10 11:51:14 -0500

    Update Practice.js

--- Variables/Practice.js
+++
@@ -1,9 +0,0 @@
-function divisibleRange (min, max, num) {
-    for (i = num; i < max; i += num)
-    if (i > min) {
-    console.log(i)
-    }
-};
-
-divisibleRange(17, 40, 9);
-divisibleRange(10, 24, 4);
