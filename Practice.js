6522543cf69cafe6c0367473c3b94a4c205dce65
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-10 17:08:14 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,16 +1,22 @@
 function bleepVowels (str) {
-    let nustr = "";
+    let censored = "";
     for (i = 0; i < str.length; i++) {
-        if (str[i] === "a" || "e" || "i" || "o" || "u")
-        nustr += "*";
-    } else {
-        nustr += str[i];
+        let letter = str[i];
+        if (letter === "a" ||
+            letter === "e" ||
+            letter === "i" ||
+            letter === "o" ||
+            letter === "u") {
+            censored += "*";
+        } else {
+            censored += letter;
+        }
     }
-    }
-    return nustr;
+    return censored
 };
 
 console.log(bleepVowels("skateboard")); // 'sk*t*b**rd'
 console.log(bleepVowels("slipper")); // 'sl*pp*r'
 console.log(bleepVowels("range")); // 'r*ng*'
 console.log(bleepVowels("brisk morning")); // 'br*sk m*rn*ng'
+console.log(bleepVowels("apple"));
