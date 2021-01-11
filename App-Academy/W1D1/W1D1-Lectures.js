5340cc9d7f4e21443c001803136f513ba1a20bf9
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-11 16:46:59 -0500

    Create W1D1-Lectures.js

---
+++ App-Academy/W1D1/W1D1-Lectures.js
@@ -0,0 +1,27 @@
+// Data Types
+// Numbers
+// Booleans
+// Strings
+// Arrays
+// Objects
+
+// "()" are grouping operators
+// "++" is the "increment operator"
+
+// JS reads from top down
+
+// Variable name to the left, value to the right
+// Variable Declerations:
+// "let" (can be reassigned)
+// "const" (cant be reassigned)
+
+// Variable Assignment - assigns value to the variable
+
+// let age = 0;
+// age = 5;
+// age += 5;
+// console.log(age); // 10
+
+// let count = 0;
+// count++
+// console.log(count) // 1
