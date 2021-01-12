72e2019245167a737c2706932157edc1909b7b90
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-12 12:25:02 -0500

    Update W1D2-Lectures.js

--- App-Academy/W1/D2/W1D2-Lectures.js
+++ App-Academy/W1/D2/W1D2-Lectures.js
@@ -49,6 +49,85 @@
 
 
 
+// // ====== Functions ======:
+// function averageOfFour(arg1, arg2, arg3, arg4) {
+//    console.log((arg1 + arg2 + arg3 + arg4) / 4);
+// }
+
+// averageOfFour(1, 20, 500, 11); // 133
+// averageOfFour(10, 20, 30, 40); //25
+
+
+
+
+
+// // ====== Conditionals ===:
+// let myBoolean = false;
+
+// if (myBoolean) {
+//     console.log("hi");
+// } else {
+//     console.log("hello");
+// }
+
+// myBoolean = 5;
+// if (myBoolean < 6) {
+//     console.log("this is a truthy value");
+// }
+// if (myBoolean < 10 ) {
+//     console.log("this is a falsey value");
+// }
+
+// // 'else' is mutually exclusive:
+// let hungerLevel = 5;
+// if (hungerLevel < 1) {
+//     console.log('not hungry yet');
+// } else if (hungerLevel <= 5) {
+//     console.log('snack time!');
+// } else if (hungerLevel <= 9) {
+//     console.log('Time for a meal!');
+// } else {
+//     console.log('HANGRY!');
+// } else if (hungerLevel <= 9) {
+//     console.log('almost HANGRY!');
+// }
+
+
+
+
+
+// // ====== Loops ===:
+
+// let ticker = 3;
+
+// while (ticker > 0 ) {
+//     console.log("I ran a lap!");
+//     ticker--;
+// }
+
+// for (let i = 0; i < 3; i++) {
+//     console.log("I ran a lap!")
+// }
+
+
+
+
+// // ======= Amazing Arrays === :
+// let array = [];
+
+// array.push(4);
+// array.push(5);
+// array.push("sup");
+
+// console.log(array); // => ?
+// console.log(array[0]); // => ?
+// console.log(array[array.length - 1]); // => ?
+// console.log(array.length); // => ?
+
+
+
+
+
 // # `Functions`
 // - input (aka Arguments), output (aka return value),
 // - "Invoking a function" --> calling your function into action / running the function code
