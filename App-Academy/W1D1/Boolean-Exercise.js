c75d826004892ba9fbbdc30a4d7cfe7434d91be3
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-12 11:16:38 -0500

    Delete Boolean-Exercise.js

--- App-Academy/W1D1/Boolean-Exercise.js
+++
@@ -1,8 +0,0 @@
-// Booleans Exercise
-// Write 5 examples of expressions using each of the operations: !, &&, ||
-
-console.log(true || false); // true
-console.log(true && false); // false
-console.log(!true && false); // false
-console.log(!(false || false) && true); // true
-console.log(true || !false); // true
