f949e29d6118b16c8c0ff1447fc6370dac51e8df
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 14:26:28 -0500

    Create typeof.js

---
+++ Operators-&-Expressions/typeof.js
@@ -0,0 +1,16 @@
+// The typeof operator returns a string indicating the type of the unevaluated operand.
+
+console.log(typeof 42);
+// expected output: "number"
+
+console.log(typeof 'blubber');
+// expected output: "string"
+
+console.log(typeof true);
+// expected output: "boolean"
+
+console.log(typeof undeclaredVariable);
+// expected output: "undefined"
+
+console.log(typeof [1, 2]);
+// expected output: "object"
