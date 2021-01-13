60816a0ce1c7ee94c25fc663a677c32760e0b734
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 14:50:09 -0500

    Create Array.toString.js

---
+++ Methods/Arrays-Methods/Array.toString.js
@@ -0,0 +1,6 @@
+// The toString() method returns a string representing the specified array and its elements.
+
+const array1 = [1, 2, 'a', '1a'];
+
+console.log(array1.toString());
+// expected output: "1,2,a,1a"
