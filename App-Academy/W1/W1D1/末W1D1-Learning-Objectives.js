c7ab3e36f3d4623cb2fd8c6cd37c96f481ba1682
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 13:03:00 -0500

    Update 末W1D1-Learning-Objectives.js

--- App-Academy/W1/W1D1/末W1D1-Learning-Objectives.js
+++ App-Academy/W1/W1D1/末W1D1-Learning-Objectives.js
@@ -3,4 +3,4 @@
 // 1. Craft a clear, concise coding question to a more experienced developer
 // 2. Research unknown JavaScript code syntax using MDN
 // 3. Identify and fix a bug in code based on an error message
-// 4. Manage your time and stress at AppAcademy
+// for 3 use const rather than let
