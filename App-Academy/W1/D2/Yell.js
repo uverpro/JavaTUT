27358049b27d65fc0fdb387db018fcb179e1e8d3
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 11:05:26 -0500

    Delete Yell.js

--- App-Academy/W1/D2/Yell.js
+++
@@ -1,8 +0,0 @@
-// Write a function yell that takes in a string and returns a "yelled" version of that string
-
-function yell(str) {
-    return str.toUpperCase();
-  }
-
-  console.log(yell("I want to go to the store")); // => "I WANT TO GO TO THE STORE!!!"
-  console.log(yell("Time to program")); // => "TIME TO PROGRAM!!!"
