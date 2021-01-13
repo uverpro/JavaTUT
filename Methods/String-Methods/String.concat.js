b6091e57cdfd55ff154d37820d2496dd3187329d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 11:48:13 -0500

    Create String.concat.js

---
+++ Methods/String-Methods/String.concat.js
@@ -0,0 +1,15 @@
+// The CONCAT() METHOD
+
+// The concat() method concatenates the string arguments to the calling string
+// and returns a new string.
+
+const str1 = "Hello ";
+const str2 = "World";
+console.log(str1.concat(str2)); // => "Hello World"
+console.log(str1 + str2); // => "Hello World"
+
+
+
+let string1 = "Goodbye";
+let string2 = "cruel world";
+console.log(string1.concat(", ", string2)); // => "Goodbye, cruel world"
