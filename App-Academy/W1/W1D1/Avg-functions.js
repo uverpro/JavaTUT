5b6e362cae32be30152dde31feb156547e88915a
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 11:05:32 -0500

    Create Avg-functions.js

---
+++ App-Academy/W1/W1D1/Avg-functions.js
@@ -0,0 +1,8 @@
+function averageOf (num1, num2) {
+    return (num1 + num2) / 2;
+};
+
+console.log(averageOf(5, 10)); // 7.5
+console.log(averageOf(10, 20)); // 15
+console.log(averageOf(50, 100)); // 75
+console.log(averageOf(100, 200)); // 150
