b314ce50b63e2fcf91cd26d6541dfd1d47c966ad
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 12:46:28 -0500

    Create 末W1D1-Learning-Objectives.js

---
+++ App-Academy/W1/W1D1/末W1D1-Learning-Objectives.js
@@ -0,0 +1,6 @@
+// Problem Solving Learning Objectives
+
+// 1. Craft a clear, concise coding question to a more experienced developer
+// 2. Research unknown JavaScript code syntax using MDN
+// 3. Identify and fix a bug in code based on an error message
+// 4. Manage your time and stress at AppAcademy
