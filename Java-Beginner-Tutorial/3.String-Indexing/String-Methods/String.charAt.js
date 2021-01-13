caf30d5696e058eb235bf3314c4f66d52eb81918
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-12 20:12:01 -0500

    Update String.charAt.js

--- Java-Beginner-Tutorial/3.String-Indexing/String-Methods/String.charAt.js
+++ Java-Beginner-Tutorial/3.String-Indexing/String-Methods/String.charAt.js
@@ -9,3 +9,5 @@ console.log(sentence.charAt(4)); // h
 const index = 4;
 console.log("The character at index " + index + " is " + sentence.charAt(index));
 // The character at index 4 is h
+console.log("The character at index " + index + " is " + sentence[index]);
+// The character at index 4 is h
