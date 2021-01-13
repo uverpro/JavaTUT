fd38a2fd837933fc92fe7c773746c7cdb6ae6f11
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 14:26:25 -0500

    Update 末W1D1-Learning-Objectives.js

--- App-Academy/W1/W1D1/末W1D1-Learning-Objectives.js
+++ App-Academy/W1/W1D1/末W1D1-Learning-Objectives.js
@@ -3,4 +3,19 @@
 // 1. Craft a clear, concise coding question to a more experienced developer
 // 2. Research unknown JavaScript code syntax using MDN
 // 3. Identify and fix a bug in code based on an error message
-// for 3 use const rather than let
+
+function arrayToUpperCase (array) {
+    for (let i = 0; i < array.length; i++) {
+      const element = array[i];
+      if (typeof element === 'string') { // you need "if (typeof element === 'string')"
+        let upperCased = element.toUpperCase();
+        array.splice(i, 1, upperCased);
+      }
+    }
+    return array;
+  };
+
+  const foods = ['banana', 'taco', 'cookie'];
+  console.log(arrayToUpperCase(foods));
+
+// TYPEOF
