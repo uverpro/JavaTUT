9ff87fa850e3783ddbf596b4f932aec8c0481c6a
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 17:01:51 -0500

    Create Increment-Operator.js

---
+++ Operators-&-Expressions/Increment-Operator.js
@@ -0,0 +1,19 @@
+// The increment operator (++) increments (adds one to) its operand and returns a value.
+
+let x = 3;
+const y = x++;
+
+console.log("x:" + x + ", y:" + y);
+// expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+console.log("a:" + a + ", b:" + b);
+// expected output: "a:4, b:4"
+
+// If used postfix, with operator after operand (for example, x++),
+// the increment operator increments and returns the value before incrementing.
+// If used prefix, with operator before operand (for example, ++x),
+// the increment operator increments and returns the value after incrementing.
