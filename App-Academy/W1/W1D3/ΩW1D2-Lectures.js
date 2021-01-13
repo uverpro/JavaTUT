0f326ef8c77b8d4785217bcf0fed8984f639f6b1
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 11:19:33 -0500

    Create ΩW1D2-Lectures.js

---
+++ App-Academy/W1/W1D3/ΩW1D2-Lectures.js
@@ -0,0 +1,12 @@
+// {/* <label>Favorite Cookie</label>
+// <input type="text" id="fav-cookie" name="favorite_cookie">
+// What HTML attribute needs to be added to the label tag to connect it to the input tag?
+// And what would be the value of the HTML attribute?
+
+
+// for="fav-cookie"
+
+// EXPLANATION
+// The "for" attribute on a label tag is should have the same value as the id of the input
+// you want to connect the label to.
+// Alternatively, you can put the input element inside of the label element.
