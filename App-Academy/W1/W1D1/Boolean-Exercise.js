bd53fc7bcb54f4ee0bb2b7fe84c9e38099b8c2e8
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 11:05:34 -0500

    Create Boolean-Exercise.js

---
+++ App-Academy/W1/W1D1/Boolean-Exercise.js
@@ -0,0 +1,8 @@
+// Booleans Exercise
+// Write 5 examples of expressions using each of the operations: !, &&, ||
+
+console.log(true || false); // true
+console.log(true && false); // false
+console.log(!true && false); // false
+console.log(!(false || false) && true); // true
+console.log(true || !false); // true
