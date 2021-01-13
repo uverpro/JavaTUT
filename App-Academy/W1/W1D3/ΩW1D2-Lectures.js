6708100c35d2d8514199751edd0e257da2dcc465
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 11:39:50 -0500

    Update ΩW1D2-Lectures.js

--- App-Academy/W1/W1D3/ΩW1D2-Lectures.js
+++ App-Academy/W1/W1D3/ΩW1D2-Lectures.js
@@ -10,3 +10,98 @@
 // The "for" attribute on a label tag is should have the same value as the id of the input
 // you want to connect the label to.
 // Alternatively, you can put the input element inside of the label element.
+
+
+
+// FUNCTION DECLERATION SYNTAX
+// function sayHello () {
+    // console.log('hello);
+    // console.log('bye');
+// };
+
+// FUNCTION EXPRESSION SYNTAX
+// let sayHello = function () {
+//      console.log('hello');
+//      console.log('bye');
+//};
+
+
+
+
+
+// === Array.prototype.push
+let people = ['Miah'];
+let firstPeople = people;
+
+people.push('Adrian');
+people.push('Tom');
+people.push('Justin');
+people.push('Emily');
+
+console.log(people); // [ 'Miah', 'Adrian', 'Tom', 'Justin', 'Emily' ]
+console.log(firstPeople === people); // true
+
+
+
+// === Array.prototype.pop
+// Permanently removes and returns the last element in an array
+let dogs = ['Fido', 'Digby', 'Fluffy'];
+let beforePop = dogs;
+
+const lastDog = dogs.pop();
+
+// console.log(lastDog); // 'Fluffy'
+// console.log(dogs); // ['Fido', 'Digby']
+// console.log(beforePop === dogs);
+
+
+
+// Array.prototype.shift
+// let cats = ['Paprika', 'Whiskers', 'Garfield'];
+// let beforeShift = cats;
+
+// let firstCat = cats.shift();
+
+// console.log(firstCat); // 'Paprika'
+// console.log(cats); // ['Whiskers', 'Garfield']
+// console.log(beforeShift === cats);
+
+
+
+// Array.prototype.unshift
+let beforeUnshift = cats;
+
+cats.unshift('Sennacy');
+
+// console.log(cats); // ['Sennacy', 'Whiskers', 'Garfield']
+// console.log(beforeUnshift === cats);
+
+
+
+
+
+
+
+
+
+
+// Array.prototype.splice
+let drinks = ['coffee', 'oj', 'pumpkin juice'];
+drinks.splice(1, 2, 'milk');
+
+console.log(drinks); // [ 'coffee', 'milk' ]
+
+
+
+
+
+
+function arrayToUpperCase(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    if (typeof element === 'string') {
+      element = element.toUpperCase();
+    }
+  }
+  return arr;
+}
