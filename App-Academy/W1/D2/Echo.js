4aa44138ef3b29acc3e944b63278531261f6389f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 11:05:07 -0500

    Delete Echo.js

--- App-Academy/W1/D2/Echo.js
+++
@@ -1,9 +0,0 @@
-// Write a function echo that takes in a string and console.logs that string "echo-ized".
-
-function echo (string) {
-    console.log(string.toUpperCase() + " ... " + string + " ... " + string.toLowerCase());
-};
-
-echo("Mom!"); // => prints "MOM! ... Mom! ... mom!"
-echo("hey"); // => prints "HEY ... hey ... hey"
-echo("JUMp"); // => prints "JUMP ... JUMp ... jump"
