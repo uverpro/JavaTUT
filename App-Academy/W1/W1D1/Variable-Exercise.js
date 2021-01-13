cd88d9b4f8de8defed3c9553f2409b890b0a71e4
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 11:05:43 -0500

    Create Variable-Exercise.js

---
+++ App-Academy/W1/W1D1/Variable-Exercise.js
@@ -0,0 +1,27 @@
+// Variables Exercise
+
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+
+let firstName = "Cory";
+
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+
+let lastName = "Pedigo";
+
+// 3. Declare a variable called `age`:
+
+let age;
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+console.log(firstName, lastName, age); // Cory Pedigo undefined
+
+// 5. Assign the `age` variable to a number:
+
+age = 35;
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+console.log(firstName, lastName, age); // Cory Pedigo 35
