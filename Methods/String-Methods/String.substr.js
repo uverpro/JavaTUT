24434a781f19895745f4affc511882dea337144c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 20:24:22 -0500

    Create String.substr.js

---
+++ Methods/String-Methods/String.substr.js
@@ -0,0 +1,10 @@
+// The substr() method returns a portion of the string,
+// starting at the specified index and extending for a given number of characters afterwards.
+
+const str = 'Mozilla';
+
+console.log(str.substr(1, 2)); // gives the next two chars from str[1]
+// expected output: "oz"
+
+console.log(str.substr(2)); // works same as slice without an optional argument
+// expected output: "zilla"
