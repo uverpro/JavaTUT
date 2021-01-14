17ad4f643e7ed2d09a368018cc2ceb14306ca608
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 23:23:45 -0500

    Update ΩW1D2-Learning-Objectives.js

--- App-Academy/W1/W1D2/ΩW1D2-Learning-Objectives.js
+++ App-Academy/W1/W1D2/ΩW1D2-Learning-Objectives.js
@@ -24,5 +24,32 @@ console.log(str[0]); // L
 // 7. Create a new string from a sub-section of another string
 
 str = "Don't slice me bro, to get a nice new sting or something."
-console.log(str.slice(0, 18));
-console.log(str.substr(27, 16));
+console.log(str.slice(0, 18)); // Don't slice me bro
+console.log(str.substr(27, 16)); // a nice new sting
+
+
+
+// Control Flow and Array Lesson Learning Objectives
+
+// 1. Define a function that accepts a sentence string and two words as args.
+// The function should return a boolean indicating if the sentence includes either word.
+function eitherWord (str, word1, word2) {
+    for (i = 0, i < str.length; i++)
+    console.log(str[i])
+};
+console.log(eitherWord("I like the cheese", "I", "cheese"))
+
+// 2. Identify a pair of mutually exclusive conditions.
+
+// 3. Given a for loop, translate it into a while loop, and vice-versa.
+
+// 4. Write a function that iterates through a provided string argument.
+
+// 5. Given a description of pig latin, write a function that takes in a string argument and utilizes String's slice() method to translate the string into pig latin.
+
+// 6. Write a function that takes in an array of words and a string as arguments
+// and returns a boolean indicating whether the string is located inside of the array.
+// The function must use Array's indexOf() method.
+
+// 7. Define that an array literal is an ordered list of values defined by using bracket
+//  and individual values are read by indexing.
