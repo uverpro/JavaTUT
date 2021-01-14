4d9733388e3750216829b0614ce4c615cdfb9777
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-14 17:53:13 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,16 +1,11 @@
-// 6. Write a function that takes in an array of words and a string as arguments
-// and returns a boolean indicating whether the string is located inside of the array.
-// The function must use Array's indexOf() method.
-function inArray (array, string) {
-  if (array.indexOf(string) > - 1) {
-    return true;
-  } else {
-    return false;
-  }
+let bigNumber = function () {
+  console.log("FUCK BUTT POINT");
 };
 
-console.log(inArray(["hi", "bye", 8], 8)); // true
-console.log(inArray(["hi", "bye", 8], "hi")); // true
-console.log(inArray([10, 11, 8], 911)); // false
-console.log(inArray([10, 11, 8], 100)); // false
-console.log(inArray(["ya", "yo", "no"], "false")); // false
+function numberSeparator(number) {
+  if (number <= 100) {
+    smallNumber();
+  } else {
+    bigNumber();
+  }
+}
