05d8f3781ae12357166687ec5e4de5e8bd911da5
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-13 23:41:55 -0500

    Update ΩW1D2-Learning-Objectives.js

--- App-Academy/W1/W1D2/ΩW1D2-Learning-Objectives.js
+++ App-Academy/W1/W1D2/ΩW1D2-Learning-Objectives.js
@@ -33,15 +33,44 @@ console.log(str.substr(27, 16)); // a nice new sting
 
 // 1. Define a function that accepts a sentence string and two words as args.
 // The function should return a boolean indicating if the sentence includes either word.
-function eitherWord (str, word1, word2) {
-    for (i = 0, i < str.length; i++)
-    console.log(str[i])
-};
-console.log(eitherWord("I like the cheese", "I", "cheese"))
+function containsEither (word, str1, str2) {
+    // str1.indexOf(str2) // if str2 is in the index of str1 it will be greater than "-1" because "-1" IS NOT A VALID INDEX
+    // return str1.indexOf(str2) > -1; // will return all correct except line 13 b/c capitalization
+    let lowerCaseWord = word.toLowerCase();
+    let lowerCaseStr1 = str1.toLowerCase();
+    let lowerCaseStr2 = str2.toLowerCase();
+    if (lowerCaseWord.indexOf(lowerCaseStr1) > -1 || lowerCaseWord.indexOf(lowerCaseStr2) > -1) {
+    return true;
+    } else return false;
+    }
+
+  console.log(containsEither("caterpillar", "pill", "ar")); // true
+  console.log(containsEither("lion's share", "on", "io")); // true
+  console.log(containsEither("SORRY", "or", "so")); // true
+  console.log(containsEither("tangent", "gan", "tan")); // true
+  console.log(containsEither("clock", "ok", "co")); // false
 
 // 2. Identify a pair of mutually exclusive conditions.
+let str = "I am"
+    if (str.length < 5)  {
+        console.log(str + " not!");
+        } else {
+            console.log(str);
+}
 
 // 3. Given a for loop, translate it into a while loop, and vice-versa.
+let str = "I am a String!"
+
+    for (i = 0; i < str.length; i++) {
+        console.log(str[i]);
+    }
+
+let str = "I am a String!"
+    let i = 0;
+    while (i < str.length) {
+        console.log(str[i]);
+    i++
+}
 
 // 4. Write a function that iterates through a provided string argument.
 
