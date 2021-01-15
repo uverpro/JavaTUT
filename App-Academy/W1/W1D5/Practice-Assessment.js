c99ce5953a8573ab5c11bd495611f9b2a945969c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-15 16:43:56 -0500

    Update Practice-Assessment.js

--- App-Academy/W1/W1D5/Practice-Assessment.js
+++ App-Academy/W1/W1D5/Practice-Assessment.js
@@ -98,3 +98,40 @@ if (mostExpensiveGroceries([groceriesA, groceriesD]) === 0) score++;
 if (mostExpensiveGroceries([groceriesA, groceriesD, groceriesC]) === 2) score++;
 
 console.log("You have scored " + score + "/7 points.");
+
+//====
+
+function stringChanger(word, operation) {
+  let nuWord = "";
+  if (operation === "capitalize")
+      nuWord += word[0].toUpperCase() + word.slice(1);
+  if (operation === "uppercase")
+      nuWord += word.toUpperCase();
+  if (operation === "double")
+      nuWord += word + word;
+  if (operation === "reverse")
+      nuWord += word.split("").reverse().join("");
+  if (operation === "unknown")
+      nuWord += word;
+  return nuWord;
+}
+
+console.log(stringChanger("foo", "capitalize")); // Foo
+console.log(stringChanger("foo", "uppercase")); // FOO
+console.log(stringChanger("foo", "double")); // foofoo
+console.log(stringChanger("foo", "reverse")); // oof
+console.log(stringChanger("foo", "unknown")); // foo
+
+// TESTS
+// DO NOT MODIFY ANYTHING BELOW THIS LINE
+
+let score = 0;
+
+if (stringChanger("foo", "capitalize") === "Foo") score++;
+if (stringChanger("foo", "uppercase") === "FOO") score++;
+if (stringChanger("foo", "double") === "foofoo") score++;
+if (stringChanger("foo", "reverse") === "oof") score++;
+
+if (stringChanger("foo", "unknown") === "foo") score++;
+
+console.log("You have scored " + score + "/5 points.");
