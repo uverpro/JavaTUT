80dad7d04a9cbc582bddad6a00076ddae2ff85c9
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-15 13:05:42 -0500

    Create Most-Expensive-Groceries.js

---
+++ App-Academy/W1/W1D5/Most-Expensive-Groceries.js
@@ -0,0 +1,57 @@
+function costOfGroceries(nums) {
+    let total = 0;
+    for (let i = 0; i < nums.length; i++) {
+        let item = nums[i];
+        if (item === "butter")
+          total += 1;
+        if (item === "eggs")
+          total += 2;
+        if (item === "milk")
+          total += 3;
+        if (item === "bread")
+          total += 4;
+        if (item === "cheese")
+          total += 5;
+    }
+    return total
+  };
+
+  function mostExpensiveGroceries(numsList) {
+    let highestCost = 0;
+    for (i = 0; i < numsList.length; i++) {
+        costOfGroceries() {
+          if (nums > highestCost)
+            highestCost += nums
+        }
+      }
+      return highestCost
+  }
+
+
+// TESTS
+// DO NOT MODIFY ANYTHING BELOW THIS LINE
+
+const groceriesA = ['cheese', 'butter', 'eggs'];
+const groceriesB = ['eggs', 'milk', 'bread', 'bread'];
+const groceriesC = ['cheese', 'bread'];
+const groceriesD = ['eggs', 'butter'];
+
+costOfGroceries(groceriesA);  // 8
+costOfGroceries(groceriesB);  // 13
+costOfGroceries(groceriesC);  // 9
+costOfGroceries(groceriesD);  // 3
+
+mostExpensiveGroceries([groceriesA, groceriesB, groceriesC, groceriesD]);
+
+let score = 0;
+
+if (costOfGroceries(groceriesA) === 8) score++;
+if (costOfGroceries(groceriesB) === 13) score++;
+if (costOfGroceries(groceriesC) === 9) score++;
+if (costOfGroceries(groceriesD) === 3) score++;
+
+if (mostExpensiveGroceries([groceriesA, groceriesB, groceriesC, groceriesD]) === 1) score++;
+if (mostExpensiveGroceries([groceriesA, groceriesD]) === 0) score++;
+if (mostExpensiveGroceries([groceriesA, groceriesD, groceriesC]) === 2) score++;
+
+console.log("You have scored " + score + "/7 points.");
