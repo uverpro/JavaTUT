fe165824601be1bde38e4094953ec542b6fd7a19
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-15 12:32:33 -0500

    Update Practice-Assessment.js

--- App-Academy/W1/W1D5/Practice-Assessment.js
+++ App-Academy/W1/W1D5/Practice-Assessment.js
@@ -45,22 +45,19 @@ function costOfGroceries(nums) {
     let total = 0;
     for (let i = 0; i < nums.length; i++) {
         let item = nums[i];
-        if (item = "butter")
-            total += 1;
-        if (item = "eggs")
-            total += 2;
-        if (item = "milk")
-            total += 3;
-        if (item = "bread")
-            total += 4;
-        if (item = "cheese")
-            total += 5;
+        if (item === "butter")
+          total += 1;
+        if (item === "eggs")
+          total += 2;
+        if (item === "milk")
+          total += 3;
+        if (item === "bread")
+          total += 4;
+        if (item === "cheese")
+          total += 5;
     }
     return total
-};
-
-const groceriesA = ['cheese', 'butter', 'eggs'];
-console.log(costOfGroceries(groceriesA));  // 8
+  };
 
 function mostExpensiveGroceries(numsList) {
     // Your code here
