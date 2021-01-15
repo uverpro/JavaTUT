1db08e04705326117ceb9535096d6d71e543f7a2
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-15 12:32:36 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,7 +1,20 @@
 function costOfGroceries(nums) {
-  for (let i = 0; i < nums.length; i++)
-    console.log(nums[i]);
+  let total = 0;
+  for (let i = 0; i < nums.length; i++) {
+      let item = nums[i];
+      if (item === "butter")
+        total += 1;
+      if (item === "eggs")
+        total += 2;
+      if (item === "milk")
+        total += 3;
+      if (item === "bread")
+        total += 4;
+      if (item === "cheese")
+        total += 5;
+  }
+  return total
 };
 
 const groceriesA = ['cheese', 'butter', 'eggs'];
-(costOfGroceries(groceriesA));
+console.log(costOfGroceries(groceriesA));  // 8
