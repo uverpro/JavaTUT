288bd42f1bde6fa4700f308cbfe415cf4fb1e152
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-15 17:27:52 -0500

    Update Three-or-Seven.js

---
+++ App-Academy/W1/W1D2/Three-or-Seven.js
@@ -0,0 +1,6 @@
+// Three or Seven
+
+// Write a function threeOrSeven that takes in a number
+// and returns true if the number is divisible by either 3 or 7 and false otherwise.
+
+//Write two versions of this function, using conditionals and without using conditionals.
