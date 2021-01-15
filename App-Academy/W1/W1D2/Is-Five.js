4997690cf9b874f32f1e0b411b67785cf3fad4c0
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-15 00:10:51 -0500

    Update Is-Five.js

--- App-Academy/W1/W1D2/Is-Five.js
+++ App-Academy/W1/W1D2/Is-Five.js
@@ -1,11 +1,12 @@
 // Define a function isFive that will return true if a number is equal to 5 and false if it is not.
 
-function isFive(num) {
+function isFive (num) {
     if (num === 5) {
     return true
-    } else {
+    } // else { //   // we don't have to wrap this in an `else` because if the above code did *not*
+                    // run then the number can't be 5 so we can just return false!
         return false
-    }
+    //}
 };
 
 console.log(isFive(5)); // => true
