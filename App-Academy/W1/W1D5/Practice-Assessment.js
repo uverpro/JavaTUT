78087aa5fa39542823316dbfcdd7e9584b6d82e5
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-15 11:47:25 -0500

    Update Practice-Assessment.js

--- App-Academy/W1/W1D5/Practice-Assessment.js
+++ App-Academy/W1/W1D5/Practice-Assessment.js
@@ -19,3 +19,59 @@
 // Divides a string into two
 // Removes the first character from a string
 // Extracts a section of a string and returns a new string ==> TRUE
+
+
+
+// MOST EXPENSIVE GROCERIES
+
+// Compile a price checker for a grocery store. The grocery prices are as follows:
+// butter: $1, eggs: $2, milk: $3, bread: $4, cheese: $5
+// 1. Create a function called costOfGroceries(grocieries) which takes a single array of grocery items
+// and returns the total cost.
+// 2. Write a function mostExpensiveGroceries(groceriesList) that takes in a 2-dimensional array of grocery items
+// and returns the index of the sub-array with the highest cost.
+
+// butter: $1
+// eggs: $2
+// milk: $3
+// bread: $4
+// cheese: $5
+
+
+function costOfGroceries(nums) {
+    // Your code here
+}
+
+
+function mostExpensiveGroceries(numsList) {
+    // Your code here
+}
+
+
+// TESTS
+// DO NOT MODIFY ANYTHING BELOW THIS LINE
+
+const groceriesA = ['cheese', 'butter', 'eggs'];
+const groceriesB = ['eggs', 'milk', 'bread', 'bread'];
+const groceriesC = ['cheese', 'bread'];
+const groceriesD = ['eggs', 'butter'];
+
+costOfGroceries(groceriesA);  // 8
+costOfGroceries(groceriesB);  // 13
+costOfGroceries(groceriesC);  // 9
+costOfGroceries(groceriesD);  // 3
+
+mostExpensiveGroceries([groceriesA, groceriesB, groceriesC, groceriesD]);
+
+let score = 0;
+
+if (costOfGroceries(groceriesA) === 8) score++;
+if (costOfGroceries(groceriesB) === 13) score++;
+if (costOfGroceries(groceriesC) === 9) score++;
+if (costOfGroceries(groceriesD) === 3) score++;
+
+if (mostExpensiveGroceries([groceriesA, groceriesB, groceriesC, groceriesD]) === 1) score++;
+if (mostExpensiveGroceries([groceriesA, groceriesD]) === 0) score++;
+if (mostExpensiveGroceries([groceriesA, groceriesD, groceriesC]) === 2) score++;
+
+console.log("You have scored " + score + "/7 points.");
