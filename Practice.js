df78eca3bb71d90d37810917baa521e3b25db465
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-15 13:01:48 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,4 +1,4 @@
-function costOfGroceries(nums) {
+let costOfGroceries = function (nums) {
   let total = 0;
   for (let i = 0; i < nums.length; i++) {
       let item = nums[i];
@@ -16,5 +16,46 @@ function costOfGroceries(nums) {
   return total
 };
 
+let mostExpensiveGroceries = function(numsList) {
+  let highestCost = 0;
+  for (i = 0; i < numsList.length; i++) {
+        if (costOfGroceries(nums) > highestCost)
+          highestCost += nums
+    }
+  }
+    return highestCost
+};
+
+
+// TESTS
+// DO NOT MODIFY ANYTHING BELOW THIS LINE
+
 const groceriesA = ['cheese', 'butter', 'eggs'];
-console.log(costOfGroceries(groceriesA));  // 8
+const groceriesB = ['eggs', 'milk', 'bread', 'bread'];
+const groceriesC = ['cheese', 'bread'];
+const groceriesD = ['eggs', 'butter'];
+
+costOfGroceries(groceriesA);  // 8
+costOfGroceries(groceriesB);  // 13
+costOfGroceries(groceriesC);  // 9
+costOfGroceries(groceriesD);  // 3
+
+console.log(mostExpensiveGroceries([groceriesA, groceriesB, groceriesC, groceriesD]));
+
+// function stringChanger(word, operation) {
+//   // Your code here
+// }
+
+// // TESTS
+// // DO NOT MODIFY ANYTHING BELOW THIS LINE
+
+// let score = 0;
+
+// if (stringChanger("foo", "capitalize") === "Foo") score++;
+// if (stringChanger("foo", "uppercase") === "FOO") score++;
+// if (stringChanger("foo", "double") === "foofoo") score++;
+// if (stringChanger("foo", "reverse") === "oof") score++;
+
+// if (stringChanger("foo", "unknown") === "foo") score++;
+
+// console.log("You have scored " + score + "/5 points.");
