c8e6dc1ab76eb1ea1bfa670c12dbb21f9450b5d0
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-15 11:31:38 -0500

    Update Practice-Assessment.js

---
+++ App-Academy/W1/W1D5/Practice-Assessment.js
@@ -0,0 +1,14 @@
+// Quiz => Problems I had trouble with
+
+// let num = 100;
+// let isBig = num >= 100;
+// let isSmall = num < 100;
+// console.log(isBig || isSmall);
+// TRUE
+
+
+// Which of the following is NOT true?
+// !(!A && !B) === A || B
+// A && B === !A || !B    // NOT TRUE
+// !A && !B === !(A || B)
+// !(A && B) === !A || !B
