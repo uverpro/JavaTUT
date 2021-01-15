c1a756f90c32d7fa902b40d18419f8afdc9deca4
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-15 10:21:12 -0500

    Create Array.includes.js

---
+++ Methods/Arrays-Methods/Array.includes.js
@@ -0,0 +1,17 @@
+// Array.includes()
+
+// The includes() method determines whether an array includes a certain value among its entries,
+// returning true or false as appropriate.
+
+const array1 = [1, 2, 3];
+
+console.log(array1.includes(2));
+// expected output: true
+
+const pets = ['cat', 'dog', 'bat'];
+
+console.log(pets.includes('cat'));
+// expected output: true
+
+console.log(pets.includes('at'));
+// expected output: false
