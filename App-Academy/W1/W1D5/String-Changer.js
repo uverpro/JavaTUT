a251b7a9ae9981492ac315e43c793bbbe07c5601
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-15 13:48:03 -0500

    Update String-Changer.js

--- App-Academy/W1/W1D5/String-Changer.js
+++ App-Academy/W1/W1D5/String-Changer.js
@@ -1,14 +1,18 @@
 function stringChanger(word, operation) {
     let nuWord = "";
     if (operation === "capitalize")
-        nuWord += word.toUpperCase();
+        nuWord += word[0].toUpperCase() + word.slice(1);
     if (operation === "uppercase")
         nuWord += word.toUpperCase();
+    if (operation === "reverse")
+        nuWord += word.split("").reverse().join("")
     return nuWord;
 }
 
 console.log(stringChanger("foo", "capitalize")); // Foo
 console.log(stringChanger("foo", "uppercase")); // FOO
+console.log(stringChanger("foo", "double")); // foofoo
+console.log(stringChanger("foo", "reverse")); // oof
 // TESTS
 // DO NOT MODIFY ANYTHING BELOW THIS LINE
 
