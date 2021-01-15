91b5ea0d3dc9cbd2a2ef46e2389125031518bdfc
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-15 11:26:41 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,11 +1,1 @@
-let bigNumber = function () {
-  console.log("FUCK BUTT POINT");
-};
-
-function numberSeparator(number) {
-  if (number <= 100) {
-    smallNumber();
-  } else {
-    bigNumber();
-  }
-}
+console.log(1 == "1.0");
