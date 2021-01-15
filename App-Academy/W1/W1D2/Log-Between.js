6e3f36ae2ab2ed704278f89b49ae232bcc897785
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-15 11:19:38 -0500

    Create Log-Between.js

---
+++ App-Academy/W1/W1D2/Log-Between.js
@@ -0,0 +1,23 @@
+// Define a function logBetween(lowNum, highNum) that will print every number from lowNum to highNum, inclusive.
+// Inclusive means that the range includes lowNum and highNum.
+
+// This function only needs to print using console.log it does not need to return.
+
+function logBetween (lowNum, highNum) {
+    for (i = lowNum; i <= highNum; i++)
+    console.log(i) // console.log(i) not console.log([i])
+};
+
+logBetween(-1, 2); // prints out: -1, 0, 1, 2
+
+logBetween(14, 6); // => prints nothing
+
+logBetween(4, 6); // prints out: 4, 5, 6
+
+function logBetween (lowNum, highNum) {
+    let i = lowNum;
+    while (i <= highNum) {
+        console.log(i);
+        i++;
+    }
+};
