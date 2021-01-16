37ace40aebc2e84a0d0771cbdf191763b549b4a8
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-16 14:01:39 -0500

    Update String-Included.js

--- App-Academy/W1/W1D2/String-Included.js
+++ App-Academy/W1/W1D2/String-Included.js
@@ -4,3 +4,12 @@
 // The eitherStringIncluded function should return true if either word1 or word2 is found in the sentence,
 // and false if neither is found.
 // Hint: the String.includes method will return true or false if a given string is included in another string.
+
+function eitherStringIncluded(sentence, word1, word2) {
+
+};
+
+console.log(eitherStringIncluded("how now brown cow?", "panther", "cow")); //=> true
+console.log(eitherStringIncluded("Question?", "Answer", "?")); //=> true
+console.log(eitherStringIncluded("I love programming", "apple", "potato")); //=> false
+console.log(eitherStringIncluded("Dance party!", "?", "what")); //=> false
