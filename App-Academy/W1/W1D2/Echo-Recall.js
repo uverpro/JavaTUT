10f46ef6c530cdf4b01ca343da3413cd828115f0
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-16 13:52:16 -0500

    Update Echo-Recall.js

--- App-Academy/W1/W1D2/Echo-Recall.js
+++ App-Academy/W1/W1D2/Echo-Recall.js
@@ -1,6 +1,6 @@
 function echo (string) {
-    // your code here...
-  }
+    console.log(`${string.toUpperCase()} ... ${string} ... ${string.toLowerCase()}`);
+  };
 
   echo("Mom!"); // => prints "MOM! ... Mom! ... mom!"
   echo("hey"); // => prints "HEY ... hey ... hey"
