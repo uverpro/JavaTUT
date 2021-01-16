ab498598444a59f1db9d718694161398e9934819
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-16 13:49:16 -0500

    Create Echo-Recall.js

---
+++ App-Academy/W1/W1D2/Echo-Recall.js
@@ -0,0 +1,7 @@
+function echo (string) {
+    // your code here...
+  }
+
+  echo("Mom!"); // => prints "MOM! ... Mom! ... mom!"
+  echo("hey"); // => prints "HEY ... hey ... hey"
+  echo("JUMp"); // => prints "JUMP ... JUMp ... jump"
