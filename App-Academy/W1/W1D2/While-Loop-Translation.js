76351555f459191731519cc74f53a66f43a91c32
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-16 18:26:36 -0500

    Create While-Loop-Translation.js

---
+++ App-Academy/W1/W1D2/While-Loop-Translation.js
@@ -0,0 +1,6 @@
+// While Loop Translation (*)
+// The following function is named eCounter(word).
+// The function takes in a string word and returns the number of e's in the word.
+// The function counts both lowercase (e) and uppercase (E).
+// Your job is to translate the following function to use a while loop instead of a for loop!
+// If you forget the syntax for a while loop go back and check out the reading.
