867e4518b9d537f01a9802502d5c5ef4c9479107
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-16 14:00:24 -0500

    Create String-Included.js

---
+++ App-Academy/W1/W1D2/String-Included.js
@@ -0,0 +1,6 @@
+// String Included (*)
+// Write a function named eitherStringIncluded(sentence, word1, word2)
+// that accepts a sentence and two words as arguments.
+// The eitherStringIncluded function should return true if either word1 or word2 is found in the sentence,
+// and false if neither is found.
+// Hint: the String.includes method will return true or false if a given string is included in another string.
