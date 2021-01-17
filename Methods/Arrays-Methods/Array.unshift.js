ff463676530c94c4bd24f788f4cd4e74b30bd0a5
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-17 18:26:15 -0500

    Update Array.unshift.js

--- Methods/Arrays-Methods/Array.unshift.js
+++ Methods/Arrays-Methods/Array.unshift.js
@@ -3,10 +3,22 @@
 // The unshift() method adds one or more elements to the beginning of an array
 // and returns the new length of the array.
 
-const array1 = [1, 2, 3];
+const array = [1, 2, 3];
 
-console.log(array1.unshift(4, 5));
+console.log(array.unshift(4, 5));
 // expected output: 5
 
-console.log(array1);
+console.log(array);
 // expected output: Array [4, 5, 1, 2, 3]
+
+
+
+let array1 = ["shift", "pop"];
+array1.shift()
+console.log(array1); // [ 'pop' ]
+array1.unshift("unshift");
+console.log(array1); // [ 'unshift', 'pop' ]
+array1.pop();
+console.log(array1); // [ 'unshift' ]
+array1.push("push");
+console.log(array1); // [ 'unshift', 'push' ]
