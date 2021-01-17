32bbc660a7069730a0adeaa5deccf54aceb6906d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-17 18:26:08 -0500

    Update Array.shift.js

--- Methods/Arrays-Methods/Array.shift.js
+++ Methods/Arrays-Methods/Array.shift.js
@@ -3,12 +3,24 @@
 // The shift() method removes the first element from an array and returns that removed element.
 // This method changes the length of the array.
 
-const array1 = [1, 2, 3];
+const array = [1, 2, 3];
 
-const firstElement = array1.shift();
+const firstElement = array.shift();
 
-console.log(array1);
+console.log(array);
 // expected output: Array [2, 3]
 
 console.log(firstElement);
 // expected output: 1
+
+
+
+let array1 = ["shift", "pop"];
+array1.shift()
+console.log(array1); // [ 'pop' ]
+array1.unshift("unshift");
+console.log(array1); // [ 'unshift', 'pop' ]
+array1.pop();
+console.log(array1); // [ 'unshift' ]
+array1.push("push");
+console.log(array1); // [ 'unshift', 'push' ]
