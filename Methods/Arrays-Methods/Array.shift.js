5629a3d5b5e3aa3930b9ac83b085d2ebe842fe39
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-17 17:24:13 -0500

    Create Array.shift.js

---
+++ Methods/Arrays-Methods/Array.shift.js
@@ -0,0 +1,14 @@
+// Array.shift()
+
+// The shift() method removes the first element from an array and returns that removed element.
+// This method changes the length of the array.
+
+const array1 = [1, 2, 3];
+
+const firstElement = array1.shift();
+
+console.log(array1);
+// expected output: Array [2, 3]
+
+console.log(firstElement);
+// expected output: 1
