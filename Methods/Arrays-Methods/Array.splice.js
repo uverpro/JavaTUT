f785d7325aeb30fcd24b9a4673b1f1d639bde705
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-17 18:26:11 -0500

    Update Array.splice.js

--- Methods/Arrays-Methods/Array.splice.js
+++ Methods/Arrays-Methods/Array.splice.js
@@ -11,3 +11,17 @@ months.splice(4, 1, 'May');
 // replaces 1 element at index 4
 console.log(months);
 // expected output: Array ["Jan", "Feb", "March", "April", "May"]
+
+
+
+let array2 = ["True", "Blue", "Vogue", "Borderline"];
+array2.splice(2, 0, "Lucky Star")
+console.log(array2); // ["True", "Blue", "Lucky Star", "Vogue", "Borderline"]
+array2.splice(2, 3);
+console.log(array2); // [ 'True', 'Blue' ]
+array2.splice(2, 0, "Wrap", "You", "Up");
+console.log(array2); // [ 'True', 'Blue', 'Wrap', 'You', 'Up' ]
+array2.splice(5, 0, "Borderline")
+console.log(array2); // [ 'True', 'Blue', 'Wrap', 'You', 'Up', 'Borderline' ]
+array2.splice(2, 3, "Holiday");
+console.log(array2); // [ 'True', 'Blue', 'Holiday', 'Borderline' ]
