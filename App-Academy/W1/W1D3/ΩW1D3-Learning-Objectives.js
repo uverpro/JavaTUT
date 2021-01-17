d0d5178674b8443c8db2bad351a0508c98077d9a
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-17 17:24:04 -0500

    Create ΩW1D3-Learning-Objectives.js

---
+++ App-Academy/W1/W1D3/ΩW1D3-Learning-Objectives.js
@@ -0,0 +1,18 @@
+// Intermediate Functions Lesson Learning Objectives
+// Below is a complete list of the terminal learning objectives across all "Intermediate Function" lessons.
+// When you complete these lessons, you should be able to perform each of the following objectives.
+// These objectives capture how you may be evaluated on the assessment for these lessons.
+
+// 1. Identify that strings are immutable and arrays are mutable
+
+// 2. Define a function using both function declaration and function expression syntax
+// let score = function (num); function score (num)
+
+// 1. Utilize Array.push, .pop, .shift, .unshift to mutate an array
+
+// List the arguments that can be used with Array.splice
+// Write a function that sums up elements of an array, given an array of numbers as an argument
+// Define a function that takes in an array of numbers and returns a new array containing only the primes
+// Define a function that takes in a 2D array of numbers and returns the total sum of all elements in the array
+// Define a function that takes in an array of elements and returns a 2d array where the subarrays represent unique pairs of elements
+// Define a function that takes in an array of numbers as an argument and returns the smallest value in the array; if the array is empty return null
