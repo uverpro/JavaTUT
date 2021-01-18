fa6f95e5c5f5fc5f8e1fa3fafc6dc8692f5feb18
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 12:37:28 -0500

    Create Log-Between-Stepper-Recall.js

---
+++ App-Academy/W1/W1D3/Log-Between-Stepper-Recall.js
@@ -0,0 +1,9 @@
+// Log Between Stepper Recall
+// Write a function logBetweenStepper(min, max, step) that takes in 3 numbers as parameters.
+// The function should print out numbers between min and max at step intervals.
+
+// Hint: this function only needs to print using console.log it does not need to return.
+
+let logBetweenStepper = function (min, max, step) {
+
+};
