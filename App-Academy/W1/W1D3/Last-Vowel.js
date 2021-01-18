ce44b14b9694573a662646ca24130a9abe07ae4c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 17:44:00 -0500

    Create Last-Vowel.js

---
+++ App-Academy/W1/W1D3/Last-Vowel.js
@@ -0,0 +1,6 @@
+// Last Vowel
+// Write a function lastVowel(str) that takes in a string
+// and returns the last vowel that appears sequentially in the string.
+// Note that the string may contain capitalization.
+
+// Hint: You may find the String.toLowerCase or String.toUpperCase methods useful
