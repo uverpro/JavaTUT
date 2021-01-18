d29f396eb88e601123d191114d05aca98b8edbbf
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 12:23:16 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,16 +1,3 @@
-function removeOranges(fruitArray) {
-
-    for (let i = 0 ; i < fruitArray.length ; i++) {
-
-      // If the item at the ith position is "orange"
-      if (fruitArray[i] == "orange") {
-        // Remove 1 item from the ith position
-        fruitArray.splice(i, 1);
-      }
-
-    }
-    return fruitArray;
-  }
-
-  console.log(removeOranges(["apple", "banana", "orange", "peach", "orange", "pear"])
-["apple", "banana", "peach", "pear"]);
+let arr = ['a', 'b', 'c', 'd', 'e', 'f'];
+arr.splice(2, 3);
+console.log(arr);
