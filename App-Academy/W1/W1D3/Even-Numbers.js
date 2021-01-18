5be20457a62310788f6aa135c98bea45fa3bc4f7
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 17:32:13 -0500

    Create Even-Numbers.js

---
+++ App-Academy/W1/W1D3/Even-Numbers.js
@@ -0,0 +1,13 @@
+// Even Numbers
+// Write a function evenNumbers(max) that takes in a number as an arg.
+// The function should return an array containing all positive, even numbers that are less than max.
+
+// Define this function using function expression syntax.
+
+let evenNumbers = function (max) {
+
+};
+
+console.log(evenNumbers(7)); // [ 2, 4, 6 ]
+console.log(evenNumbers(12)); // [ 2, 4, 6, 8, 10 ]
+console.log(evenNumbers(15)); // [ 2, 4, 6, 8, 10, 12, 14 ]
