3c24bacc65105290332b35f30ff5498249385aa6
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 18:51:00 -0500

    Create Remove-Last-Vowel.js

---
+++ App-Academy/W1/W1D3/Remove-Last-Vowel.js
@@ -0,0 +1,7 @@
+// Remove Last Vowel
+// Write a function removeLastVowel(word) that takes in a string
+// and returns the string with its last vowel removed.
+
+let removeLastVowel = function (word) {
+
+};
