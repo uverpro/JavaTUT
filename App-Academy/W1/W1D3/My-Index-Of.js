061ed6bc88c877ca2e03e0f4f3f03a05e97652bc
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 14:30:15 -0500

    Create My-Index-Of.js

---
+++ App-Academy/W1/W1D3/My-Index-Of.js
@@ -0,0 +1,4 @@
+// My Index Of
+// Write a function myIndexOf(arr, target) that takes in an array and target value as args.
+// The function should return the first index where the target is found in the array.
+// If the target is not found, it should return -1. Solve this without using Array.indexOf.
