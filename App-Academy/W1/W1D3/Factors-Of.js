7e82a90caa97ae75bedaced150ecace70a208738
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 14:03:57 -0500

    Create Factors-Of.js

---
+++ App-Academy/W1/W1D3/Factors-Of.js
@@ -0,0 +1,10 @@
+// Factors Of
+// Write a function factorsOf(num) that takes in a number as an arg.
+// The method should return an array containing all positive numbers
+// that are able to divide into num with no remainder.
+
+// Define this function using function expression syntax.
+
+let factorsOf = function (num) {
+
+};
