f6e4d46d34749f22e4be09fda1b61d4175174a04
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 17:43:57 -0500

    Create Is-Prime-Recall.js

---
+++ App-Academy/W1/W1D3/Is-Prime-Recall.js
@@ -0,0 +1,21 @@
+// Is Prime Recall
+// Define a function isPrime(number) that returns true if number is prime. Otherwise, false.
+// A number is prime if it is only divisible by 1 and itself.
+
+let isPrime = function (number) {
+    if (number < 2) {
+        return false;
+    } else {
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0)
+        return false;
+        }
+    }
+    return true;
+};
+
+console.log(isPrime(2)); // => true
+console.log(isPrime(10)); // => false
+console.log(isPrime(11)); // => true
+console.log(isPrime(9)); // => false
+console.log(isPrime(2017)); // => true
