639f1564cf1388e694eb0fe41ee97759645f4254
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 12:42:48 -0500

    Update Log-Between-Stepper-Recall.js

--- App-Academy/W1/W1D3/Log-Between-Stepper-Recall.js
+++ App-Academy/W1/W1D3/Log-Between-Stepper-Recall.js
@@ -5,5 +5,12 @@
 // Hint: this function only needs to print using console.log it does not need to return.
 
 let logBetweenStepper = function (min, max, step) {
-
+    for (let i = min; i <= max; i += step) {
+        console.log(i);
+    }
 };
+
+
+
+logBetweenStepper(5, 9, 1); // 5, 6, 7, 8, 9
+logBetweenStepper(-10, 15, 5)  // -10, -5, 0, 5, 10, 15
