70b5cde237459d5df97a8d599374faf587bd225e
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 18:33:48 -0500

    Update Pit-Pat.js

--- App-Academy/W1/W1D3/Pit-Pat.js
+++ App-Academy/W1/W1D3/Pit-Pat.js
@@ -4,5 +4,15 @@
 // less than or equal to max that are divisible by either 4 or 6, but not both.
 
 let pitPat = function (max) {
-
+    let nuArray = [];
+    for (let i = 0; i <= max; i++) {
+        if (((i % 4 === 0) || (i % 6 === 0))
+            && !(i % 4 === 0 && i % 6 === 0)) {
+                nuArray.push(i);
+            }
+    }
+    return nuArray;
 };
+
+console.log(pitPat(18)); // [ 4, 6, 8, 16, 18 ]
+console.log(pitPat(30)); // [ 4, 6, 8, 16, 18, 20, 28, 30 ]
