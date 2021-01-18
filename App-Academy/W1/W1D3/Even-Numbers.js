f16fe2e508a216f13fe62d7e8e2ee684e2039b48
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 17:34:14 -0500

    Update Even-Numbers.js

--- App-Academy/W1/W1D3/Even-Numbers.js
+++ App-Academy/W1/W1D3/Even-Numbers.js
@@ -5,7 +5,13 @@
 // Define this function using function expression syntax.
 
 let evenNumbers = function (max) {
-
+    let evensArray = [];
+    for (let i = 2; i < max; i++) {
+        if (i % 2 === 0) {
+            evensArray.push(i);
+        }
+    }
+    return evensArray;
 };
 
 console.log(evenNumbers(7)); // [ 2, 4, 6 ]
