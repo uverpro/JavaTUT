fecfce7e21360c28659f4a5abf75205c3dc3941d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 01:08:55 -0500

    Update Longest-Name.js

--- App-Academy/W1/W1D3/Longest-Name.js
+++ App-Academy/W1/W1D3/Longest-Name.js
@@ -4,22 +4,42 @@
 // However, this implementation is full of bugs!
 // Use your debugging skills to find and fix those bugs.
 
+// What I wrote:
+// function longestName(names) {
+//     let longestName = "";
+//     for (i = 0; i < names.length; i++)
+//     // console.log(names[i].length) // will return the individual strings
+//     if (names[i].length > longestName.length) {
+//         longestName = names[i];
+//     }
+//     return longestName
+// };
+
+// testNames = ["James", "Patricia", "Michael", "Elizabeth", "Christopher",
+//              "Sarah", "Margaret", "Kenneth", "Stephanie", "Jonathan",
+//              "Jeremy", "Samantha", "Alexander", "Catherine", "Benjamin"]
+
+// // longestName(testNames);
+// console.log(longestName(testNames)); // "Christopher"
+
+
+// The exercise:
 function longestName(names) {
-
+    console.log("Start")
     // Set the first name to be the longest
-    set currentLongest = names[1];
-
+    let currentLongest = names[1];
+    console.log(names.length)
     // Check each other name in the array starting from the second
-    for (let i = 2 ; i <= names.Length ; i++) {
-
+    for (let i = 2 ; i < names.length ; i++) {
+        console.log(names[i])
         // If the name we're checking is longer than our
         // current longest, set that name to be the new longest
-        if (names[i].Length > currentLongest.Length) {
+        if (names[i].length > currentLongest.length) {
             currentLongest = names[i];
         }
 
     }
-
+    console.log("End")
     return currentLongest;
 }
 
@@ -28,28 +48,3 @@ testNames = ["James", "Patricia", "Michael", "Elizabeth", "Christopher",
              "Jeremy", "Samantha", "Alexander", "Catherine", "Benjamin"]
 
 console.log(longestName(testNames)); // "Christopher"
-
-// function longestName(names) {
-
-//     // Set the first name to be the longest
-//     set currentLongest = names[1];
-
-//     // Check each other name in the array starting from the second
-//     for (let i = 2 ; i <= names.Length ; i++) {
-
-//         // If the name we're checking is longer than our
-//         // current longest, set that name to be the new longest
-//         if (names[i].Length > currentLongest.Length) {
-//             currentLongest = names[i];
-//         }
-
-//     }
-
-//     return currentLongest;
-// }
-
-// testNames = ["James", "Patricia", "Michael", "Elizabeth", "Christopher",
-//              "Sarah", "Margaret", "Kenneth", "Stephanie", "Jonathan",
-//              "Jeremy", "Samantha", "Alexander", "Catherine", "Benjamin"]
-
-// console.log(longestName(testNames)); // "Christopher"
