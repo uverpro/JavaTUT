ba5763005ca7087f71e00d6e5018991738336169
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 12:57:06 -0500

    Create My-Includes.js

---
+++ App-Academy/W1/W1D3/My-Includes.js
@@ -0,0 +1,12 @@
+// My Includes
+// Write a function myIncludes(arr, target) that accepts an array and an target value as args.
+// The function should return a boolean indicating whether the target is found in the array.
+// Solve this without Array.includes or Array.indexOf.
+
+
+
+console.log(myIncludes(['a', 'b', 'c', 'e'], 'c')); // true
+console.log(myIncludes(['a', 'b', 'c', 'e'], 'a')); // true
+console.log(myIncludes(['a', 'b', 'c', 'e'], 'z')); // false
+console.log(myIncludes([43, -7, 11, 13], 11)); // true
+console.log(myIncludes([43, -7, 11, 13], 1)); // false
