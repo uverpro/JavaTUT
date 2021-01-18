77f08c0d9b1d22281766d5339ab9b2aaf928fe66
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 18:06:01 -0500

    Update Last-Vowel.js

--- App-Academy/W1/W1D3/Last-Vowel.js
+++ App-Academy/W1/W1D3/Last-Vowel.js
@@ -6,11 +6,31 @@
 // Hint: You may find the String.toLowerCase or String.toUpperCase methods useful
 
 let lastVowel = function (str) {
-    let bigstr
+    let upperCaseVowels = "AEIOU";
+    let lowerCaseVowels = "aeiou";
+    for (let i = str.length - 1; i >= 0; i--) {
+        if ((upperCaseVowels.includes(str[i])) || (lowerCaseVowels.includes(str[i]))) { // Not necessary
+            return str[i];
+        }
+    }
+    return null;
 };
 
 console.log(lastVowel('battery')); // 'e'
 console.log(lastVowel('TUNNEL')); // 'E'
 console.log(lastVowel('dog')); // 'o'
 console.log(lastVowel('conventional')); // 'a'
-console.log(lastVowel('rhythm')); // nul
+console.log(lastVowel('rhythm')); // null
+
+let lastVowel = function(str) {
+    let vowels = 'aeiou';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        let char = str[i];
+        if (vowels.includes(char.toLowerCase())) {
+            return char;
+        }
+    }
+
+    return null;
+};
