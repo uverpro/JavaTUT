a115c2c4ee9d334a44572cf2a026e9b247fe1625
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 12:17:17 -0500

    Update Array.slice.js

--- Methods/Arrays-Methods/Array.slice.js
+++ Methods/Arrays-Methods/Array.slice.js
@@ -1,5 +1,7 @@
-// The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end
-// (end not included AKA end is exclusive) where start and end represent the index of items in that array.
+// The slice() method returns a shallow copy of a portion of an array
+// into a new array object selected from start to end
+// (end not included AKA end is exclusive) where start
+// and end represent the index of items in that array.
 // The original array will not be modified.
 
 const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
