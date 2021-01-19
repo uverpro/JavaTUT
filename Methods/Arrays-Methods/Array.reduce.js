0f7143c66b518655d69ccbad915d97c47788ee95
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-19 14:04:48 -0500

    Create Array.reduce.js

---
+++ Methods/Arrays-Methods/Array.reduce.js
@@ -0,0 +1,15 @@
+// Array.reduce()
+
+// The reduce() method executes a reducer function (that you provide) on each element of the array,
+// resulting in single output value.
+
+const array1 = [1, 2, 3, 4];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer));
+// expected output: 10
+
+// 5 + 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer, 5));
+// expected output: 15
