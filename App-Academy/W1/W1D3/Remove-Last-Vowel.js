564a0841a29dad42b95ba1e227009de573d6bfce
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-18 21:06:32 -0500

    Update Remove-Last-Vowel.js

--- App-Academy/W1/W1D3/Remove-Last-Vowel.js
+++ App-Academy/W1/W1D3/Remove-Last-Vowel.js
@@ -3,5 +3,17 @@
 // and returns the string with its last vowel removed.
 
 let removeLastVowel = function (word) {
-
+    let vowels = "aeiou";
+    for (let i = word.length - 1; i >= 0; i--) {
+        let char = word[i];
+        if (vowels.includes(char)) {
+            return word.slice(0, i) + word.slice(i + 1);
+        }
+    }
+    return word
 };
+
+console.log(removeLastVowel('bootcamp')); // 'bootcmp'
+console.log(removeLastVowel('better')); // 'bettr'
+console.log(removeLastVowel('graph')); // 'grph'
+console.log(removeLastVowel('thy')); // 'thy'
