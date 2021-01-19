d51c82136039fa5ce6e2c412918db761c24fd5cd
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-19 16:55:58 -0500

    Update W2D2-Notes.js

--- App-Academy/W2/W2D2/W2D2-Notes.js
+++ App-Academy/W2/W2D2/W2D2-Notes.js
@@ -3,29 +3,29 @@
 let parks = ['Zion', 'Yellowstone', 'Acadia', 'Yosemite'];
 
 // This is a classic for loop to return each element in the Array:
-// for (let i = 0; i < parks.length; i++) {
-    //     let park = parks[i];
-    //     console.log(park);
-    // }; <=== This code will print: Zion, Yellowstone, Acadia, Yosemite
+for (let i = 0; i < parks.length; i++) {
+        let park = parks[i];
+        console.log(park);
+    }; // <=== This code will print: Zion, Yellowstone, Acadia, Yosemite
 
 // write the "parks.forEach()"
 // then, assign forEach a function:
 // parks.forEach(function() {})
 // then assign an argument for the function
 
-// parks.forEach(function(park) {
-//     console.log(park);
-// });   // <== Will print: Zion, Yellowstone, Acadia, Yosemite
+parks.forEach(function(park) {
+    console.log(park);
+});   // <== Will print: Zion, Yellowstone, Acadia, Yosemite
 
 // You can assign more than 1 argument
 // Here we assign the index as the second argument
 // Printing the element (ele = park) and the index location of the element
 
-// parks.forEach(function(ele, i) {
-//     console.log(ele);
-//     console.log(i);
-//     console.log('---')
-// });       <== Will print Zion, 0, ---
+parks.forEach(function(ele, i) {
+    console.log(ele);
+    console.log(i);
+    console.log('---')
+});       // <== Will print Zion, 0, ---
 
 // There's a third argument you can input
 // The Array itself!
