909308f8f45e412e31c4a2c2f170c42cece39c13
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-20 13:45:30 -0500

    Update ☭Objects-Lecture.js

--- App-Academy/W2/W2D2/☭Objects-Lecture.js
+++ App-Academy/W2/W2D2/☭Objects-Lecture.js
@@ -1,1 +1,5 @@
-// The Object Type
+// The Object Type:
+
+/*  + An unordered collection of key-value pairs
+    + Affectionately known by the industry jargon:
+    + POJO: Plain Old Javascript Object */
