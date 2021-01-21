16f41465130bcc492dd4d4e1ae402b25da4f78c8
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-20 23:25:13 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,29 +1,7 @@
+let cat = "Ivan"
 
+let mood = "GRUMPY!"
 
-let noE = function (words) {
-  let val = words.indexOf("e");
-  // let val2 = word.indexOf("E");
-  if (val === -1) { // && val2 === -1)
-  return true;
-} else {
-  return false;
-  }
-};
+console.log("My cat " + cat + " is really, really " + mood)
 
-let removeEWords = function (sentence) {
-  const word = sentence.split(" ");
-//   let result = "";
-//   for (let i = 0; i < words.length; i++) {
-//     if (hasE(word) {
-//       // do nothing
-//   } else {
-//     result += word;
-//   }
-//   return result;
-};
-
-let result = words.filter(noE);
-console.log(result('What time is it everyone?'));
-
-console.log(removeEWords('What time is it everyone?')); // 'What is it'
-console.log(removeEWords('Enter the building')); // 'building'
+console.log(`My cat ${cat} is really really ${mood}`);
