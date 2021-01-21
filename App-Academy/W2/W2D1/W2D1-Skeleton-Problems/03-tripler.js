9cd64525e05ea9419359ccc6a09283440535b1dc
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-20 23:57:17 -0500

    Update 03-tripler.js

--- App-Academy/W2/W2D1/W2D1-Skeleton-Problems/03-tripler.js
+++ App-Academy/W2/W2D1/W2D1-Skeleton-Problems/03-tripler.js
@@ -1,11 +1,6 @@
 /*******************************************************************************
 Write a function tripler(array) that takes in an array and returns a new array
 containing 3 times every element of the original array.
-
-Examples:
-
-tripler([1,2,3]); // => [ 3, 6, 9 ]
-tripler([4, 1, 7]); // => [ 12, 3, 21 ]
 *******************************************************************************/
 
 function tripler (array) {
@@ -17,5 +12,5 @@ function tripler (array) {
   return nuArray;
 };
 
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
-module.exports = tripler;
+console.log(tripler([1,2,3])); // => [ 3, 6, 9 ]
+console.log(tripler([4, 1, 7])); // => [ 12, 3, 21 ]
