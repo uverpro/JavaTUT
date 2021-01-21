49124e01f459a10893537d853280b3f9e330c544
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-20 23:25:07 -0500

    Update 03-tripler.js

--- App-Academy/W2/W2D1/W2D1-Skeleton-Problems/03-tripler.js
+++ App-Academy/W2/W2D1/W2D1-Skeleton-Problems/03-tripler.js
@@ -8,12 +8,14 @@ tripler([1,2,3]); // => [ 3, 6, 9 ]
 tripler([4, 1, 7]); // => [ 12, 3, 21 ]
 *******************************************************************************/
 
-function tripler(array) {
-  tripled = array.map(function(num) {
-    return num * 3
-  });
-  return tripled;
-}
+function tripler (array) {
+  let nuArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i];
+    nuArray.push(array[i] * 3);
+  }
+  return nuArray;
+};
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
 module.exports = tripler;
