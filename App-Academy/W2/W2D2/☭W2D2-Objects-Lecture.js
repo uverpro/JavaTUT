80fcc53af6988aea81fa2ee1a4cbe1b3501cb1fd
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-21 13:21:38 -0500

    Create ☭W2D2-Objects-Lecture.js

---
+++ App-Academy/W2/W2D2/☭W2D2-Objects-Lecture.js
@@ -0,0 +1,14 @@
+// The Object Type:
+
+/*  + An unordered collection of key-value pairs
+    + Affectionately known by the industry jargon:
+    + POJO: Plain Old Javascript Object */
+
+
+// Things to know about Objects:
+
+/*  1. Used to store multiple key value pairs
+        + keys are strings and unique
+        + values can be anything (string, other objects, etc.)
+
+    2. ORDER IS NOT GUARANTEED    */
