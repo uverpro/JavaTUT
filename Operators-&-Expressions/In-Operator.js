991f93df6033ca7e365191d934a7cb2a2dd2be8a
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-20 22:21:28 -0500

    Create In-Operator.js

---
+++ Operators-&-Expressions/In-Operator.js
@@ -0,0 +1,18 @@
+// In Operator
+
+/* The in operator returns true if the specified property
+is in the specified object or its prototype chain. */
+
+const car = { make: 'Honda', model: 'Accord', year: 1998 };
+
+console.log('make' in car);
+// expected output: true
+
+delete car.make;
+if ('make' in car === false) {
+  car.make = 'Suzuki';
+}
+
+console.log(car.make);
+// expected output: "Suzuki"
+console.log(car);
