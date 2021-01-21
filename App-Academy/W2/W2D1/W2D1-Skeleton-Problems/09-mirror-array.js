87af788967c9c664a7d12da513346fef2213d431
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-21 00:00:36 -0500

    Update 09-mirror-array.js

--- App-Academy/W2/W2D1/W2D1-Skeleton-Problems/09-mirror-array.js
+++ App-Academy/W2/W2D1/W2D1-Skeleton-Problems/09-mirror-array.js
@@ -1,11 +1,6 @@
 /***********************************************************************
 Write a function `mirrorArray(array)` that takes in an array as an
 argument and returns a new array "mirrored" as shown in the examples:
-
-Examples:
-
-mirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
-mirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
 function mirrorArray(array) {
@@ -18,6 +13,3 @@ function mirrorArray(array) {
 }
 
 console.log(mirrorArray([1, 2, 3]));
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-module.exports = mirrorArray;
