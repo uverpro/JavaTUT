4fd9444dc2d6de3c483400a3edde7d50436e3a6c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-21 20:20:40 -0500

    Update make-acronym.js

--- Java-Beginner-Tutorial/7.Arrays/B_Arrays-Exercises/make-acronym.js
+++ Java-Beginner-Tutorial/7.Arrays/B_Arrays-Exercises/make-acronym.js
@@ -2,7 +2,15 @@
 // The function should return a string
 // containing the first character of each word in the sentence.
 
-
+let makeAcronym = function (sentence) {
+    newString = "";
+    let words = sentence.split(" "); // ["New", "York"]
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        newString += word[0];
+    }
+    return newString.toUpperCase();
+};
 
 console.log(makeAcronym("New York")); // NY
 console.log(makeAcronym("same stuff different day")); // SSDD
