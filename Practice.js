dddbcefdacb912360f08b9f9086b115263c0e147
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-22 11:42:35 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,16 +1,12 @@
-let maximum = function (numbers) {
-  if (numbers.length === 0) {
-      return null;
-  }
-      let currentMax = numbers[0];
-      for (let i = 1; i < numbers.length; i++) {
-          if (numbers[i] > currentMax) {
-              highestNumber + array[i];
-      }
-  }
-  return highestNumber;
-};
+let arr = [1, 1, 3, 2, 2, 1];
 
-console.log(maximum([5, 6, 3, 7])); // 7
-console.log(maximum([17, 15, 19, 11, 2])); // 19
-console.log(maximum([])); // null
+const result = arr.reduce(function(acc, value) {
+  if (acc[value]) {
+    acc[value]++;
+  } else {
+    acc[value] = 1;
+  }
+  return acc;
+}, {});
+
+console.log(result)
