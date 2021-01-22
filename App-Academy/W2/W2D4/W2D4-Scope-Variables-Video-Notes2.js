1a9cc96f935fade19debbb17ce96c31fda0a92ea
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-22 12:49:19 -0500

    Update W2D4-Scope-Variables-Video-Notes2.js

--- App-Academy/W2/W2D4/W2D4-Scope-Variables-Video-Notes2.js
+++ App-Academy/W2/W2D4/W2D4-Scope-Variables-Video-Notes2.js
@@ -2,16 +2,35 @@
 
 // LET
 // "let" is a block scope variable and can be reassigned
+// (can't redeclare a variable within the same scope but can reassign it)
 
 function blockScope () {
     let test = "upper scope";
+    // let test = "not upper scope" // ==> ERROR
+    // test = "not upper scope" // ==> "not upper scope"
     if (true) {
         let test = "lower scope";
-        // console.log(test); // ==> "lower scope"
+        console.log(test); // ==> "lower scope"
     }
     console.log(test); // ==> "upper scope"
 }
 
-
+blockScope();
 
 // CONST
+// "const" - is BLOCK SCOPED and can NOT be reassigned!
+
+function constBlockScope () {
+    const test = ["upper scope"];
+    // const = ["not upper scope"] // ==> ERROR
+    if (true) {
+        const test = ["lower scope"];
+        console.log(test); // ==> [ "upper scope" ]
+    }
+    console.log(test); // ==> [ "upper scope" ]
+};
+
+constBlockScope();
+
+// VAR
+// "var" is FUNCTION SCOPED and can be reassigned
