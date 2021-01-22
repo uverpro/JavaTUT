dd5bf5cf75a9a854f0833e529cc8946d4ac0984d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-21 22:14:25 -0500

    Update Array.reverse.js

--- Methods/Arrays-Methods/Array.reverse.js
+++ Methods/Arrays-Methods/Array.reverse.js
@@ -4,12 +4,12 @@
 
 const array1 = ['one', 'two', 'three'];
 console.log('array1:', array1);
-// expected output: "array1:" Array ["one", "two", "three"]
+// expected output: "array1: ["one", "two", "three"]"
 
 const reversed = array1.reverse();
 console.log('reversed:', reversed);
-// expected output: "reversed:" Array ["three", "two", "one"]
+// expected output: "reversed: ["three", "two", "one"]"
 
 // Careful: reverse is destructive -- it changes the original array.
 console.log('array1:', array1);
-// expected output: "array1:" Array ["three", "two", "one"]
+// expected output: "array1: ["three", "two", "one"]""
