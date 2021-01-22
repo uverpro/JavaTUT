7abfd2f64b53739994041e71b0d31fad10a2840a
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-22 11:48:57 -0500

    Create W2D4-Scope-Video-Notes.js

---
+++ App-Academy/W2/W2D4/W2D4-Scope-Video-Notes.js
@@ -0,0 +1,20 @@
+// SCOPE VIDEO
+/*
+
+The Scope of a program in JS is the set of variable
+that are avaliable for use within the program.
+
+
+DIFFERENT TYPES OF SCOPE IN JS:
+
+1. GLOBAL - the global space is Javascript
+Variables: declared variables in the global scope
+
+2. LOCAL or FUNCTION - created when a function is defined
+Variables: includes arguments, local variables, previously declared variables
+
+3. BLOCK SCOPE - created by entering a pair of curly braces
+Variables: denoted by the "{ }" includes previously declared variables.
+
+
+*/
