df86694bf149ba55b876b16f84bb2a7ef7e987e7
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-21 22:41:54 -0500

    Update Array.reduce.js

--- Methods/Arrays-Methods/Array.reduce.js
+++ Methods/Arrays-Methods/Array.reduce.js
@@ -1,6 +1,7 @@
 // Array.reduce()
 
-// The reduce() method executes a reducer function (that you provide) on each element of the array,
+// The reduce() method executes a reducer function
+// (that you provide) on each element of the array,
 // resulting in single output value.
 
 const array1 = [1, 2, 3, 4];
