64609b8ef71e4da831df48da5934b3a1c3abe361
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-21 20:06:47 -0500

    Create make-acronym.js

---
+++ Java-Beginner-Tutorial/7.Arrays/B_Arrays-Exercises/make-acronym.js
@@ -0,0 +1,10 @@
+// Write a function `makeAcronym` that accepts a sentence string as an argument.
+// The function should return a string
+// containing the first character of each word in the sentence.
+
+
+
+console.log(makeAcronym("New York")); // NY
+console.log(makeAcronym("same stuff different day")); // SSDD
+console.log(makeAcronym("Laugh out loud")); // LOL
+console.log(makeAcronym("don't over think stuff")); // DOTS
