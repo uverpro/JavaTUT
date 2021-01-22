21439135c532b3cb983541457db43b7765b06f25
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-21 19:38:43 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,7 +1,17 @@
-let cat = "Ivan"
+// Write a function `numOdds` that accepts an array of numbers as an argument.
+// The function should return a number representing the count of odd elements in the array.
 
-let mood = "GRUMPY!"
+let numOdds = function (array) {
+  sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i];
+    if (num % 2 !== 0) {
+      sum++;
+    }
+  }
+  return sum;
+};
 
-console.log("My cat " + cat + " is really, really " + mood)
-
-console.log(`My cat ${cat} is really really ${mood}`);
+console.log(numOdds([4, 7, 2, 5, 9])); // 3
+console.log(numOdds([11, 31, 58, 99, 21, 60])); // 4
+console.log(numOdds([100, 40, 4])); // 0
