7afc262d6b672fc8273cf5fb70056875ccd80082
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-21 19:09:06 -0500

    Create 01-My-For-Each.js

---
+++ App-Academy/W2/W2D3/W2D3-Tryout-Callback-Functions/01-My-For-Each.js
@@ -0,0 +1,36 @@
+/*******************************************************************************
+Write a function `myForEach` that accepts an array and a callback as arguments.
+The function should call the callback on each element of the array, passing in the
+element, index, and array itself. The function does not need to return any value.
+
+Do not use the built in Array#forEach.
+
+Examples:
+
+myForEach(['a', 'b', 'c'], function (el, i) {
+    console.log(el + ' is at index ' + i);
+}); // prints
+// a is at index 0
+// b is at index 1
+// c is at index 2
+
+let test = [];
+myForEach(['laika', 'belka'], function (el) {
+    test.push(el.toUpperCase());
+});
+console.log(test); // ['LAIKA', 'BELKA']
+*******************************************************************************/
+
+let letters = ['a', 'b', 'c']
+
+let myForEach = function(arr, cb) {
+
+    for ( let i = 0; i < arr.length; i++ ) {
+        let el = arr[i]
+        cb(el, i)
+    }
+};
+
+myForEach(letters, function(el, i) {
+    console.log(`${el} is at index ${i}`)
+} )
