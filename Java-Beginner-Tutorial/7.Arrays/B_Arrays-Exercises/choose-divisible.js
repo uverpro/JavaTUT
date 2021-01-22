15095956a177d51802178caa89dc78fa4ce42dbd
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-21 22:48:48 -0500

    Update choose-divisible.js

--- Java-Beginner-Tutorial/7.Arrays/B_Arrays-Exercises/choose-divisible.js
+++ Java-Beginner-Tutorial/7.Arrays/B_Arrays-Exercises/choose-divisible.js
@@ -4,7 +4,9 @@
 // The function should return an array containing elements of the original
 // array that are divisible by the target.
 
+let chooseDivisibles = function (numbers, targer) {
 
+};
 
 console.log(chooseDivisibles([40, 7, 22, 20, 24], 4)); // [40, 20, 24]
 console.log(chooseDivisibles([9, 33, 8, 17], 3)); // [9, 33]
