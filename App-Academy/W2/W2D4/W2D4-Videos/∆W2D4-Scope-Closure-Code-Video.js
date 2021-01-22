effd2762a3055097355053e630a22da1eb4372f9
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-22 13:59:23 -0500

    Create ∆W2D4-Scope-Closure-Code-Video.js

---
+++ App-Academy/W2/W2D4/W2D4-Videos/∆W2D4-Scope-Closure-Code-Video.js
@@ -0,0 +1,12 @@
+// CLOSURE Code Demo
+
+// 1. Closure: a closure is when an inner function uses, or changes,
+// variables in an outer function.
+
+function pizzaMaker(food) {
+    let order = "I'd like a pizza with ";
+    function oven() {
+        return order + food;
+    }
+    return oven();
+};
