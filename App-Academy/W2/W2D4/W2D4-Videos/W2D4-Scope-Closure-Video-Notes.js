56cc41b80744884e59bc9849c6d7eb56195f9f4e
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-22 13:37:09 -0500

    Update W2D4-Scope-Closure-Video-Notes.js

--- App-Academy/W2/W2D4/W2D4-Videos/W2D4-Scope-Closure-Video-Notes.js
+++ App-Academy/W2/W2D4/W2D4-Videos/W2D4-Scope-Closure-Video-Notes.js
@@ -42,3 +42,18 @@ function treeMaker() {
 
 const treeFunc = treeMaker(); // ==> returns a func [Function: addTree]
 treeFunc('Pine'); // ==> ["Pine"]
+
+
+// Closures allow us to create Private State
+function  createCount() {
+    let count = 0;
+
+    return function() {
+        count++
+        return count;
+    };
+}
+
+let counter1 = createCounter();
+console.log(counter1()); // ==> 1
+console.log(counter1()); // ==> 2
