911cc475e707780bf73c8138fb85d0d3272e0ce7
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-22 17:01:48 -0500

    Update ∆W2D4-Scope-Arrow-Functions.js

--- App-Academy/W2/W2D4/W2D4-Videos/∆W2D4-Scope-Arrow-Functions.js
+++ App-Academy/W2/W2D4/W2D4-Videos/∆W2D4-Scope-Arrow-Functions.js
@@ -2,11 +2,15 @@
 // 1. Syntax
 // 2. Scoping with Arrow Functions
 
-// function sayHello(name) {
-//     return `Hello ${name}`;
-// };
+////////////////////////////////
 
-// console.log(sayHello("Leon")); // Hello Leon
+function sayHello(name) {
+    return `Hello ${name}`;
+};
+
+console.log(sayHello("Leon")); // Hello Leon
+
+////////////////////////////////
 
 let sayHello = (name) => {
     return `Hello ${name}`;
@@ -14,8 +18,42 @@ let sayHello = (name) => {
 
 console.log(sayHello("Leon")); // Hello Leon
 
+////////////////////////////////
+
+let sayHello = name => `Hello ${name}`;
+
+console.log(sayHello("Leon"));
+
 /*
 Can implicitly return single expression arrow functions
 Implicit return means the function will automatically return value
 without having to explicitly use the keyword return
 */
+
+/**********************************************************************/
+
+function myFilter(array, cb) {
+    let filtered = [];
+    array.forEach(function (ele, i) {
+        if (cb(ele)) {
+            filtered.push(ele);
+        }
+    });
+    return filtered;
+}
+
+console.log(myFilter([1, 2, 3], (el) => el === 1)); // [ 1 ]
+
+
+
+const myFilter = (array, cb) => {
+    let filtered = [];
+    array.forEach((ele, i) => {
+        if (cb(ele)) {
+            filtered.push(ele);
+        }
+    });
+    return filtered;
+};
+
+console.log(myFilter([1, 2, 3], el => el === 1)); // [ 1 ]
