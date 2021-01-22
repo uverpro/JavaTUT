520ba52f1354ff8bfe2cba1c8b573af94ee34918
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-22 17:11:18 -0500

    Create ΩW2D4-Learning-Objectives.js

---
+++ App-Academy/W2/W2D4/ΩW2D4-Learning-Objectives.js
@@ -0,0 +1,12 @@
+// Scope Lesson Learning Objectives
+
+// 1. Identify the difference between const, let, and var declarations
+
+// 2. Explain the difference between const, let, and var declarations
+
+// 3. Predict the evaluation of code that utilizes function scope, block scope,
+// lexical scope, and scope chaining
+
+// 4. Define an arrow function
+
+// 5. Implement a closure and explain how the closure effects scope
