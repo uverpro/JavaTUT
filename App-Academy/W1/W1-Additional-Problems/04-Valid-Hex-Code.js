184999a779e7a44194e735b01e85364c69091fa1
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-22 11:03:45 -0500

    Create 04-Valid-Hex-Code.js

---
+++ App-Academy/W1/W1-Additional-Problems/04-Valid-Hex-Code.js
@@ -0,0 +1,19 @@
+/*
+
+    Valid Hex Code
+    Create a function that determines whether a string is a valid hex code.
+    A hex code must begin with a pound key # and is exactly 6 characters in length.
+    Each character must be a digit from 0-9 or an alphabetic character from A-F.
+    All alphabetic characters may be uppercase or lowercase.
+
+    console.log(validHexCode("#ABCDEF")); // prints true;
+    console.log(validHexCode("#88ef29")); // prints true;
+    console.log(validHexCode("943234")); // prints false;
+    console.log(validHexCode("#222!E4")); // prints false;
+    console.log(validHexCode("#22222")); // prints false;
+
+*/
+
+function validHexCode(string) {
+
+}
