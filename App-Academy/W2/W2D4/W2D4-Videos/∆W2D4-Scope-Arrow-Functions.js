881b470759518c77ea263e94a2c5b481ab4d5233
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-22 16:09:06 -0500

    Update ∆W2D4-Scope-Arrow-Functions.js

---
+++ App-Academy/W2/W2D4/W2D4-Videos/∆W2D4-Scope-Arrow-Functions.js
@@ -0,0 +1,15 @@
+// Arrow Functions
+// 1. Syntax
+// 2. Scoping with Arrow Functions
+
+// function sayHello(name) {
+//     return `Hello ${name}`;
+// };
+
+// console.log(sayHello("Leon")); // Hello Leon
+
+let sayHello = (name) => {
+    return `Hello ${name}`;
+};
+
+console.log(sayHello("Leon")); // Hello Leon
