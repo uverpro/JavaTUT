5c07d70a516ede1f44d455fe55de9fdec5a63619
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-21 22:17:58 -0500

    Update reverseArray.js

--- Java-Beginner-Tutorial/7.Arrays/B_Arrays-Exercises/reverseArray.js
+++ Java-Beginner-Tutorial/7.Arrays/B_Arrays-Exercises/reverseArray.js
@@ -8,3 +8,12 @@ let reverseArray = function (array) {
 
 console.log(reverseArray(["zero", "one", "two", "three"])); // ['three', 'two', 'one', 'zero']
 console.log(reverseArray([7, 1, 8])); // [8, 1, 7]
+
+
+// let reverseArray = function (array) {
+//     let reversed = [];
+//     for (let i = array.length - 1; i >= 0; i--) {
+//         reversed.push(array[i]);
+//     }
+//     return reversed;
+// };
