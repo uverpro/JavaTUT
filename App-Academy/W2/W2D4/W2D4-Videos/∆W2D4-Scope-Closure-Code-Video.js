fa6fe0f068dfe4e363826b92535d367b8db0ea6c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-22 15:09:15 -0500

    Update ∆W2D4-Scope-Closure-Code-Video.js

--- App-Academy/W2/W2D4/W2D4-Videos/∆W2D4-Scope-Closure-Code-Video.js
+++ App-Academy/W2/W2D4/W2D4-Videos/∆W2D4-Scope-Closure-Code-Video.js
@@ -15,15 +15,21 @@ console.log(pizzaMaker("cheese")); // ==> "I'd like a pizza with cheese"
 
 
 
-function groceryList(list) {
-    let groceries = list;
+function groceryList(list) { // 1. Evoked.
+    let groceries = list; // isn't necessary, WILL WORK with "list"
 
-    function addItem() {
-        groceries.push("and ice cream!")
+    function addItem() { // 3. Ran.
+        groceries.push("and ice cream!") // push adds element to the END of the array
     }
 
-    addItem();
-    return groceries;
+    addItem(); // 2. Evoked.
+    return groceries; // 4. Returned.
 }
 
 console.log(groceryList(["milk", "eggs"]));
+// ==> [ 'milk', 'eggs', 'and ice cream!' ]
+
+// Line 29 evokes function "groceryList(list)" on line 18
+// Line 25 evokes function "addItem()" on line 21
+// Line 21 function "addItem()" pushes arguement into "list"
+// Line 26 returns groceries (aka list) which was modified by function "addItem"
