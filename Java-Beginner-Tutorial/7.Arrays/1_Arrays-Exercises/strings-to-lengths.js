476298c051ee24ad54bc748e1310aab540c3c651
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-21 19:51:41 -0500

    Update strings-to-lengths.js

--- Java-Beginner-Tutorial/7.Arrays/1_Arrays-Exercises/strings-to-lengths.js
+++ Java-Beginner-Tutorial/7.Arrays/1_Arrays-Exercises/strings-to-lengths.js
@@ -1,15 +1,15 @@
 // Write a function `stringsToLengths` that accepts an array of strings as an argument.
 //The function should return a new array containing the lengths of the elements of the original array.
 
-function stringsToLengths (words) {
-    let lengths = [];
-    for (i = 0; i < words.length; i++) {
-        let nums = words[i];    //console.log(array[i]);
-        lengths.push(nums.length); // words[i].length WORKS. No need for conditionals!
+function stringsToLengths (array) {
+    let stringLength = [];
+    for (i = 0; i < array.length; i++) {
+        let word = array[i];    //console.log(array[i]);
+        stringLength.push(word.length); // words[i].length WORKS. No need for conditionals!
         // lengths.push puts "()" into new array
         // words[i].length = words[0].length = belly.length = 5
     }
-    return lengths;
+    return stringLength;
 };
 
 
