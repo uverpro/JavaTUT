3b65e7037c111f889e25b00ebcc04717e3e56c27
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-22 11:42:28 -0500

    Create W2D4-Boost-acc.js

---
+++ App-Academy/W2/W2D4/W2D4-Boost-acc.js
@@ -0,0 +1,12 @@
+let arr = [1, 1, 3, 2, 2, 1];
+
+const result = arr.reduce(function(acc, value) {
+  if (acc[value]) {
+    acc[value]++;
+  } else {
+    acc[value] = 1;
+  }
+  return acc;
+}, {});
+
+console.log(result)
