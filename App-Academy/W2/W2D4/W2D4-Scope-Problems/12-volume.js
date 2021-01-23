91aaf26dd811b14c82600053943d5f42b6cea615
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-22 20:17:18 -0500

    Update 12-volume.js

--- App-Academy/W2/W2D4/W2D4-Scope-Problems/12-volume.js
+++ App-Academy/W2/W2D4/W2D4-Scope-Problems/12-volume.js
@@ -4,10 +4,10 @@ for a rectangle (height * width * length). In order to enter the
 required measurements we'll need to measure them one at a time.
 
 Write a function named `recVolume(height)`. The recVolume function will be passed
-a height int and will return a function. The function returned by recVolume can 
+a height int and will return a function. The function returned by recVolume can
 then be invoked two more times with a single argument number each time (one for
 length and one for width). Once all three numbers (height, width, length) have
-been collected return the volume of the rectangle. Any subsequent calls to the 
+been collected return the volume of the rectangle. Any subsequent calls to the
 function returned by recVolume should continue to return the original volume.
 
 Example 1:
@@ -22,15 +22,30 @@ table2(2); // returns a function
 console.log(table2(1)); // prints 6
 console.log(table2(75)); // STILL prints 6
 
+// currying
+
 ***********************************************************************/
 
-// your code here
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-
-try {
-  module.exports = recVolume;
-} catch (e) {
-  // catch the ref err
-  module.exports = null;
+function recVolume(num) {
+  let n = 1;
+  let final = num;
+  if (n < 3) {
+    return function inner(num2) {
+      // final *= num2;
+      n++;
+      // console.log(final);
+      if (n <= 3) {
+        final *= num2
+      }
+      if (n < 3) {
+      return inner;
+      }
+      return final;
+    }
+  }
 }
+
+let table2 = recVolume(3); // returns a function
+table2(2); // returns a function
+console.log(table2(1)); // prints 6
+console.log(table2(75)); // STILL prints 6
