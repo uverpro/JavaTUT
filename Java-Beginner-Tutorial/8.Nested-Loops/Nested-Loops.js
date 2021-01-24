6c894402bcd4d482099da66c0a2ec0df2b99aab2
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-24 16:36:54 -0500

    Update Nested-Loops.js

--- Java-Beginner-Tutorial/8.Nested-Loops/Nested-Loops.js
+++ Java-Beginner-Tutorial/8.Nested-Loops/Nested-Loops.js
@@ -9,6 +9,10 @@ for (let i = 1; i <= 5; i++) {
 for (let i = 1; i <= 5; i++) {
     for (let j = 1; j <= 3; j++) {
         console.log(i, j);
-    // 1 1, 1 2, 1 3, 2 1, 2 2, 2 3, 3 1, 3 2, 3 3, 4 1, 4 2, 4 3, 5 1, 5 2, 5 3
+    /* 1 1, 1 2, 1 3, // first iteration of i (outer loop)
+    2 1, 2 2, 2 3, // second
+    3 1, 3 2, 3 3, // third
+    4 1, 4 2, 4 3, // fourth
+    5 1, 5 2, 5 3 // fifth */
     }
 };
