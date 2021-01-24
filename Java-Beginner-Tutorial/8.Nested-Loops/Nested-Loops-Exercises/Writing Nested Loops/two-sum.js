fc9f8dfd92af9a359751c797d60d73c63cda22b9
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-24 18:59:16 -0500

    Create two-sum.js

---
+++ Java-Beginner-Tutorial/8.Nested-Loops/Nested-Loops-Exercises/Writing Nested Loops/two-sum.js
@@ -0,0 +1,11 @@
+// Write a function `twoSum(numbers, target)` that accepts an array of numbers
+// and a target number as an argument.
+// The function should return a boolean indicating whether or not there exists a pair
+// of distinct elements in the array that sum to the target.
+
+
+
+console.log(twoSum([2, 3, 5, 9], 7)); // true
+console.log(twoSum([2, 3, 5, 9], 4)); // false
+console.log(twoSum([6, 3, 4], 10)); // true
+console.log(twoSum([6, 5, 1], 10)); // false
