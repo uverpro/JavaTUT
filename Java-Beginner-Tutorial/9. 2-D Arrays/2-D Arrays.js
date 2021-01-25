599281250090f7fd0e2db37590f252d4ffaeaab5
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-24 20:33:02 -0500

    Update 2-D Arrays.js

--- Java-Beginner-Tutorial/9. 2-D Arrays/2-D Arrays.js
+++ Java-Beginner-Tutorial/9. 2-D Arrays/2-D Arrays.js
@@ -1,18 +1,33 @@
 // 2D ARRAYS
 // is a NESTED ARRAY
 
+// let array = [
+//     ["a", "b"],
+//     ["c", "d", "e"]
+// ];
+
+// console.log(array); // [ [ 'a', 'b' ], [ 'c', 'd', 'e' ] ]
+// console.log(array[0]); // [ 'a', 'b' ]
+// console.log(array[1]); // [ 'c', 'd', 'e' ]
+
+// let subArray = array[1];
+// console.log(subArray); // [ 'c', 'd', 'e' ]
+
+// console.log(subArray[2]); // e  >===\
+// //                                    >-- UNDERSTAND BOTH OF THESE METHODS
+// console.log(array[1][2]); // e  >===/
+
+
+
 let array = [
     ["a", "b"],
     ["c", "d", "e"]
+    ["f", "g", "h"]
 ];
 
-console.log(array); // [ [ 'a', 'b' ], [ 'c', 'd', 'e' ] ]
-console.log(array[0]); // [ 'a', 'b' ]
-console.log(array[1]); // [ 'c', 'd', 'e' ]
-
-let subArray = array[1];
-console.log(subArray); // [ 'c', 'd', 'e' ]
-
-console.log(subArray[2]); // e  >===\
-//                                    >-- UNDERSTAND BOTH OF THESE METHODS
-console.log(array[1][2]); // e  >===/
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    for (let j = 0; j < array[i].length; j++) {
+        console.log(array[i][j]);
+    }
+};
