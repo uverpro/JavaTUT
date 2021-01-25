97d307d6e6f6f378b4a6e81dacc9c5e573e102fa
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-24 20:10:20 -0500

    Create censor-e.js

---
+++ Java-Beginner-Tutorial/6. Loops/C_Loops_Exercise/censor-e.js
@@ -0,0 +1,20 @@
+// Write a function `censorE` that accepts a string as an argument.
+//The function should return the a new version of string where all characters that are 'e's are replaced with '*'s.
+
+let censorE = function (str) {
+    let nustring = "";
+    for (let i = 0; i < str.length; i++) {
+         let char = str[i]; //console.log(str[i]) s, p, e, e, d, y
+         if (char === "e") {
+            nustring += "*";
+         } else {
+             nustring += char;
+         }
+    }
+    return nustring;
+};
+
+console.log(censorE("speedy")); // sp**dy
+console.log(censorE("pending")); // p*nding
+console.log(censorE("scene")); // sc*n
+console.log(censorE("heat")); // h*at
