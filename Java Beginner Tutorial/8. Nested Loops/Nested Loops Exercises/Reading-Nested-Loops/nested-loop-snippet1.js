0b7605801bbb6dd34a88449484224da236c59aa6
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-25 01:09:13 -0500

    Create nested-loop-snippet1.js

---
+++ Java Beginner Tutorial/8. Nested Loops/Nested Loops Exercises/Reading-Nested-Loops/nested-loop-snippet1.js
@@ -0,0 +1,13 @@
+// snippet 1
+for (let i = 1; i <= 4; i++) {
+    for (let j = 1; j <= 3; j++) {
+      console.log(i, j);
+    }
+  };
+
+// PREDICTION:
+// Will print 12 lines.
+// 1, 1, / 1, 2 / 1, 3
+// 2, 1
+// 3, 1
+// 4, 1 etc
