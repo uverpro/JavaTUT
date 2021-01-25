2279e36f64ce948f7538690edee779d19a7075e9
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-25 01:13:07 -0500

    Create make-matrix.js

---
+++ Java Beginner Tutorial/9. 2-D Arrays/2-D Array Exercises/make-matrix.js
@@ -0,0 +1,36 @@
+// Write a function `makeMatrix(m, n, value)` that accepts three arguments.
+// The function should return a 2-dimensional array of height `m`
+// and width `n` that contains the `value` as every element.
+
+let makeMatrix = function(m, n, value) {
+    let array = [];
+    for (i = 0; i < m; i++) {
+        let subArray = [];
+        for (let j = 0; j < n; j++) {
+            subArray.push(value)
+        }
+        array.push(subArray);
+    }
+    return array;
+};
+
+console.log(makeMatrix(3, 5, null));
+// [
+//   [ null, null, null, null, null ],
+//   [ null, null, null, null, null ],
+//   [ null, null, null, null, null ]
+// ]
+
+console.log(makeMatrix(4, 2, "x"));
+// [
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ]
+// ]
+
+console.log(makeMatrix(2, 2, 0));
+// [
+//   [ 0, 0 ],
+//   [ 0, 0 ]
+// ]
