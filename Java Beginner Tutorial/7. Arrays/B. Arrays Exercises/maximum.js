68cee7e0a38380706ae7d1466bde7b964b204f10
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-25 01:08:41 -0500

    Create maximum.js

---
+++ Java Beginner Tutorial/7. Arrays/B. Arrays Exercises/maximum.js
@@ -0,0 +1,20 @@
+// Write a function `maximum` that accepts an array of numbers as an argument.
+// The function should return the largest number of the array.
+// If the array is empty, then the function should return null.
+
+let maximum = function (array) {
+    if (array.length === 0) {
+        return null;
+    }
+        let highestNumber = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] > highestNumber) {
+                highestNumber = array[i];
+        }
+    }
+    return highestNumber;
+};
+
+console.log(maximum([5, 6, 3, 7])); // 7
+console.log(maximum([17, 15, 19, 11, 2])); // 19
+console.log(maximum([])); // null
