92c0fb0048f68fe8a6dc150df6f0dbdffb4da9db
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-25 01:13:36 -0500

    Create half.js

---
+++ Java Beginner Tutorial/5. Functions/A. Function Exercise/half.js
@@ -0,0 +1,9 @@
+// Write a function `half` that accepts a number as an argument. The function should return half of the number.
+
+let half = function (num) {
+ return num / 2; // again, use a simple Boolean when possible
+};
+
+console.log(half(8)); // 4
+console.log(half(15)); // 7.5
+console.log(half(90)); // 45
