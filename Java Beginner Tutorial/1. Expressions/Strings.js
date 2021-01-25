2ee1d35bb7b1ff784c96a317afa2b71b4725e442
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-25 01:05:40 -0500

    Create Strings.js

---
+++ Java Beginner Tutorial/1. Expressions/Strings.js
@@ -0,0 +1,17 @@
+console.log("A1");
+console.log("ABC111222333");
+console.log("That's what the lines are for.");
+console.log('Useful for quoting, "Cory"');
+
+console.log("Cat" + "Dog");
+console.log("cat".toUpperCase());
+console.log("DOG".toLowerCase());
+
+console.log('apple' === 'apple'); // true
+console.log('apple' === 'Apple'); // false
+
+// > or < in Strings measure the order of characters in alphabetical order
+console.log('cat' < 'dog'); // true
+console.log('cat' < 'cats'); // true
+console.log('cat' < 'apple'); // false
+console.log()
