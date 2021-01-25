53ced85efe57099312d86ac31d3eca5d4d50aac1
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-24 23:58:39 -0500

    Create zipper.js

---
+++ Java-Beginner-Tutorial/9. 2-D Arrays/2-D Array Exercises/zipper.js
@@ -0,0 +1,26 @@
+// Write a function `zipper` that accepts two arrays as arguments.
+// The function should return a 2D array containing pairs of elements at the same indices.
+// You can assume that the arrays have the same length.
+
+let zipper = function (array1, array2) {
+
+};
+
+let array1 = ["a", "b", "c", "d"];
+let array2 = [-1, -2, -3, -4];
+console.log(zipper(array1, array2));
+// [
+//   ['a', -1],
+//   ['b', -2],
+//   ['c', -3],
+//   ['d', -4],
+// ]
+
+let array3 = ["whisper", "talk", "shout"];
+let array4 = ["quiet", "normal", "loud"];
+console.log(zipper(array3, array4));
+// [
+//   ['whisper', 'quiet'],
+//   ['talk', 'normal'],
+//   ['shout', 'loud'],
+// ]
