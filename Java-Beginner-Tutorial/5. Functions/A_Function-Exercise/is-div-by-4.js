1ffac2de1bbd7ad9539ee35831e8c3ad823ee6e4
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-24 19:50:04 -0500

    Create is-div-by-4.js

---
+++ Java-Beginner-Tutorial/5. Functions/A_Function-Exercise/is-div-by-4.js
@@ -0,0 +1,20 @@
+// Write a function `isDivBy4` that accepts a number as an argument. The function should return a
+// boolean indicating whether or not the number is divisible by 4.
+
+let isDivBy4 = function(num) { // the "if" & "else" Statements are in the first brackets
+    if (num % 4 === 0) { // every Return Function has its own curly brackets
+    return true;
+} else {
+    return false;
+    }
+};
+
+// let isDivBy4 = function (num) { // more efficient use a simple Boolean when possible
+//     return num % 4 === 0;
+// };
+
+console.log(isDivBy4(8)); // true
+console.log(isDivBy4(12)); // true
+console.log(isDivBy4(24)); // true
+console.log(isDivBy4(9)); // false
+console.log(isDivBy4(10)); // false
