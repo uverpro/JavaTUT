947ca910abc434385beb7e783f8b96b088b6998f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-24 23:15:24 -0500

    Update twoSumPairs.js

--- Java-Beginner-Tutorial/9. 2-D Arrays/2-D Array Exercises/twoSumPairs.js
+++ Java-Beginner-Tutorial/9. 2-D Arrays/2-D Array Exercises/twoSumPairs.js
@@ -3,7 +3,18 @@
 // The function should return a 2D array containing all unique pairs of elements that
 // sum to the target.
 
-
+let twoSumPairs = function(numbers, target) {
+    let pairs = [];
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) {
+                let pair = [ numbers[i], numbers[j] ];
+                pairs.push(pair);
+            }
+        }
+    }
+    return pairs;
+};
 
 console.log(twoSumPairs([2, 3, 4, 6, 5], 8)); // [ [2, 6], [3, 5] ]
 console.log(twoSumPairs([10, 7, 4, 5, 2], 12)); // [ [10, 2], [7, 5] ]
