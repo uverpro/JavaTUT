c12e286e8c816ddbd769dba2dd6220917e9c3470
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-01-24 20:18:57 -0500

    Update 2-D Arrays.js

--- Java-Beginner-Tutorial/9. 2-D Arrays/2-D Arrays.js
+++ Java-Beginner-Tutorial/9. 2-D Arrays/2-D Arrays.js
@@ -9,3 +9,7 @@ let array = [
 console.log(array); // [ [ 'a', 'b' ], [ 'c', 'd', 'e' ] ]
 console.log(array[0]); // [ 'a', 'b' ]
 console.log(array[1]); // [ 'c', 'd', 'e' ]
+
+let subArray = array[1];
+console.log(subArray); // [ 'c', 'd', 'e' ]
+console.log(subArray[2]); // e
