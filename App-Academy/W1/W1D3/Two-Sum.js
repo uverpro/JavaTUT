4c76934134a982b16c8a2f14316f5141375ec854
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-02-09 07:41:46 -0500

    Update Two-Sum.js

--- App-Academy/W1/W1D3/Two-Sum.js
+++ App-Academy/W1/W1D3/Two-Sum.js
@@ -4,3 +4,24 @@
 // indicating if two distinct numbers of the array add up to the target value.
 //  You can assume that input array contains only unique numbers.
 // Hint: USE A NESTED LOOP
+
+let twoSum = function (arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        let num1 = arr[i];
+
+        for (let j = i + 1; j < arr.length; j++) {
+            let num2 = arr[j];
+
+            if (num1 + num2 === target) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+};
+
+console.log(twoSum([1, 7, 3, 0, 2], 5)); // true
+console.log(twoSum([1, 7, 3, 0, 2], 6)); // false
+console.log(twoSum([4, 6, 2, 3], 8)); // true
+console.log(twoSum([4, 6, 2, 3], 11)); // false
