08aadf0dbb0cedfb33eb9e63848f599453e95692
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-02-21 14:10:08 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,20 +1,19 @@
-console.log(2 + 3); // 5
-console.log(10 - 15); // -5
-console.log(4 + 1 - 5); // 0
-console.log(4 * 3); // 12
+console.log(false); // false
+console.log(!true); // false
+console.log(!false); // true
+console.log(!!true); // true
 
-console.log(7 / 2); // 3.5
-console.log(4 + 2 * 3); // 10
-console.log((4 + 2) * 3); // 12
-console.log(5 % 2); // 1
+console.log(false && false); // false
+console.log(false && true); // false
+console.log(true && false); // false
+console.log(true && true); // true
 
-console.log(6 % 2); // 0
-console.log(7 % 2); // 1
-console.log(8 % 2); // 0
-console.log(19 % 8); // 3
+console.log(false || false); // false
+console.log(false || true); // true
+console.log(true || false); // true
+console.log(true || true); // true
 
-console.log(24 % 8);  // 0
-console.log(7 % 4); // 3
-console.log(4 % 7); // 0
-console.log(5 + (10 % 5)); // 5
-console.log((5 + 10) % 5); // 0
+console.log(!false || false); // true
+console.log(false || (true && true)); // true
+console.log(false || !(true && true)); // false
+console.log(!true && (false || true)); // false
