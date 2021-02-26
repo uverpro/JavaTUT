e223ffdc041d89dc1751d08111540244e67ebf14
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-02-26 18:47:11 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,17 +1,11 @@
-// Write a function `oneOrNone` that accepts two booleans as arguments.
-// The function should return true
-// if exactly one of the arguments is true.
-// If BOTH arguments are true, then it should return false.
+// Write a function `longer` that accepts two strings as arguments.
+//The function should return the
+// string that is longer. If the strings have the same length, then return the first string.
 
-let oneOrNone = function(boo1, boo2) {
-    if ((boo1 || boo2) && !(boo1 && boo2)) {
-        return true;
-        } else {
-        return false;
-    }
-};
 
-console.log(oneOrNone(false, false)); // false
-console.log(oneOrNone(true, false)); // true
-console.log(oneOrNone(false, true)); // true
-console.log(oneOrNone(true, true)); // false
+
+console.log(longer("drum", "piranha")); // 'piranha'
+console.log(longer("basket", "fork")); // 'basket'
+console.log(longer("flannel", "sustainable")); // 'sustainable'
+console.log(longer("disrupt", "ability")); // 'disrupt'
+console.log(longer("bird", "shoe")); // 'bird'
