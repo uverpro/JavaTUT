67e4126c923b8e559a31e1c12a6548ce31faec58
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-02-26 18:59:17 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -2,7 +2,13 @@
 //The function should return the
 // string that is longer. If the strings have the same length, then return the first string.
 
-
+let longer = function(str1, str2) {
+    if (str1.length >= str2.length) {
+        return str1;
+    } else {
+        return str2;
+    }
+};
 
 console.log(longer("drum", "piranha")); // 'piranha'
 console.log(longer("basket", "fork")); // 'basket'
