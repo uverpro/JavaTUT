0c5b27d79a9a64a84578e28fdfbb64f9771e3cb9
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-02-26 20:49:52 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,9 @@
+// Write a function `numberChange` that accepts a number as an argument.
+// The function should return
+// half the number if it is even.
+// The function should return double the number if it is odd.
+
+console.log(numberChange(6)); // 3
+console.log(numberChange(7)); // 14
+console.log(numberChange(16)); // 8
+console.log(numberChange(21)); // 42
