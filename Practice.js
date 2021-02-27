9590c27d92ec5427e264be826139b31816e0fed2
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-02-26 20:18:20 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,12 @@
+// Write a function `stringSize` that accepts a string as an argument.
+// The function should return the
+// string 'small' if the argument is shorter than 5 characters,
+// 'medium' if it is exactly 5 characters, and
+// 'large' if it is longer than 5 characters.
+
+console.log(stringSize("cat")); // 'small'
+console.log(stringSize("bell")); // 'small'
+console.log(stringSize("ready")); // 'medium'
+console.log(stringSize("shirt")); // 'medium'
+console.log(stringSize("shallow")); // 'large'
+console.log(stringSize("intelligence")); // 'large'
