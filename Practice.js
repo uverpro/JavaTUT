b878cc588d840c7878bbef2068979f278e129ed4
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-02-26 20:34:18 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,11 @@
+// Write a function `caseChange` that accepts a string and a boolean as arguments.
+// The function should
+// return the uppercase version of the string if the boolean is true.
+// The function should return the
+// lowercase version of the string if the boolean is false.
+
+
+console.log(caseChange("Super", true)); // 'SUPER'
+console.log(caseChange("Super", false)); // 'super'
+console.log(caseChange("tAmBourine", true)); // 'TAMBOURINE'
+console.log(caseChange("tAmBourine", false)); // 'tambourine'
