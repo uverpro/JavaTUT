f2290bd941e644c6d550da3b7b18f27523133613
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-02-26 20:56:41 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -3,7 +3,11 @@
 // larger number.
 
 let larger = function (num1, num2) {
-
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
 };
 
 console.log(larger(256, 400)); // 400
