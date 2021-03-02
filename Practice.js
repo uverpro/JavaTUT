5941678b25ea2ba9f4efc61f0536b4340a337739
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-01 19:24:04 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,20 @@
+// Write a function named `noOhs` that accepts a string as an argument.
+// The functions should print the
+// characters of the string one by one except the character 'o'.
+// The function doesn't need to return
+// any value. It should just print to the terminal.
+
+
+
+noOhs("code");
+// prints
+//  c
+//  d
+//  e
+
+noOhs("school");
+// prints
+//  s
+//  c
+//  h
+//  l
