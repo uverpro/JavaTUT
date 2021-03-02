dc22f17a46bf1fc1bfba6a401ec399dcfcc6d62f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-01 19:32:19 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -4,7 +4,14 @@
 // The function doesn't need to return
 // any value. It should just print to the terminal.
 
-
+let noOhs = function(str) {
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (char !== "o") {
+            console.log(str[i]);
+        }
+    }
+};
 
 noOhs("code");
 // prints
