91e79a354159db63b3b5ab3e37e68181823d9a30
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-01 22:56:12 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -2,7 +2,9 @@
 // The function should return the a new
 // version of string where all characters that are 'e's are replaced with '*'s.
 
+let censorE = function (str) {
 
+};
 
 console.log(censorE("speedy")); // 'sp**dy'
 console.log(censorE("pending")); // 'p*nding'
