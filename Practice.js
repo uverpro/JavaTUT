b9bdcc589901162423fb93dc4381a8bfe42103a6
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-01 21:32:26 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,7 +1,9 @@
 // Write a function `removeCapitals` that accepts a string as an argument.
 // The function should return a new version of the string with all capital letters removed.
 
+let removeCapitals = function(str) {
 
+};
 
 console.log(removeCapitals("fOrEver")); // 'frver'
 console.log(removeCapitals("raiNCoat")); // 'raioat'
