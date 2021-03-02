4feb90b3f283f99b26644d3388ffb23b062c65c0
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-01 20:58:36 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -2,7 +2,11 @@
 //  The function should print all numbers between `min` and `max` (exclusive)
 // that are also divisible by num.
 
+let divisibleRange = function (min, max, num) {
+    for (let i = min; i < max; i++) {
 
+    }
+}
 
 divisibleRange(17, 40, 9);
 // prints
