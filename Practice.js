f4563ee8c1aa639092c8b3443538ba21071ffed3
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-01 22:55:19 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,10 @@
+// Write a function `censorE` that accepts a string as an argument.
+// The function should return the a new
+// version of string where all characters that are 'e's are replaced with '*'s.
+
+
+
+console.log(censorE("speedy")); // 'sp**dy'
+console.log(censorE("pending")); // 'p*nding'
+console.log(censorE("scene")); // 'sc*n*'
+console.log(censorE("heat")); // 'h*at'
