73d29d970bdb9be85cb18bb5cdd07c146032eed9
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-04 02:05:50 -0500

    Update Rotate-Right.js

--- App-Academy/W1/W1D3/Rotate-Right.js
+++ App-Academy/W1/W1D3/Rotate-Right.js
@@ -1,20 +1,25 @@
-/*Rotate Right
-Write a function rotateRight(array, num) that takes in an array and a number as args.
-The function should return a new array where the elements of the array
-are rotated to the right num times.
+/* Rotate Right
+Write a function rotateRight(array, num)
+that takes in an array and a number as args.
+The function should return a new array
+where the elements of the array are rotated to the right num times.
 The function should not mutate the original array and instead return a new array.
 
 Define this function using function expression syntax.
 
-HINT: you can use Array's slice() method to create a copy of an array*/
+HINT: you can use Array's slice() method to create a copy of an array */
 
-let rotateRight = function (array, number) {
-    for (let i = 0; i < array.length; i++) {
-
+let rotateRight = function(array, num) {
+    let copy = array.slice();
+console.log(copy);
+    for (let i = 0; i < num; i++) {
+        let el = copy.pop();
+        console.log(el);
+        copy.unshift(el);
     }
-    return array;
-};
 
+    return copy;
+};
 
 let arr = ['a', 'b', 'c', 'd', 'e'];
 console.log(rotateRight(arr, 2)); // [ 'd', 'e', 'a', 'b', 'c' ]
