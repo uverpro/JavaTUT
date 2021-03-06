c22ad54b8e9e667f587a1664e951f17029d10e6f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-06 05:24:59 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -2,7 +2,7 @@ let twoDimensionalSum = function (arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
         // for (let j = 1; j < arr.length; j++) {
-    sum += i;
+    sum += arr.push(arr[i]);
     }
     return sum;
 };
