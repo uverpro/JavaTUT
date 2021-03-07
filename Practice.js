2c8436f248d2329c6d09490fde5b8bee0b7d98c3
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-06 22:32:03 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,5 +1,7 @@
-// Write a function `total` that accepts an array of numbers as an argument. The function should return
-// the total sum of all elements of the array.
+// Write a function `total` that accepts an array of numbers as an argument.
+// The function should return the total sum of all elements of the array.
+
+
 
 console.log(total([3, 2, 8])); // 13
 console.log(total([-5, 7, 4, 6])); // 12
