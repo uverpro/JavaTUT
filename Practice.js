c9be6b22fc7726884c81639742612242c548246c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-06 22:37:54 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,8 @@
+// Write a function `stayPositive` that accepts an array of numbers as an argument.
+// The function should return an array containing only the positive numbers.
+
+
+
+console.log(stayPositive([10, -4, 3, 6])); // [10, 3, 6]
+console.log(stayPositive([-5, 11, -40, 30.3, -2])); // [11, 30.3]
+console.log(stayPositive([-11, -30])); // []
