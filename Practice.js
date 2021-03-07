0f37d10e81f76ff49ecb69f48b89727137dc126f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-06 23:10:22 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -2,7 +2,9 @@
 // The function should return a new array
 // containing the lengths of the elements of the original array.
 
+let stringsToLengths = function (array) {
 
+};
 
 console.log(stringsToLengths(["belly", "echo", "irony", "pickled"]));
 // [5, 4, 5, 7]
