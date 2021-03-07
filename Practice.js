34eec81157267707fbf61e948e27ba3049c91d4f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-07 01:53:25 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,14 @@
+// Write a function `yourAverageFunction` that accepts an array of numbers as an argument.
+// The function should return the average of all elements of the array.
+// If the input array is empty, then the function should return null.
+
+let yourAverageFunction = function (array) {
+    if (array.length === -1) {
+        return null;
+    }
+};
+
+console.log(yourAverageFunction([5, 2, 7, 24])); // 9.5
+console.log(yourAverageFunction([100, 6])); // 53
+console.log(yourAverageFunction([31, 32, 40, 12, 33])); // 29.6
+console.log(yourAverageFunction([])); // null
