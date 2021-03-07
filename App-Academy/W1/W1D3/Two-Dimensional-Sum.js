a5fe5977c1c8ebe859446e93bf9379e4ad36a307
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-06 21:41:09 -0500

    Create Two-Dimensional-Sum.js

---
+++ App-Academy/W1/W1D3/Two-Dimensional-Sum.js
@@ -0,0 +1,27 @@
+/*Two Dimensional Sum
+Write a function twoDimensionalSum(arr) that takes in a 2D array of numbers
+and returns the total sum of all numbers.*/
+
+let twoDimensionalSum = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < arr[i].length; j++) {
+            sum += arr[i][j];
+        }
+    }
+    return sum;
+};
+
+
+
+let arr1 = [
+    [1, 3],
+    [-4, 7, 10],
+    [2]
+];
+console.log(twoDimensionalSum(arr1)); // 19
+
+let arr2 = [
+    [],
+    [3, 1, 2],
+];
