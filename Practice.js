579fc1b615aaa4504cf4ebdcd27fd91c9c5edc49
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-06 22:56:03 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -2,7 +2,9 @@
 // The function should return a new array
 // containing only the strings that are less than 5 characters long.
 
+let filterLongWords = function (array) {
 
+};
 
 console.log(filterLongWords(["kale", "cat", "retro", "axe", "heirloom"]));
 // ['kale', 'cat', 'axe']
