b70b89307f850e0caca8817ed64b6040993a52e2
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-07 01:49:51 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -3,7 +3,12 @@
 // containing the elements of the original array in reverse order.
 
 let reverseArray = function (array) {
-
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let el = array[i];
+        newArray.unshift(el);
+    }
+    return newArray;
 };
 
 console.log(reverseArray(["zero", "one", "two", "three"])); // ['three', 'two', 'one', 'zero']
