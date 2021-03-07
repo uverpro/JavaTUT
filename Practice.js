cc5968f2932f9cc7a4eb93e9571da3484a9c201c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-06 23:17:32 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,9 @@
+// Write a function `divisors` that accepts a number as an argument.
+// The function should return an array
+// containing all positive numbers that can divide into the argument.
+
+
+
+console.log(divisors(15)); // [1, 3, 5, 15]
+console.log(divisors(7)); // [1, 7]
+console.log(divisors(24)); // [1, 2, 3, 4, 6, 8, 12, 24]
