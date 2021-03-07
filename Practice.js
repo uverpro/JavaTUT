214acab8ef5e8fa40ce9dbd6f36a68e0157f1997
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-07 00:31:06 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,5 +1,12 @@
 let smallestNumber = function (nums) {
-
+    let smallest = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        let num = nums[i];
+        if (num < smallest) {
+            smallest = num;
+        }
+    }
+    return smallest;
 };
 
 smallestNumber([5, 6, 4, 3, 7]); // 3
