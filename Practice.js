5ebccb8cb151a3696557bd900fac095f3f0ee134
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-07 02:23:00 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,11 @@
+// Write a function `maximum` that accepts an array of numbers as an argument.
+// The function should return the largest number of the array.
+// If the array is empty, then the function should return null.
+
+let maximum = function (array) {
+
+};
+
+console.log(maximum([5, 6, 3, 7])); // 7
+console.log(maximum([17, 15, 19, 11, 2])); // 19
+console.log(maximum([])); // null
