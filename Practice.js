b975111890391658215e4fd2aace551798142806
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-07 01:46:28 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -2,7 +2,9 @@
 // The function should return a array
 // containing the elements of the original array in reverse order.
 
+let reverseArray = function (array) {
 
+};
 
 console.log(reverseArray(["zero", "one", "two", "three"])); // ['three', 'two', 'one', 'zero']
 console.log(reverseArray([7, 1, 8])); // [8, 1, 7]
