f2731163050186b8040874380df3ad0e80e4af71
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-06 23:04:41 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,7 +1,9 @@
 // Write a function `numOdds` that accepts an array of numbers as an argument.
 // The function should return a number representing the count of odd elements in the array.
 
+let numOdds = function (array) {
 
+};
 
 console.log(numOdds([4, 7, 2, 5, 9])); // 3
 console.log(numOdds([11, 31, 58, 99, 21, 60])); // 4
