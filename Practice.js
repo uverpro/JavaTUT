6ed17f7d12f60b55c1fc67c9530738f879cb8a0d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-06 22:44:11 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,10 @@
+// Write a function `bleepVowels` that accepts a string as an argument.
+// The function should return a new string
+// where all vowels are replaced with `*`s. Vowels are the letters a, e, i, o, u.
+
+
+
+console.log(bleepVowels("skateboard")); // 'sk*t*b**rd'
+console.log(bleepVowels("slipper")); // 'sl*pp*r'
+console.log(bleepVowels("range")); // 'r*ng*'
+console.log(bleepVowels("brisk morning")); // 'br*sk m*rn*ng'
