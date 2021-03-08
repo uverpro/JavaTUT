5120317847ca04010d9a3f8e20a110a78c4f3f73
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-07 23:38:23 -0500

    Update Practice.js

--- Practice.js
+++
@@ -1,7 +0,0 @@
-let locations = ["flatbush", "williamsburg", "bushwick", "greenpoint"];
-
-for (let i = 0; i < locations.length; i++) {
-  for (let j = i + 1; j < locations.length; j++) {
-    console.log(locations[i], locations[j]);
-  }
-}
