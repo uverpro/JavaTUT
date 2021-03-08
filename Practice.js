f0decfe562d7f569a47665f1dc0a3df7b08c3f54
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-07 22:48:41 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -2,7 +2,9 @@
 // The function should return a new sentence
 // where all of the words shorter than 4 characters are removed.
 
+let removeShortWords = function (str) {
 
+};
 
 console.log(removeShortWords("knock on the door will you")); // 'knock door will'
 console.log(removeShortWords("a terrible plan")); // 'terrible plan'
