a492511f18307f12a434d9f2934e9f744c628e47
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-07 22:09:49 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,7 +1,9 @@
 // Write a function `alternatingCaps` that accepts a sentence string as an argument.
 // The function should return the sentence where words alternate between lowercase and uppercase.
 
+let alternatingCaps = function (str) {
 
+};
 
 console.log(alternatingCaps("take them to school")); // 'take THEM to SCHOOL'
 console.log(alternatingCaps("What did ThEy EAT before?")); // 'what DID they EAT before?'
