e142a14c960498c1cc546d186a747cb44428d933
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-07 20:57:52 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,10 @@
+// Write a function `wordCount(sentence, targetWords)` that accepts a sentence string
+// and an array of `targetWords`.
+// The function should return a count of the number of words
+// of the sentence that are in `targetWords`.
+
+
+
+console.log(wordCount("open the window please", ["please", "open", "sorry"])); // 2
+console.log(wordCount("drive to the cinema", ["the", "driver"])); // 1
+console.log(wordCount("can I have that can", ["can", "I"])); // 3
