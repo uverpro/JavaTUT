63df1567cd6b2d8054608afe0393df61404425d4
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-07 23:07:40 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -7,7 +7,10 @@
 let commonElements = function (array1, array2) {
     let commonElements = [];
     for (let i = 0; i < array1.length; i++) {
-
+        let array1[i] = element;
+        if (array2.includes(element)) {
+            commonElements.push(element);
+        }
     }
 };
 
