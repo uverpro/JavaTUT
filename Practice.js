dc95d8562d9b9485d8a8d0bfa665d449a091925b
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-07 21:59:55 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -2,7 +2,9 @@
 // The function should return the longest word of the sentence.
 // If there is a tie, return the word that appears later in the sentence.
 
+let lengthiestWord = function (str) {
 
+};
 
 console.log(lengthiestWord("I am pretty hungry")); // 'hungry'
 console.log(lengthiestWord("we should think outside of the box")); // 'outside'
