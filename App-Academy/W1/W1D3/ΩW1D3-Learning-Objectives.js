11815ad861d7733db72044c835ec8122c3270dcb
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-09 18:15:00 -0500

    Update ΩW1D3-Learning-Objectives.js

--- App-Academy/W1/W1D3/ΩW1D3-Learning-Objectives.js
+++ App-Academy/W1/W1D3/ΩW1D3-Learning-Objectives.js
@@ -108,7 +108,14 @@ console.log(sumArrays(arr1)); // 20
 // 8. Define a function that takes in an array of elements
 // and returns a 2d array where the subarrays represent unique pairs of elements
 
+let uniquePairs = function (array) {
+    let finalArray = [];
+    let subArray = [];
 
+};
+
+console.log(uniquePairs(["blue", "velvet", "eraserhead", "blue", "mulholland", "eraserhead"]));
+console.log(uniquePairs([1, 2, 3, 10, 1, 2, 10, 15, 20, 20]));
 
 // 9. Define a function that takes in an array of numbers as an argument
 // and returns the smallest value in the array; if the array is empty return null
