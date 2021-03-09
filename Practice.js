1c15f3ea5aa0dd0f5237e22459a97677cc1d5468
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-09 15:22:49 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,16 +1,14 @@
-function pigLatinWord(word) {
-    for (let i = 0; i < word.length; i++) {
-        let firstLetter = word[0];
-        let vowel = ["a", "e", "i", "o", "u"];
-        if (vowel.includes(firstLetter)) {
-        return `${word}yay`;
-            } else {
-
-        }
+function pigLatinWord (word) {
+    const vowels = ["a", "e", "i", "o", "u"]
+    if (vowels.includes(word[0])) {
+        return `${word}yay`
+    }
+    for (let i = 0; i < word.length; i++)
+    if (vowels.includes(word[i])) {
+        return `${word.slice(i)}${word.slice(0, i)}ay`
     }
 };
-
-  console.log(pigLatinWord("apple")); //=> "appleyay"
-  console.log(pigLatinWord("eat")); //=> "eatyay"
-  console.log(pigLatinWord("banana")); //=> "ananabay"
-  console.log(pigLatinWord("trash")); //=> "ashtray"
+console.log(pigLatinWord("apple")); //=> "appleyay"
+console.log(pigLatinWord("eat")); //=> "eatyay"
+console.log(pigLatinWord("banana")); //=> "ananabay"
+console.log(pigLatinWord("trash")); //=> "ashtray"
