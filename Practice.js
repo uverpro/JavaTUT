c6110b02d65e24d52e40c5c31f854e43fe84d052
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-09 13:26:41 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,12 +1,14 @@
 function pigLatinWord(word) {
     for (let i = 0; i < word.length; i++) {
         let firstLetter = word[0];
-      let vowel = "a,e,i,o,u";
-      if (firstLetter.includes(vowel)) {
+        let vowel = ["a", "e", "i", "o", "u"];
+        if (vowel.includes(firstLetter)) {
         return `${word}yay`;
-      }
+            } else {
+
+        }
     }
-  };
+};
 
   console.log(pigLatinWord("apple")); //=> "appleyay"
   console.log(pigLatinWord("eat")); //=> "eatyay"
