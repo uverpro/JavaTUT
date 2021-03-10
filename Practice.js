77d8240e46e49d9a52b6f0af860e5503df4f8632
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-10 15:36:20 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,19 +1,4 @@
-let factorsOf = function (num) {
-	let factors = [];
-  	for (let i = 1; i <= num; i++) {
-      if (num % i === 0) {
-        factors.push(i);
-      }
-    }
-  return factors;
-};
 
-console.log(factorsOf(5)); // [ 1, 5 ]
-console.log(factorsOf(8)); // [ 1, 2, 4, 8 ]
-console.log(factorsOf(9)); // [ 1, 3, 9 ]
-console.log(factorsOf(10)); // [ 1, 2, 5, 10 ]
-console.log(factorsOf(24)); // [ 1, 2, 3, 4, 6, 8, 12, 24 ]
-console.log(factorsOf(2017)); // [ 1, 2017 ]
 
 // // 8. Define a function that takes in an array of elements
 // // and returns a 2d array where the subarrays represent unique pairs of elements
