48684923a4c1b075d7841d2e13e78762d0063c9b
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-10 11:36:46 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,30 +1,44 @@
-// 8. Define a function that takes in an array of elements
-// and returns a 2d array where the subarrays represent unique pairs of elements
 
-let pairPrint = function (array) {
-    let pairedArrays = [];
-    let pairs = [];
-    for (let i = 0; i < array.length; i++) {
-        let a = array[i];
-        for (let j = i + 1; j < array.length; j++) {
-            let b = array[j];
-            pairs.push(a, b);
-        }
-    }
-    return pairedArrays.push(pairs);
-};
 
-console.log(pairPrint(["artichoke", "broccoli", "carrot", "daikon"]));
-// prints
-//  artichoke - broccoli
-//  artichoke - carrot
-//  artichoke - daikon
-//  broccoli - carrot
-//  broccoli - daikon
-//  carrot - daikon
 
-console.log(pairPrint(["apple", "banana", "clementine"]));
-// prints
-//  apple - banana
-//  apple - clementine
-//  banana - clementine
+
+
+
+
+
+
+
+
+
+
+
+// // 8. Define a function that takes in an array of elements
+// // and returns a 2d array where the subarrays represent unique pairs of elements
+
+// let pairPrint = function (array) {
+//     let pairedArrays = [];
+//     let pairs = [];
+//     for (let i = 0; i < array.length; i++) {
+//         let a = array[i];
+//         for (let j = i + 1; j < array.length; j++) {
+//             let b = array[j];
+//             pairs.push(a, b);
+//         }
+//     }
+//     return pairedArrays.push(pairs);
+// };
+
+// console.log(pairPrint(["artichoke", "broccoli", "carrot", "daikon"]));
+// // prints
+// //  artichoke - broccoli
+// //  artichoke - carrot
+// //  artichoke - daikon
+// //  broccoli - carrot
+// //  broccoli - daikon
+// //  carrot - daikon
+
+// console.log(pairPrint(["apple", "banana", "clementine"]));
+// // prints
+// //  apple - banana
+// //  apple - clementine
+// //  banana - clementine
