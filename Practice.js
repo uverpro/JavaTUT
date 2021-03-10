3e211c80604e20990b7d5e9b9c67f208ee0cceb8
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-10 11:42:04 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,4 +1,25 @@
 
+function removeFromArray(location, arr) {
+    if (location === "FRONT") {
+        arr.shift();
+    }
+    if (location === "BACK") {
+        arr.pop();
+    } else {
+        console.log("ERROR");
+    }
+};
+
+testArray = [0,1,2,3,4]
+
+removeFromArray("FRONT", testArray)
+console.log(testArray) // [1,2,3,4]
+
+removeFromArray("BACK", testArray)
+console.log(testArray) // [1,2,3]
+
+removeFromArray("MIDDLE", 4, testArray) // "ERROR"
+console.log(testArray) // [1,2,3]
 
 
 
