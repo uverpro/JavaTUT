762e7448451ead0e6535448f6ae773ec980b79ed
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-11 18:33:12 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,8 @@
+let removeLastVowel = function (word) {
+
+};
+
+console.log(removeLastVowel('bootcamp')); // 'bootcmp'
+console.log(removeLastVowel('better')); // 'bettr'
+console.log(removeLastVowel('graph')); // 'grph'
+console.log(removeLastVowel('thy')); // 'thy'
