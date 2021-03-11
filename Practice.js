beb35737b57b3fbd73064927b149a6649ebeddec
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-11 10:58:02 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,12 +1,14 @@
-let fizzBuzz = function (max) {
-    let nums = [];
-    for (let i = 0; i < max; i++) {
-        if ((i % 3 === 0 || i % 5 === 0) && !(i % 3 === 0 && i % 5 ===0)) {
-            nums.push(i);
-        }
-    }
-    return nums;
+let firstVowel = function (str) {
+
 };
 
-console.log(fizzBuzz(12)); // [ 3, 5, 6, 9, 10 ]
-console.log(fizzBuzz(20)); // [ 3, 5, 6, 9, 10, 12, 18 ]
+
+
+
+
+
+console.log(firstVowel('battery')); // 'a'
+console.log(firstVowel('tunnel')); // 'u'
+console.log(firstVowel('dog')); // 'o'
+console.log(firstVowel('conventional')); // 'o'
+console.log(firstVowel('rhythm')); // null
