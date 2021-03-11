e2dfb8968baa596dcbe3a6519c6c0475ff73ec0a
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-11 18:00:06 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,14 @@
+let lastVowel = function (string) {
+    let vowel = "";
+    let vowels = "aeiou"
+    for (let i = 0; i < string.length; i++) {
+        let letter = string[i];
+
+    }
+};
+
+console.log(lastVowel('battery')); // 'e'
+console.log(lastVowel('TUNNEL')); // 'E'
+console.log(lastVowel('dog')); // 'o'
+console.log(lastVowel('conventional')); // 'a'
+console.log(lastVowel('rhythm')); // null
