00b0422787f7ead4cba3c8dbdd721d379c47b3f3
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-10 20:46:02 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,7 +1,11 @@
 let fizzBuzz = function (max) {
+    let nums = [];
     for (let i = 0; i < max; i++) {
-        if ((i % 3)
+        if ((i % 3 === 0 || i % 5 === 0) && !(i % 3 === 0 && i % 5 ===0)) {
+            nums.push(i);
+        }
     }
+    return nums;
 };
 
 console.log(fizzBuzz(12)); // [ 3, 5, 6, 9, 10 ]
