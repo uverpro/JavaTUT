b48b2a5fa40902810a054b240d8cf5d71febe3d1
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-11 19:19:29 -0500

    Update Remove-Last-Vowel.js

--- App-Academy/W1/W1D3/Remove-Last-Vowel.js
+++ App-Academy/W1/W1D3/Remove-Last-Vowel.js
@@ -17,3 +17,15 @@ console.log(removeLastVowel('bootcamp')); // 'bootcmp'
 console.log(removeLastVowel('better')); // 'bettr'
 console.log(removeLastVowel('graph')); // 'grph'
 console.log(removeLastVowel('thy')); // 'thy'
+
+
+let removeLastVowel = function (word) {
+    let vowels = "aeiou"
+    for (let i = word.length - 1; i >= 0; i--) {
+        let char = word[i];
+        if (vowels.includes(char)) {
+            return `${word.slice(0, i)}${word.slice(i + 1)}`;
+        }
+    }
+    return word;
+};
