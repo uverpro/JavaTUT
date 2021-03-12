d156c2ff78422419a0ca56265b72635c5b111c59
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-12 12:03:46 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,12 @@
+console.log(pairsMaker(['a', 'b', 'c', 'd'])); // =>
+// [ [ 'a', 'b' ],
+//   [ 'a', 'c' ],
+//   [ 'a', 'd' ],
+//   [ 'b', 'c' ],
+//   [ 'b', 'd' ],
+//   [ 'c', 'd' ] ]
+
+console.log(pairsMaker(['Rosemary', 'Alex', 'Connor'])); // =>
+// [ [ 'Rosemary', 'Alex' ],
+//   [ 'Rosemary', 'Connor' ],
+//   [ 'Alex', 'Connor' ] ]
