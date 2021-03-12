52323bb93c13f541a69c3920a46db38ef46fa00b
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-11 19:52:54 -0500

    Update Uncompress.js

--- App-Academy/W1/W1D4/Uncompress.js
+++ App-Academy/W1/W1D4/Uncompress.js
@@ -27,3 +27,22 @@ let uncompress = function (str) {
 console.log(uncompress('x3y4z2')); // 'xxxyyyyzz'
 console.log(uncompress('a5b2c4z1')); // 'aaaaabbccccz'
 console.log(uncompress('b1o2t1')); // 'boot'
+
+let uncompress = function(str) {
+    let newStr = '';
+
+    for (let i = 0; i < str.length; i += 2) {
+        let char = str[i];
+        let num = Number(str[i + 1]);
+
+        for (let times = 0; times < num; times++) {
+            newStr += char;
+        }
+    }
+
+    return newStr;
+};
+
+console.log(uncompress('x3y4z2')); // 'xxxyyyyzz'
+console.log(uncompress('a5b2c4z1')); // 'aaaaabbccccz'
+console.log(uncompress('b1o2t1')); // 'boot'
