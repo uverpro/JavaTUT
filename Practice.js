e018630c41cddea63c481918638c65d3d33a9e63
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-11 23:08:03 -0500

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,16 +1,2 @@
-let removeLastVowel = function (word) {
-    let vowels = "aeiou"
-    for (let i = word.length - 1; i >= 0; i--) {
-        let char = word[i];
-        if (vowels.includes(char)) {
-            return `${word.slice(0, i)}${word.slice(i + 1)}`;
-        }
-    }
-    return word;
-};
-
-// removeLastVowel('bootcamp')
-console.log(removeLastVowel('bootcamp')); // 'bootcmp'
-console.log(removeLastVowel('better')); // 'bettr'
-console.log(removeLastVowel('graph')); // 'grph'
-console.log(removeLastVowel('thy')); // 'thy'
+Write a function uncompressed
+ehich takes
