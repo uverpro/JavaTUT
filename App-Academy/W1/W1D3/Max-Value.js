8eb0b1f1b96be15b209c819dc107b2ba6383bdaf
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-12 12:41:02 -0500

    Update Max-Value.js

--- App-Academy/W1/W1D3/Max-Value.js
+++ App-Academy/W1/W1D3/Max-Value.js
@@ -8,12 +8,12 @@ let maxValue = function (nums) {
     let max = null;
     for (let i = 0; i < nums.length; i++) {
         let num = nums[i];
-        if (max === null || num > max)
+        if (num > max)
         max = num;
     }
     return max;
 };
 
-console.log(maxValue([4, 6, 3, 5, 2, 4])); // 2
-console.log(maxValue([-2, -3, -7, 3 ])); // -7
+console.log(maxValue([4, 6, 3, 5, 2, 4])); // 6
+console.log(maxValue([-2, -3, -7, 3 ])); // -3
 console.log(maxValue([])); // null
