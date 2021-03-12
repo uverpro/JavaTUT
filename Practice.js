d98eb3bc3dddc24d74f08589f2f9c694f74c86be
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-12 12:52:10 -0500

    Update Practice.js

--- Practice.js
+++
@@ -1,8 +0,0 @@
-let twoSum = function (arr, target) {
-
-};
-
-console.log(twoSum([1, 7, 3, 0, 2], 5)); // true
-console.log(twoSum([1, 7, 3, 0, 2], 6)); // false
-console.log(twoSum([4, 6, 2, 3], 8)); // true
-console.log(twoSum([4, 6, 2, 3], 11)); // false
