a59846c347ae285f7a6cb6c885edc9d8531bb250
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-12 20:11:07 -0500

    Update Has-Three-Vowels.js

--- App-Academy/W1/W1D5/Has-Three-Vowels.js
+++ App-Academy/W1/W1D5/Has-Three-Vowels.js
@@ -3,3 +3,11 @@
 /* Write a function hasThreeVowels that accepts a string as an argument.
 The function should return a boolean
 indicating whether or not the string contains at least three different vowels. */
+
+
+
+console.log(hasThreeVowels('delicious'));       //  true
+console.log(hasThreeVowels('bootcamp prep'));   //  true
+console.log(hasThreeVowels('bootcamp'));        //  false
+console.log(hasThreeVowels('dog'));             //  false
+console.log(hasThreeVowels('go home'));         //  false
