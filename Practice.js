e9e160e7fefb68370d1a51fc3c5341b138f7050c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-13 21:32:43 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,7 @@
+
+
+
+console.log(twoSum([1, 7, 3, 0, 2], 5)); // true
+console.log(twoSum([1, 7, 3, 0, 2], 6)); // false
+console.log(twoSum([4, 6, 2, 3], 8)); // true
+console.log(twoSum([4, 6, 2, 3], 11)); // false
