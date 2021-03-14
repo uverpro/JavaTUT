7b958bcbc55d52a3da72feeade8608d545a35526
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-13 21:54:04 -0500

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,9 @@
+
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(rotateRight(arr, 2)); // [ 'd', 'e', 'a', 'b', 'c' ]
+console.log(arr); // [ 'a', 'b', 'c', 'd', 'e' ]
+
+let animals = ['wombat', 'koala', 'opossum', 'kangaroo'];
+console.log(rotateRight(animals, 3)); // [ 'koala', 'opossum', 'kangaroo', 'wombat' ]
+console.log(animals); // [ 'wombat', 'koala', 'opossum', 'kangaroo' ]
