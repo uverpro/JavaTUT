a6170302ad93eee9cf2a86a1c15781fdc17a2730
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-14 03:07:16 -0400

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,15 @@
+
+
+
+let arr1 = [
+    [1, 3],
+    [-4, 7, 10],
+    [2]
+];
+console.log(twoDimensionalSum(arr1)); // 19
+
+let arr2 = [
+    [],
+    [3, 1, 2],
+];
+console.log(twoDimensionalSum(arr2)); // 6
