be1a905f9d1e1a1336c2fec0c804cc5deca31d0b
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-14 22:33:03 -0400

    Update Least-Common-Multiple.js

--- App-Academy/W1/W1D3/Least-Common-Multiple.js
+++ App-Academy/W1/W1D3/Least-Common-Multiple.js
@@ -1,6 +1,11 @@
 // Least Common Multiple
-// Write a function leastCommonMultiple(num1, num2) that accepts two numbers as arguments. The functions should return the smallest number that is divisible by both num1 and num2.
+// Write a function leastCommonMultiple(num, num2)
+// that accepts two numbers as arguments.
+// The functions should return the smallest number that is divisible by both num1 and num2.
 
+let leastCommonMultiple = function (num, max) {
+
+};
 
 console.log(leastCommonMultiple(4, 6)); // 12
 console.log(leastCommonMultiple(3, 5)); // 15
