d18a1485d8f62266e36dcd622b63559da5ed79e7
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-15 15:08:07 -0400

    Update ☭W2D1-Map-Notes.js

---
+++ App-Academy/W2/W2D1/☭W2D1-Map-Notes.js
@@ -0,0 +1,10 @@
+// Map is like forEach but it results in a new array
+// the return value of the function that's ran is mapped to a respective place in the new array
+
+let a = [1, 2, 3];
+
+let result = a.map(function(el, i, arr) {
+    return el * 2;
+});
+
+console.log(result);
