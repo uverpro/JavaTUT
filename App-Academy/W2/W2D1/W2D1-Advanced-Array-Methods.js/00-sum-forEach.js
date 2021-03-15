d480bbdd02e3228511efe3cbee09fe72fd1f62e0
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-15 19:27:59 -0400

    Update 00-sum-forEach.js

--- App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/00-sum-forEach.js
+++ App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/00-sum-forEach.js
@@ -3,16 +3,10 @@ Write a function `sumForEach(arr)` that accepts an array as an arg.
 The function should return the total sum of all values in the array.
 
 Solve this using Array's `forEach()` method.
-
-Examples:
-
-console.log(sumArray([4, 3, -1, 10])); // 16
-console.log(sumArray([6, 7, 2])); // 15
-console.log(sumArray([])); // 0
 */
 
+function sumForEach (array)
 
-
-console.log(sumArray([4, 3, -1, 10])); // 16
-console.log(sumArray([6, 7, 2])); // 15
-console.log(sumArray([])); // 0
+console.log(sumForEach([4, 3, -1, 10])); // 16
+console.log(sumForEach([6, 7, 2])); // 15
+console.log(sumForEach([])); // 0
