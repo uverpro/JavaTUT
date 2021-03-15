10214a0f098c47aeae974006a27c06a45897dda0
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-15 03:16:09 -0400

    Update Remove-E-Words.js

--- App-Academy/W1/W1D4/Remove-E-Words.js
+++ App-Academy/W1/W1D4/Remove-E-Words.js
@@ -2,3 +2,11 @@
 // Write a function removeEWords(sentence) that accepts a sentence string as an arg.
 // The function should return a new string,
 // containing only the words that don't have the letter "e" in them.
+
+let removeEWords = function (sentence) {
+
+};
+
+
+console.log(removeEWords('What time is it everyone?')); // 'What is it'
+console.log(removeEWords('Enter the building')); // 'building'
