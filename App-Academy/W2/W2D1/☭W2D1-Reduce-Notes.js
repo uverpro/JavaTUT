fb0b0b0c45212c7c5532c8c1cb7976783cdaa6e3
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-15 16:11:16 -0400

    Update ☭W2D1-Reduce-Notes.js

--- App-Academy/W2/W2D1/☭W2D1-Reduce-Notes.js
+++ App-Academy/W2/W2D1/☭W2D1-Reduce-Notes.js
@@ -1,7 +1,11 @@
 let nums = [3, 7, 5, 9];
 
 let sum = nums.reduce(function (accum, num, i, array) {
+    if (num % 3 === 0) {
     return accum + num;
+    } else {
+        return accum;
+    }
 });
 
 console.log(sum);
