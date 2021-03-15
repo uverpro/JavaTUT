7426d09b0fa7d72092b2d4db35714eaf9563d451
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-15 01:05:57 -0400

    Update Least-Common-Multiple.js

--- App-Academy/W1/W1D3/Least-Common-Multiple.js
+++ App-Academy/W1/W1D3/Least-Common-Multiple.js
@@ -4,9 +4,10 @@
 // The functions should return the smallest number that is divisible by both num1 and num2.
 
 let leastCommonMultiple = function (num1, num2) {
-    let common = num2;
-    for (let i = num2; i <= common % num1 === 0 && common % num2 === 0; i++) {
-    return i;
+    for (let i = 1; i <= (num1 * num2); i++) {
+        if (i % num1 === 0 && i % num2 === 0) {
+            return i;
+        }
     }
 };
 
