f27c736be15334f017b06529f133fe1ca1480db3
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-15 02:08:31 -0400

    Create Tripler.js

---
+++ App-Academy/W1/W1D4/Tripler.js
@@ -0,0 +1,4 @@
+// Tripler
+// Write a function tripler(nums) that takes in an array as an arg.
+// The function should return a new array
+// containing three times every number of the original array.
