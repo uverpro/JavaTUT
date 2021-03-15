4639f64b432fd7f0a08a6a28bee30c9c473a8d5e
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-15 03:15:24 -0400

    Create Remove-E-Words.js

---
+++ App-Academy/W1/W1D4/Remove-E-Words.js
@@ -0,0 +1,4 @@
+// Remove E Words
+// Write a function removeEWords(sentence) that accepts a sentence string as an arg.
+// The function should return a new string,
+// containing only the words that don't have the letter "e" in them.
