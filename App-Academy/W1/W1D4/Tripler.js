ac57d7f5aaedf0db88d6daed485b31915fe51788
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-15 02:09:11 -0400

    Update Tripler.js

--- App-Academy/W1/W1D4/Tripler.js
+++ App-Academy/W1/W1D4/Tripler.js
@@ -3,7 +3,9 @@
 // The function should return a new array
 // containing three times every number of the original array.
 
+let tripler = function (nums) {
 
+};
 
 console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
 console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
