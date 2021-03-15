1aa1dfdb12853eee0d36d7f5f9f64c1f1721c1ea
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-15 16:19:58 -0400

    Update ☭W2D1-Reduce-Notes.js

--- App-Academy/W2/W2D1/☭W2D1-Reduce-Notes.js
+++ App-Academy/W2/W2D1/☭W2D1-Reduce-Notes.js
@@ -1,6 +1,12 @@
 let nums = [3, 7, 5, 9];
 
 let sum = nums.reduce(function (accum, num, i, array) {
+    return accum + num;
+});
+
+console.log(sum);
+
+let sum3 = nums.reduce(function (accum, num, i, array) {
     if (num % 3 === 0) {
     return accum + num;
     } else {
@@ -8,4 +14,19 @@ let sum = nums.reduce(function (accum, num, i, array) {
     }
 });
 
-console.log(sum);
+console.log(sum3);
+
+let sumAt100 = nums.reduce(function (accum, num) {
+    return accum + num;
+}, 100);
+
+console.log(sumAt100);
+
+
+
+/*
+    Write a function called returnNames that
+    intakes an array of names and
+    returns a string of "Names: "
+    with all of the names attached after. Must use the .reduce()
+*/
