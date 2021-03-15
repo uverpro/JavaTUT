dd8814141d66f8178ceda9426dd874f5abed2689
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-15 01:33:01 -0400

    Update My-Index-Of-Recall.js

--- App-Academy/W1/W1D4/My-Index-Of-Recall.js
+++ App-Academy/W1/W1D4/My-Index-Of-Recall.js
@@ -3,6 +3,9 @@
 // The function should return the first index where the target is found in the array.
 // If the target is not found, it should return -1. Solve this without using Array.indexOf.
 
+let myIndexOf = function (arr, target) {
+
+};
 
 
 console.log(myIndexOf(['a', 'b', 'c', 'e'], 'c')); // 2
