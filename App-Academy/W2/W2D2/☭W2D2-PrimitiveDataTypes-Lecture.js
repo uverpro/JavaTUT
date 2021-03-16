2797f21608eee4fe6f3af1df1c2472bcbf373169
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-16 12:25:08 -0400

    Create ☭W2D2-PrimitiveDataTypes-Lecture.js

---
+++ App-Academy/W2/W2D2/☭W2D2-PrimitiveDataTypes-Lecture.js
@@ -0,0 +1,14 @@
+// Primtive Data Types vs Reference Data Types
+
+// Primitive Data Types:
+/*
+1. Boolean - True or False
+2. Null - represents the intentional absence of value
+3. Undefined - default return value for many things in JS
+4. Number - actual numbers we use (1, 2, 3, 4, 5)
+5. String - ordered collection of characters ("apple")
+*/
+
+// Reference Data Type:
+// 1. Object
+//    *An Array is also ad type of object!
