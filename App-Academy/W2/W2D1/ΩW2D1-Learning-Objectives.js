a378db42527817d84a59436d8b907f87d7b43f9b
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-16 19:32:54 -0400

    Update ΩW2D1-Learning-Objectives.js

--- App-Academy/W2/W2D1/ΩW2D1-Learning-Objectives.js
+++ App-Academy/W2/W2D1/ΩW2D1-Learning-Objectives.js
@@ -1,14 +1,32 @@
 // Plain Old JS Object Lesson Learning Objectives
 
 // 1. Label variables as either Primitive vs. Reference
+"Strings are Primitive";
+5, 4, 3, 2, 1; // so are numbers
+(A || B); // Don't forget the Booleans
+
+[ Arrays, are, reference, type ];
 
 // 2. Identify when to use . vs [] when accessing values of an object
 
-// 3. Use the obj[key] !== undefined pattern to check if a given variable that contains a key exists in an object
-Utilize Object.keys and Object.values in a function
-Iterate through an object using a for...in loop
-Define a function that utilizes ...rest syntax to accept an arbitrary number of arguments
-Use ...spread syntax for Object literals and Array literals
-Destructure an array to reference specific elements
-Destructure an object to reference specific values
-Write a function that accepts an array as an argument and returns an object representing the count of each character in the array
+
+
+// 3. Use the obj[key] !== undefined pattern
+// to check if a given variable that contains a key exists in an object
+
+
+
+// 4. Utilize Object.keys and Object.values in a function
+
+// 5. Iterate through an object using a for...in loop
+
+// 6. Define a function that utilizes ...rest syntax to accept an arbitrary number of arguments
+
+// 7. Use ...spread syntax for Object literals and Array literals
+
+// 8. Destructure an array to reference specific elements
+
+// 9. Destructure an object to reference specific values
+
+// 10. Write a function that accepts an array as an argument
+// and returns an object representing the count of each character in the array
