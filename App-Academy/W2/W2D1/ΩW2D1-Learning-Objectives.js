8d1165cb8400daa208d1102620a53df0779c4fe9
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-16 17:56:50 -0400

    Create ΩW2D1-Learning-Objectives.js

---
+++ App-Academy/W2/W2D1/ΩW2D1-Learning-Objectives.js
@@ -0,0 +1,14 @@
+// Plain Old JS Object Lesson Learning Objectives
+
+// 1. Label variables as either Primitive vs. Reference
+
+// 2. Identify when to use . vs [] when accessing values of an object
+
+// 3. Use the obj[key] !== undefined pattern to check if a given variable that contains a key exists in an object
+Utilize Object.keys and Object.values in a function
+Iterate through an object using a for...in loop
+Define a function that utilizes ...rest syntax to accept an arbitrary number of arguments
+Use ...spread syntax for Object literals and Array literals
+Destructure an array to reference specific elements
+Destructure an object to reference specific values
+Write a function that accepts an array as an argument and returns an object representing the count of each character in the array
