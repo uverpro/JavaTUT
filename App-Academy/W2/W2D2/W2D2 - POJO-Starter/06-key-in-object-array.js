62f840cbc0fe43d8ee0fe2e4e957f886d4df2fbd
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-16 19:37:04 -0400

    Update 06-key-in-object-array.js

--- App-Academy/W2/W2D2/W2D2 - POJO-Starter/06-key-in-object-array.js
+++ App-Academy/W2/W2D2/W2D2 - POJO-Starter/06-key-in-object-array.js
@@ -1,7 +1,7 @@
 /***********************************************************************
-Write a function `keyInObjectArray(objArray, keyString)` that takes in an array 
-of objects as the first parameter and a string as the second. The 
-`keyInObjectArray` function will return `true` if any of the objects contains 
+Write a function `keyInObjectArray(objArray, keyString)` that takes in an array
+of objects as the first parameter and a string as the second. The
+`keyInObjectArray` function will return `true` if any of the objects contains
 the `keyString` as a key within them, and `false` if not.
 
 
@@ -23,5 +23,7 @@ function keyInObjectArray(objArray, keyString) {
   // your code here
 }
 
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-module.exports = keyInObjectArray;
+keyInObjectArray(objArray, 'planet'); // => true
+keyInObjectArray(objArray, 'age'); // => true
+keyInObjectArray(objArray, 'food'); // => false
+keyInObjectArray(objArray, 'animal'); // => false
