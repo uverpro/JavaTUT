d7149e20cdb7da4186bb81416d5946229e777248
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-16 12:49:18 -0400

    Update ☭W2D2-ObjectDestructuring-Lecture.js

--- App-Academy/W2/W2D2/☭W2D2-ObjectDestructuring-Lecture.js
+++ App-Academy/W2/W2D2/☭W2D2-ObjectDestructuring-Lecture.js
@@ -2,4 +2,23 @@
 
 // Array Destructuring:
 // 1. Destructuring Elements in Order
+
 let array = ["blue", "red", "yellow", "green", "pink"];
+
+// So far:
+// let firstColor = array[0];
+// let secondColor = array[1];
+// console.log(firstColor); // blue
+// console.log(secondColor); // red
+
+let [firstColor, secondColor, thirdColor] = array;
+console.log(firstColor); // blue
+console.log(secondColor); // red
+
+
+
+// Examples:
+let { person, animal } = { person: 'Claude', animal: 'elephant' };
+console.log(person, animal);
+
+let [game, player] = ["State o' Decay", "Cory"];
