f1ef559dffab51800caba4ce73077d144359d477
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-16 19:36:56 -0400

    Update 01-keys-in-object.js

--- App-Academy/W2/W2D2/W2D2 - POJO-Starter/01-keys-in-object.js
+++ App-Academy/W2/W2D2/W2D2 - POJO-Starter/01-keys-in-object.js
@@ -4,19 +4,15 @@ an array of all the keys within that Object.
 
 Do this once using using a `for...in` loop and once using `Object.keys`.
 
-Examples:
-
-let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
-let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
-keysInObject(animals); // => ["dog", "cat", "bison"]
-keysInObject(foods); // => ["apple", "lemon", "mango"]
 ***********************************************************************/
 
 function keysInObject(obj) {
-  // your code here
+
 }
 
 let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
 let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
 keysInObject(animals); // => ["dog", "cat", "bison"]
 keysInObject(foods); // => ["apple", "lemon", "mango"]
+
+// Find the string as a key in object
