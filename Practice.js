7d1d74781e3d69f881f289887e137e075e8a21c7
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-17 19:45:20 -0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,13 +1,19 @@
-function myMap(array, cb) {
-    let mapped = [];
-    for (let i = 0; i < array.length; i++) {
-        let ele = array[i];
-        mapped.push(cb(ele));
-    }
-    return mapped;
-}
+function reject(array, cb) {
+    let items = [];
+    array.forEach(function(el, i) {
+      if (cb(el) === false) {
+        items.push(el);
+      }
+    });
+    return items;
+  }
 
-let result2 = myMap(['run', 'Forrest'], function (el) {
-    return el.toUpperCase() + '!'; });
+  let isEven = function(n) {
+    return n % 2 === 0;
+  };
+  console.log(reject([7, 8, 5, 6, 12, 11], isEven)); // [ 7, 5, 11 ]
 
-console.log(result2);
+  let hasA = function(s) {
+    return s.toLowerCase().includes('a');
+  };
+  console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
