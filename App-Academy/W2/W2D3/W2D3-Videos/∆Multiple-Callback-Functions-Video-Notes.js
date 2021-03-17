fbbf79186eeb95c5e2c5287c2e3e8835185bcbd4
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-17 11:28:56 -0400

    Update ∆Multiple-Callback-Functions-Video-Notes.js

--- App-Academy/W2/W2D3/W2D3-Videos/∆Multiple-Callback-Functions-Video-Notes.js
+++ App-Academy/W2/W2D3/W2D3-Videos/∆Multiple-Callback-Functions-Video-Notes.js
@@ -1,5 +1,11 @@
 // Taking Multiple Callbacks
 
+// Write a function, greaterResult that
+// accepts a number and two callbacks as arguments
+// The function should call both callbacks
+// passing in the number and
+// return the result of the callback which is larger
+
 let greaterResult = function (num, cb1, cb2) {
 
 };
@@ -13,4 +19,4 @@ let squarer = function (n) {
 };
 
 console.log(greaterResult(5, doubler, squarer)); // 25
-console.log(greaterResult(1, douber, squarer)); // 2
+console.log(greaterResult(1, doubler, squarer)); // 2
