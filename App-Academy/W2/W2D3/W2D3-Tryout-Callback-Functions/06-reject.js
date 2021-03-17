934de0b1aa652023c8fdbe0507aa5a2ac7c08750
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-17 18:24:12 -0400

    Update 06-reject.js

--- App-Academy/W2/W2D3/W2D3-Tryout-Callback-Functions/06-reject.js
+++ App-Academy/W2/W2D3/W2D3-Tryout-Callback-Functions/06-reject.js
@@ -20,14 +20,22 @@ let hasA = function(s) {
 };
 console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
 *******************************************************************************/
-
-let reject = function() {
-
+let isEven = function(n) {
+    return n % 2 === 0;
 };
 
+let reject = function(array, cb) {
+    let negatives = [];
+    for (let i = 0; i < array.length; i++) {
+        let el = array[i];
+        if (cb(el) !== true) {
+            negatives.push(el);
+        }
+    }
+    return negatives;
+};
 
-
-
+console.log(reject([7, 8, 5, 6, 12, 11], isEven)); // [ 7, 5, 11 ]
 
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
