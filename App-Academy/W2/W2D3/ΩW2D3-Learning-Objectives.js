d5c964d0d6cc8f2e9f44418ab664e13542ad5af7
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-17 12:28:56 -0400

    Update ΩW2D3-Learning-Objectives.js

--- App-Academy/W2/W2D3/ΩW2D3-Learning-Objectives.js
+++ App-Academy/W2/W2D3/ΩW2D3-Learning-Objectives.js
@@ -1,8 +1,8 @@
 // Callbacks Lesson Learning Objectives
 
-/*
-1. Given multiple plausible reasons, identify why functions are called
-“First Class Objects” in JavaScript. */
+
+// 1. Given multiple plausible reasons, identify why functions are called
+// “First Class Objects” in JavaScript. */
 
 `
 Stored in a variable, object, or array
@@ -10,20 +10,17 @@ Passed as an argument into a function
 Returns from a function
 `
 
-/*
-2. Given a code snippet containing an anonymous callback, a named callback,
-and multiple console.logs, predict what will be printed
+// 2. Given a code snippet containing an anonymous callback, a named callback,
+// and multiple console.logs, predict what will be printed
 
-3. Write a function that takes in a value and two callbacks.
-The function should return the result of the callback that is greater.
+// 3. Write a function that takes in a value and two callbacks.
+// The function should return the result of the callback that is greater.
 
-4. Write a function, myMap, that takes in an array and a callback as arguments.
-The function should mimic the behavior of Array.map.
+// 4. Write a function, myMap, that takes in an array and a callback as arguments.
+// The function should mimic the behavior of Array.map.
 
-5. Write a function, myFilter, that takes in an array and a callback as arguments.
-The function should mimic the behavior of Array.filter.
+// 5. Write a function, myFilter, that takes in an array and a callback as arguments.
+// The function should mimic the behavior of Array.filter.
 
-6. Write a function, myEvery, that takes in an array and a callback as arguments.
-The function should mimic the behavior of Array.every.
-
-*/
+// 6. Write a function, myEvery, that takes in an array and a callback as arguments.
+// The function should mimic the behavior of Array.every.
