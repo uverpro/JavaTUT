e26468312bf46a7878d262d3f71f320a3c74dda4
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-17 11:26:43 -0400

    Update ∆Multiple-Callback-Functions-Video-Notes.js

--- App-Academy/W2/W2D3/W2D3-Videos/∆Multiple-Callback-Functions-Video-Notes.js
+++ App-Academy/W2/W2D3/W2D3-Videos/∆Multiple-Callback-Functions-Video-Notes.js
@@ -5,11 +5,11 @@ let greaterResult = function (num, cb1, cb2) {
 };
 
 let doubler = function (n) {
-
+    return n * 2;
 };
 
 let squarer = function (n) {
-
+    return n * n;
 };
 
 console.log(greaterResult(5, doubler, squarer)); // 25
