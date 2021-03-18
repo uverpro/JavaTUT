4000158288228b5d848b90f9657ba4488055a735
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-18 03:37:52 -0400

    Update Object.keys.js

--- Methods/Object Methods/Object.keys.js
+++ Methods/Object Methods/Object.keys.js
@@ -3,3 +3,12 @@
 // The Object.keys() method returns an array
 // of a given object's own enumerable property names,
 // iterated in the same order that a normal loop would.
+
+const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+
+  console.log(Object.keys(object1));
+  // expected output: Array ["a", "b", "c"]
