1c29f33343651200690c88a0f93716c9fc350c28
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-18 11:42:35 -0400

    Update 02-values-in-object.js

--- App-Academy/W2/W2D2/W2D2 - POJO-Starter/02-values-in-object.js
+++ App-Academy/W2/W2D2/W2D2 - POJO-Starter/02-values-in-object.js
@@ -1,22 +1,17 @@
 /***********************************************************************
-Write a function called `valuesInObject(obj)` that takes in an object and returns 
-an array of all the values within that Object. 
+Write a function called `valuesInObject(obj)` that takes in an object and returns
+an array of all the values within that Object.
 
 
 Do this once using using a `for...in` loop and once using `Object.values`.
+***********************************************************************/
 
-
-Examples:
+function valuesInObject(obj) {
+  for (value in obj)
+  console.log(value)
+}
 
 let animals = {dog: "Wolfie", cat: "Jet", bison: "Bilbo"}
 let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
 valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
 valuesInObject(foods); // => ["tart", "sour", "sweet"]
-***********************************************************************/
-
-function valuesInObject(obj) {
-  // your code here
-}
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-module.exports = valuesInObject;
