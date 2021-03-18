8c1a35dc760d6feb41e13b04a0da85ef47f879a1
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-18 18:51:56 -0400

    Update 01-arrow-full-name.js

--- App-Academy/W2/W2D4/W2D4-Scope-Problems/01-arrow-full-name.js
+++ App-Academy/W2/W2D4/W2D4-Scope-Problems/01-arrow-full-name.js
@@ -14,9 +14,10 @@ arrowGetFullName(p2); // => 'Charlie Brown'
 
 ***********************************************************************/
 
-
+const arrowGetFullName = person => `${person.firstName} ${person.lastName}`
+// const arrowGetFullName = person => person.firstName + " " + person.lastName
 
 let p1 = {firstName: 'John', lastName: 'Doe'};
-arrowGetFullName(p1); // => 'John Doe'
+console.log(arrowGetFullName(p1)); // => 'John Doe'
 let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
-arrowGetFullName(p2); // => 'Charlie Brown'
+console.log(arrowGetFullName(p2)); // => 'Charlie Brown'
