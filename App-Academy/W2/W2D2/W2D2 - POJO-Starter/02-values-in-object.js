597781cec62eed8263308bf6ab0ea1748f97bfc2
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-18 11:48:20 -0400

    Update 02-values-in-object.js

--- App-Academy/W2/W2D2/W2D2 - POJO-Starter/02-values-in-object.js
+++ App-Academy/W2/W2D2/W2D2 - POJO-Starter/02-values-in-object.js
@@ -7,8 +7,22 @@ Do this once using using a `for...in` loop and once using `Object.values`.
 ***********************************************************************/
 
 function valuesInObject(obj) {
-  for (value in obj)
-  console.log(value)
+  let valueArray = [];
+  for (value in obj) {
+    valueArray.push(obj[value]);
+  }
+  return valueArray; // console.log(valueArray)
+}
+
+let animals = {dog: "Wolfie", cat: "Jet", bison: "Bilbo"}
+let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
+valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
+valuesInObject(foods); // => ["tart", "sour", "sweet"]
+
+
+
+function valuesInObject(obj) {
+  return Object.values(obj); // console.log(Object.values(obj))
 }
 
 let animals = {dog: "Wolfie", cat: "Jet", bison: "Bilbo"}
