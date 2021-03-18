70a3a99b0c610704b7db277f490fd6f10f823618
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-17 20:01:35 -0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,19 +1,5 @@
-function reject(array, cb) {
-    let items = [];
-    array.forEach(function(el, i) {
-      if (cb(el) === false) {
-        items.push(el);
-      }
-    });
-    return items;
-  }
+let arr = [1, 2, 3]
 
-  let isEven = function(n) {
-    return n % 2 === 0;
-  };
-  console.log(reject([7, 8, 5, 6, 12, 11], isEven)); // [ 7, 5, 11 ]
-
-  let hasA = function(s) {
-    return s.toLowerCase().includes('a');
-  };
-  console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
+arr.forEach(function(el, i) {
+    console.log(`${el} is at index ${i}`)
+})
