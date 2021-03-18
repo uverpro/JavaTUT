0a177ed93dadddd6e45c498ce38c0022b103df44
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-18 13:26:36 -0400

    Update 06-sum-reduce.js

--- App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/06-sum-reduce.js
+++ App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/06-sum-reduce.js
@@ -4,19 +4,15 @@ Write a function `sumWithReduce(nums)` that takes in an array of numbers.
 The function should return the total sum of all numbers in the array.
 
 Solve this using Array's `reduce()` method.
-
-Examples:
-
-console.log(sumWithReduce([10, 3, 5, 2])); // 20
-console.log(sumWithReduce([])); // 0
-
 */
 
-let sumWithReduce = function (nums) {
-  let sum = nums.reduce(function (accum, num, i, array){
-    return sum += num;
-  });
-}
+let sumWithReduce = function(nums) {
+  let total = nums.reduce(function(sum, num) {
+    return sum + num;
+}, 0);
+  return total;
+};
+
 
 console.log(sumWithReduce([10, 3, 5, 2])); // 20
 console.log(sumWithReduce([])); // 0
