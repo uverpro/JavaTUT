a72713b4ada7dd0c1e7969ca61b65d762fa3008d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-18 11:15:15 -0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -2,9 +2,15 @@ let arr = [1, 1, 3, 2, 2, 1];
 
 const result = arr.reduce(function(acc, value) {
   if (acc[value]) {
+      console.log(acc[value])
     acc[value]++;
+    console.log(acc[value])
   } else {
     acc[value] = 1;
+    console.log(acc[value])
   }
+  console.log(acc)
   return acc;
 }, {});
+
+console.log(result);
