c10f88bc88d59bb0ad3e483fbfd6c9e6c1ef16c1
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-19 19:45:48 -0400

    Update String.startWith.js

--- Methods/String Methods/String.startWith.js
+++ Methods/String Methods/String.startWith.js
@@ -4,3 +4,11 @@
 The startsWith() method determines whether a string begins with the characters
 of a specified string, returning true or false as appropriate.
 */
+
+const str1 = 'Saturday night plans';
+
+console.log(str1.startsWith('Sat'));
+// expected output: true
+
+console.log(str1.startsWith('Sat', 3));
+// expected output: false
