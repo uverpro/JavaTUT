c037775d7255185aee75626d358d1397e8aabc7c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-19 11:19:48 -0400

    Create 01-unique-keys.js

---
+++ App-Academy/W2/W2 - Assessment/problems/01-unique-keys.js
@@ -0,0 +1,35 @@
+/***********************************************************************
+
+Write a function  `uniqueKeys` that accepts two objects and returns an array of
+keys that are present in the first object but not in both objects.
+
+Examples:
+
+const cat = { legs: 4, favoriteFood: 'catnip', cuteness: 100, sound: 'meow'};
+const human = { legs: 4, favoriteFood: 'sushi', money: 0};
+uniqueKeys(cat, human); // ['cuteness', 'sound']
+
+const guitar = { strings: 4, volume: 11, color: 'blue' };
+const drums = { volume: 8, color: 'red' };
+uniqueKeys(guitar, drums); // ['strings']
+***********************************************************************/
+
+// function uniqueKeys(obj1, obj2) {
+//     let nuArray = [];
+//     for (let key in obj1)
+//     if obj1 [key] === obj2 [key] {
+
+//     } else
+//     nuArray.push obj1 [key];
+// }
+
+// const cat = { legs: 4, favoriteFood: 'catnip', cuteness: 100, sound: 'meow'};
+// const human = { legs: 4, favoriteFood: 'sushi', money: 0};
+// uniqueKeys(cat, human); // ['cuteness', 'sound']
+
+// const guitar = { strings: 4, volume: 11, color: 'blue' };
+// const drums = { volume: 8, color: 'red' };
+// uniqueKeys(guitar, drums); // ['strings']
+
+// /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+// module.exports = uniqueKeys;
