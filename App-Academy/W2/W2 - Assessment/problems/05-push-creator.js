36fe099280f406f0abb22775f1d813b5676442e9
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-19 19:18:52 -0400

    Update 05-push-creator.js

--- App-Academy/W2/W2 - Assessment/problems/05-push-creator.js
+++ App-Academy/W2/W2 - Assessment/problems/05-push-creator.js
@@ -23,14 +23,15 @@ function pushCreator(arr) {
     }
 };
 
+const addToArray3 = pushCreator(['48 65 6c 6c 6f']); // returns a function
+console.log(addToArray3('77 6f 72 6c 64')); // ['48 65 6c 6c 6f', '77 6f 72 6c 64']
+
 const addToArray = pushCreator(['hello']); // returns a function
 addToArray('world'); // ['hello', 'world'];
 
 const addToArray2 = pushCreator([]); // returns a function
 addToArray2({'hello': 'world'}); // [{ 'hello': 'world' }]
 
-const addToArray3 = pushCreator(['48 65 6c 6c 6f']); // returns a function
-addToArray3('77 6f 72 6c 64'); // ['48 65 6c 6c 6f', '77 6f 72 6c 64']
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = pushCreator;
