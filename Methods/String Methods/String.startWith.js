a0c2f835c1b0e439fe8eb1129e0db33083a5d480
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-19 19:45:14 -0400

    Create String.startWith.js

---
+++ Methods/String Methods/String.startWith.js
@@ -0,0 +1,6 @@
+// String.prototype.startsWith()
+
+/*
+The startsWith() method determines whether a string begins with the characters
+of a specified string, returning true or false as appropriate.
+*/
