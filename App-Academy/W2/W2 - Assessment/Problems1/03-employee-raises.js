a8fb52b8ac52c61d68430337289508feb1b2fb47
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-20 19:48:58 -0400

    Create 03-employee-raises.js

---
+++ App-Academy/W2/W2 - Assessment/Problems1/03-employee-raises.js
@@ -0,0 +1,29 @@
+/*******************************************************************************
+
+Your company is giving every employee earning less than $50,000 a 10% raise!
+
+Write a function `employeeRaises(peopleObj)` that takes an array of objects
+containing employee names and their salaries and returns an array containing
+the employees that need raises, along with their new salaries.
+
+Hint: It might be useful to create helper functions for this task.
+
+Example:
+
+employees = [{name: "Alice", salary: 80000},
+             {name: "Bob", salary: 40000},
+             {name: "Carol", salary: 60000},
+             {name: "Dan", salary: 70000},
+             {name: "Ellen", salary: 100000}]
+
+console.log(employeeRaises(employees)); // [ { name: 'Bob', salary: 44000 } ]
+*******************************************************************************/
+
+
+function employeeRaises(employees) {
+    // Your code here
+}
+
+
+/*****************DO NOT MODIFY ANYTHING UNDER THIS LINE**********************/
+module.exports = employeeRaises;
