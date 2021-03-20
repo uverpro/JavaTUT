1fb6073fd753dfda9bdf72e56f001f3471dce6c7
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-20 19:48:45 -0400

    Create 01-key-value-duplicates.js

---
+++ App-Academy/W2/W2 - Assessment/Problems1/01-key-value-duplicates.js
@@ -0,0 +1,26 @@
+/***********************************************************************
+Write a function `keyValueDuplicates(obj)` that takes an object as
+an argument and returns an array containing all keys that are also
+values in that object.
+
+Examples:
+
+obj1 = {"orange": "juice", "apple": "sauce", "sauce": "pan"}
+keyValueDuplicates(obj1) // ["sauce"]
+
+obj2 = {"big": "foot", "foot": "ball", "ball": "boy", "boy": "scout"}
+keyValueDuplicates(obj2) // ["foot", "ball", "boy"]
+
+obj3 = {"pizza": "pie", "apple": "pie", "pumpkin": "pie"}
+keyValueDuplicates(obj3) // []
+
+
+***********************************************************************/
+
+function keyValueDuplicates(obj) {
+	// Your code here
+}
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = keyValueDuplicates;
