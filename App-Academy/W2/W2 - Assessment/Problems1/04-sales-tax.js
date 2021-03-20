019873f649fac50feb1e837e37d98427fe4ea513
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-20 19:49:26 -0400

    Create 04-sales-tax.js

---
+++ App-Academy/W2/W2 - Assessment/Problems1/04-sales-tax.js
@@ -0,0 +1,22 @@
+/***********************************************************************
+Write a function using fat arrow syntax, `salesTax` that takes in an arbitrary
+number of arguments as prices, adds them up and returns a string containing the
+sales tax with a dollar sign in front. Use a tax percentage of 9%.
+
+Hint: Look up `toFixed()` on MDN
+
+Examples:
+
+salesTax(30, 70); // '$9.00'
+salesTax(1.12, 2.23, 3.34, 4.45); // '$1.00'
+salesTax(4.99, 9.99, 19.99); // '$3.15'
+***********************************************************************/
+
+// your code here
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+    module.exports = salesTax;
+} catch(e) {
+    module.exports = null;
+}
