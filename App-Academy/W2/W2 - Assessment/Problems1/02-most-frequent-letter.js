c87e2f12f0fbdfd099b1df176da7bfe0c514d882
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-20 19:48:52 -0400

    Create 02-most-frequent-letter.js

---
+++ App-Academy/W2/W2 - Assessment/Problems1/02-most-frequent-letter.js
@@ -0,0 +1,19 @@
+/***********************************************************************
+Write a function `mostFrequentLetter(string)` that will takes a
+string as an argument and returns the character that appears the
+most often. In case of a tie, you may return any of the characters.
+The string will have at least one character.
+
+Examples:
+
+mostFrequentLetter("apple") // "p"
+mostFrequentLetter("banana") // "a"
+mostFrequentLetter("What about a longer string?") // " "
+***********************************************************************/
+
+function mostFrequentLetter(string) {
+	// Your code here
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = mostFrequentLetter;
