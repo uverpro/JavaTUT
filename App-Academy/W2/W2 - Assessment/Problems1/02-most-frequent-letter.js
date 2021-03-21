e49c0b7172fffe22021d9728af2283f34cce22a6
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-20 23:12:17 -0400

    Update 02-most-frequent-letter.js

--- App-Academy/W2/W2 - Assessment/Problems1/02-most-frequent-letter.js
+++ App-Academy/W2/W2 - Assessment/Problems1/02-most-frequent-letter.js
@@ -8,20 +8,28 @@ The string will have at least one character.
 function mostFrequentLetter(string) {
 	let currentCount = {};
 	let highestNum = null;
+	let mostFrequentLetter = "";
 
 	for (let i = 0; i < string.length; i++) {
-		let char = string[i];
-		currentCount[char] = currentCount[char] || 0;
-		currentCount[char] += 1;
+		let char = string[i]
+		currentCount[char] = currentCount[char] || 0
+		currentCount[char] += 1
+		console.log(currentCount[char]);
+		console.log(currentCount)
+		console.log("BREAK")
 	}
 	for (letter in currentCount) {
-		if ((currentCount[letter] >= highestNum || (highestNum === bull)) {
-			highestNum = letter;
+		console.log(letter);
+		console.log("---")
+		console.log(currentCount[letter])
+		if (currentCount[letter] >= highestNum) {
+			highestNum = currentCount[letter];
+			mostFrequentLetter = letter;
 		}
 	}
-	return highestNum;
+	return mostFrequentLetter;
 };
 
-mostFrequentLetter("apple") // "p"
-mostFrequentLetter("banana") // "a"
-mostFrequentLetter("What about a longer string?") // " "
+console.log(mostFrequentLetter("apple")); // "p"
+console.log(mostFrequentLetter("banana")); // "a"
+// console.log(mostFrequentLetter("What about a longer string?")); // " "
