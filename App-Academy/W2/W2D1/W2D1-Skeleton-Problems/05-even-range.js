7b2eb39a3d7b499da3f3ad7198d1b6ce13e7d3b0
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 18:56:56 -0400

    Update 05-even-range.js

--- App-Academy/W2/W2D1/W2D1-Skeleton-Problems/05-even-range.js
+++ App-Academy/W2/W2D1/W2D1-Skeleton-Problems/05-even-range.js
@@ -1,12 +1,6 @@
 /*******************************************************************************
 Write a function evenRange(start, end) that returns an array containing all even
 numbers between 'start' and 'end' in sequential order.
-
-Examples:
-
-evenRange(13, 20) => [ 14, 16, 18, 20 ]
-evenRange(4, 11) => [ 4, 6, 8, 10 ]
-evenRange(8, 5) => []
 *******************************************************************************/
 
 function evenRange(start, end) {
@@ -22,6 +16,3 @@ function evenRange(start, end) {
 console.log(evenRange(13, 20)); // => [ 14, 16, 18, 20 ]
 console.log(evenRange(4, 11)); // => [ 4, 6, 8, 10 ]
 console.log(evenRange(8, 5)); // => []
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
-module.exports = evenRange;
