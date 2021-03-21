9ac09af2246e266c3dc114802e850f0b18addbfb
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 18:10:18 -0400

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,12 @@
+/***********************************************************************
+Write a function `avgValue(array)` that takes in an array of numbers and
+returns the average number.
+***********************************************************************/
+
+function avgValue(array) {
+    // your code here...
+}
+
+avgValue([10, 20]); // => 15
+avgValue([2, 3, 7]); // => 4
+avgValue([100, 60, 64]); // => 74.66666666666667
