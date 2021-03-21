630df02773d2962a6b6772e5a9989f3cbedefc60
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 18:33:20 -0400

    Update 02-avg-value.js

--- App-Academy/W2/W2D1/W2D1-Skeleton-Problems/02-avg-value.js
+++ App-Academy/W2/W2D1/W2D1-Skeleton-Problems/02-avg-value.js
@@ -1,17 +1,26 @@
 /***********************************************************************
 Write a function `avgValue(array)` that takes in an array of numbers and
 returns the average number.
-
-Examples:
-
-avgValue([10, 20]); // => 15
-avgValue([2, 3, 7]); // => 4
-avgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
 function avgValue(array) {
-  // your code here...
+  return array.reduce(function(accum, num){
+      return accum + num / array.length;
+  }, 0); // <== have to set accum to 0
 }
 
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-module.exports = avgValue;
+console.log(avgValue([10, 20])); // => 15
+console.log(avgValue([2, 3, 7])); // => 4
+console.log(avgValue([100, 60, 64])); // => 74.66666666666667
+
+// function avgValue(array) {
+//   let total = 0;
+
+//   for (let i = 0; i < array.length; i += 1) {
+//     let num = array[i];
+//     total += num;
+//   }
+
+//   let avg = total / array.length;
+//   return avg;
+// }
