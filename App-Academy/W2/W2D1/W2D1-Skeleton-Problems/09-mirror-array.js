191bf8bbfba7bc890c111aad9b1a730b813d50a4
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 19:52:18 -0400

    Update 09-mirror-array.js

--- App-Academy/W2/W2D1/W2D1-Skeleton-Problems/09-mirror-array.js
+++ App-Academy/W2/W2D1/W2D1-Skeleton-Problems/09-mirror-array.js
@@ -4,12 +4,32 @@ argument and returns a new array "mirrored" as shown in the examples:
 ***********************************************************************/
 
 function mirrorArray(array) {
-  let mirroredArray = array.slice(0, array.length);
+  let mirroredArray = array.slice(0, array.length); // [ 1, 2, 3 ]
   for (let i = array.length - 1; i >= 0; i--) {
     let element = array[i];
-    mirroredArray.push(element);
+    mirroredArray.push(element); // 3, 2, 1
   }
   return mirroredArray;
 }
 
-console.log(mirrorArray([1, 2, 3]));
+console.log(mirrorArray([1,2,3])); // => [ 1, 2, 3, 3, 2, 1 ]
+console.log(mirrorArray(['a', 'b', 'c', 'd'])); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
+
+
+
+// long solution
+function mirrorArray(array) {
+  let newArray = [];
+
+  for (let i = 0; i < array.length; i += 1) {
+    let el = array[i];
+    newArray.push(el);
+  }
+
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    let el = array[i];
+    newArray.push(el);
+  }
+
+  return newArray;
+}
