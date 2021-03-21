34868b40ce380edeb3d0587f8baf51b165b5a5e3
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 19:33:47 -0400

    Update 07-intersect-arrays.js

--- App-Academy/W2/W2D1/W2D1-Skeleton-Problems/07-intersect-arrays.js
+++ App-Academy/W2/W2D1/W2D1-Skeleton-Problems/07-intersect-arrays.js
@@ -3,16 +3,18 @@ Write a function intersect(arr1, arr2) that takes in two arrays and returns a
 new array containing the elements common to both arr1 and arr2.
 
 Hint: use indexOf
-
-Examples:
-
-intersect(['a', 'b', 'c', 'd'], ['b', 'd', 'e']) => [ 'b', 'd' ]
-intersect(['a', 'b', 'c'], ['x', 'y', 'z']) => []
 *******************************************************************************/
 
 function intersect(arr1, arr2) {
-  // your code here...
+  let inter = [];
+  for (let i = 0; i < arr1.length; i++) {
+    let el = arr1[i];
+    if (arr2.includes(el)) { // if (arr2.indexOf(el) > -1) {
+      inter.push(el);
+    }
+  }
+  return inter;
 }
 
-intersect(['a', 'b', 'c', 'd'], ['b', 'd', 'e']) // => [ 'b', 'd' ]
-intersect(['a', 'b', 'c'], ['x', 'y', 'z']) // => []
+console.log(intersect(['a', 'b', 'c', 'd'], ['b', 'd', 'e'])); // => [ 'b', 'd' ]
+console.log(intersect(['a', 'b', 'c'], ['x', 'y', 'z'])); // => []
