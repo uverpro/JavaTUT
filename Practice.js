6fdbf564bd0b9b94c608a9fd580a3399564f5879
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 05:04:20 -0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,6 +1,28 @@
 /*
-Write a function `sumForEach(arr)` that accepts an array as an arg.
-The function should return the total sum of all values in the array.
+Write a function `sumForEach(arr)`
+that accepts an array as an arg.
+The function should return the total sum
+ of all values in the array.
 
 Solve this using Array's `forEach()` method.
-*/s
+*/
+
+
+sumForEach(['a', 'b', 'c'], function (el, i) {
+  console.log(el + ' is at index ' + i);
+}); // prints
+// a is at index 0
+// b is at index 1
+// c is at index 2
+
+
+let letters = ['a', 'b', 'c']
+
+let myForEach = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let el = arr[i]
+        console.log(arr[i], i);
+    }
+};
+
+myForEach(['a', 'b', 'c'])
