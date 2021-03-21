e785fdef2930e00e0390e9dcfcd506355e07c84e
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 18:33:27 -0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -4,9 +4,20 @@ returns the average number.
 ***********************************************************************/
 
 function avgValue(array) {
-    // your code here...
+    return array.reduce(function(accum, num){
+        return accum + num / array.length;
+    }, 0);
 }
 
-avgValue([10, 20]); // => 15
-avgValue([2, 3, 7]); // => 4
-avgValue([100, 60, 64]); // => 74.66666666666667
+console.log(avgValue([10, 20])); // => 15
+console.log(avgValue([2, 3, 7])); // => 4
+console.log(avgValue([100, 60, 64])); // => 74.66666666666667
+
+
+
+var array = [129, 139, 155, 176],
+    average = array.reduce(function (avg, value, _, { length }) {
+        return avg + value / length;
+    }, 0);
+
+console.log(average);
