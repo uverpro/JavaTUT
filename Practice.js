6fc224c59f7c7e25d2d6250d87c9d733e6f6dcc2
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-20 21:55:41 -0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -3,4 +3,4 @@ Write a function `sumForEach(arr)` that accepts an array as an arg.
 The function should return the total sum of all values in the array.
 
 Solve this using Array's `forEach()` method.
-*/
+*/s
