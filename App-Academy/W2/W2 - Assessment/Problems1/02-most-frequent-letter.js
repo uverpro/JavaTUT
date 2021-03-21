5a3e7fc76aa523073eed8f8ca4c949788a176b0f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-20 22:47:58 -0400

    Update 02-most-frequent-letter.js

--- App-Academy/W2/W2 - Assessment/Problems1/02-most-frequent-letter.js
+++ App-Academy/W2/W2 - Assessment/Problems1/02-most-frequent-letter.js
@@ -3,17 +3,25 @@ Write a function `mostFrequentLetter(string)` that will takes a
 string as an argument and returns the character that appears the
 most often. In case of a tie, you may return any of the characters.
 The string will have at least one character.
+***********************************************************************/
 
-Examples:
+function mostFrequentLetter(string) {
+	let currentCount = {};
+	let highestNum = null;
+
+	for (let i = 0; i < string.length; i++) {
+		let char = string[i];
+		currentCount[char] = currentCount[char] || 0;
+		currentCount[char] += 1;
+	}
+	for (letter in currentCount) {
+		if ((currentCount[letter] >= highestNum || (highestNum === bull)) {
+			highestNum = letter;
+		}
+	}
+	return highestNum;
+};
 
 mostFrequentLetter("apple") // "p"
 mostFrequentLetter("banana") // "a"
 mostFrequentLetter("What about a longer string?") // " "
-***********************************************************************/
-
-function mostFrequentLetter(string) {
-	// Your code here
-}
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-module.exports = mostFrequentLetter;
