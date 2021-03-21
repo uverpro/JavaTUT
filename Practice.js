475052d3fa6899f8db819005c6ff91d813c7072f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 19:59:17 -0400

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,19 @@
+// The initial value for total sales is zero
+let totalSales = 0;
+
+// Function to add a value to the total sales
+const addToTotalSales = function (value) {
+   totalSales += value;
+}
+
+// Some sales numbers to experiment with
+const monthlySales = [1234, 2345, 3456, 4567, 5678];
+
+// Loop through all sales numbers to add them to the total
+for (let i=0; i<monthlySales.length; i++) {
+    addToTotalSales(monthlySales[i]);
+}
+
+// Output the total to the console
+console.log('Total Sales are', totalSales);
+// Expected result: Total Sales are 17280
