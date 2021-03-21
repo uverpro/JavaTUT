c173288e4694c6b9deeb483e649980bce5db76f3
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-20 22:38:52 -0400

    Update 01-key-value-duplicates.js

--- App-Academy/W2/W2 - Assessment/Problems1/01-key-value-duplicates.js
+++ App-Academy/W2/W2 - Assessment/Problems1/01-key-value-duplicates.js
@@ -3,20 +3,24 @@ Write a function `keyValueDuplicates(obj)` that takes an object as
 an argument and returns an array containing all keys that are also
 values in that object.
 
-Examples:
+***********************************************************************/
+
+function keyValueDuplicates(obj) {
+	let dupeArray = [];
+	for (let currentKey in obj) {
+		let value = obj[currentKey]
+		if (Object.keys(obj).includes(value)) {
+			dupeArray.push(value);
+		}
+	}
+	return dupeArray;
+}
 
 obj1 = {"orange": "juice", "apple": "sauce", "sauce": "pan"}
-keyValueDuplicates(obj1) // ["sauce"]
+console.log(keyValueDuplicates(obj1)); // ["sauce"]
 
 obj2 = {"big": "foot", "foot": "ball", "ball": "boy", "boy": "scout"}
 keyValueDuplicates(obj2) // ["foot", "ball", "boy"]
 
 obj3 = {"pizza": "pie", "apple": "pie", "pumpkin": "pie"}
 keyValueDuplicates(obj3) // []
-
-
-***********************************************************************/
-
-function keyValueDuplicates(obj) {
-	// Your code here
-}
