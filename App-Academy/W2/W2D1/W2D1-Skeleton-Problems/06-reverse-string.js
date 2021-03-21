4f48e21bd5bda825c0d78096d3349fa545ea56e8
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 19:10:24 -0400

    Update 06-reverse-string.js

--- App-Academy/W2/W2D1/W2D1-Skeleton-Problems/06-reverse-string.js
+++ App-Academy/W2/W2D1/W2D1-Skeleton-Problems/06-reverse-string.js
@@ -1,15 +1,15 @@
 /******************************************************************************
 Write a function reverseString(string) that takes in a hyphenated string and
 returns a the hyphenated string reversed.
-
-Examples:
-
-reverseString("Go-to-the-store") => "store-the-to-Go"
-reverseString("Jump,-jump-for-joy") => "joy-for-jump-Jump,"
 *******************************************************************************/
 
 function reverseString(string) {
+  let words = string.split("-"); // [ 'Go', 'to', 'the', 'store' ]
+  console.log(words)
+  let reversed = [];
+  for (let i = string.length - 1; i >= 0; i--) {
 
+  }
 }
 
 console.log(reverseString("Go-to-the-store")); // => "store-the-to-Go"
