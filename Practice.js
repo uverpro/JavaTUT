03fe26ab36f3dcd841a70df7d54c5b81ef76e0d9
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 18:07:45 -0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -14,12 +14,15 @@ diffArrayLen(a1, a3); // => true
 ***********************************************************************/
 
 function diffArrayLen(arr1, arr2) {
-    // your code here...
+    if (arr1.length !== arr2.length) {
+        return true;
+    }
+    return false;
   }
 
   let a1 = ['a', 'b', 'c'];
 let a2 = ['w', 'x', 'y'];
 let a3 = [1, 3, 7, 4];
 
-diffArrayLen(a1, a2); // => false
-diffArrayLen(a1, a3); // => true
+console.log(diffArrayLen(a1, a2)); // => false
+console.log(diffArrayLen(a1, a3)); // => true
