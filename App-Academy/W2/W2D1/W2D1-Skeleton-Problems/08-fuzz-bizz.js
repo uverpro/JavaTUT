8c3a522182355e1da53a2adfb3ea4947450b7139
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 19:45:49 -0400

    Update 08-fuzz-bizz.js

--- App-Academy/W2/W2D1/W2D1-Skeleton-Problems/08-fuzz-bizz.js
+++ App-Academy/W2/W2D1/W2D1-Skeleton-Problems/08-fuzz-bizz.js
@@ -1,11 +1,6 @@
 /******************************************************************************
 Write a function fuzzBizz(max) that returns an array of numbers under
 the max. Each number should be either divisible by 2 or 7, BUT NOT BOTH.
-
-Examples:
-
-fuzzBizz(17) => [ 2, 4, 6, 7, 8, 10, 12, 16 ]
-fuzzBizz(30) => [ 2, 4, 6, 7, 8, 10, 12, 16, 18, 20, 21, 22, 24, 26 ]
 *******************************************************************************/
 
 function fuzzBizz(max) {
@@ -19,5 +14,5 @@ function fuzzBizz(max) {
   return buzzed;
 }
 
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
-module.exports = fuzzBizz;
+console.log(fuzzBizz(17)); // => [ 2, 4, 6, 7, 8, 10, 12, 16 ]
+console.log(fuzzBizz(30)); // => [ 2, 4, 6, 7, 8, 10, 12, 16, 18, 20, 21, 22, 24, 26 ]
