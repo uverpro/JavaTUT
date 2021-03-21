4dd753517ab920708638fdd177e6a98e537a3dc7
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 18:56:12 -0400

    Update 05-even-range.js

--- App-Academy/W2/W2D1/W2D1-Skeleton-Problems/05-even-range.js
+++ App-Academy/W2/W2D1/W2D1-Skeleton-Problems/05-even-range.js
@@ -10,8 +10,18 @@ evenRange(8, 5) => []
 *******************************************************************************/
 
 function evenRange(start, end) {
-  // your code here...
+  let evens = [];
+  for (let i = start; i <= end; i++) {
+    if (i % 2 === 0) {
+      evens.push(i);
+    }
+  }
+  return evens;
 }
+
+console.log(evenRange(13, 20)); // => [ 14, 16, 18, 20 ]
+console.log(evenRange(4, 11)); // => [ 4, 6, 8, 10 ]
+console.log(evenRange(8, 5)); // => []
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
 module.exports = evenRange;
