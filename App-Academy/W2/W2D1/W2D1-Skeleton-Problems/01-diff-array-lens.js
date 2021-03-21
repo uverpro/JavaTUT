4377d358c426fcc42d13cba9593e70ac8a8ffa83
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 18:07:40 -0400

    Update 01-diff-array-lens.js

--- App-Academy/W2/W2D1/W2D1-Skeleton-Problems/01-diff-array-lens.js
+++ App-Academy/W2/W2D1/W2D1-Skeleton-Problems/01-diff-array-lens.js
@@ -4,15 +4,16 @@ function should return true if the arrays have different lengths. The
 function should return false otherwise.
 ***********************************************************************/
 
+function diffArrayLen(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return true;
+    }
+        return false;
+};
+
 let a1 = ['a', 'b', 'c'];
 let a2 = ['w', 'x', 'y'];
 let a3 = [1, 3, 7, 4];
-
-function diffArrayLen(arr1, arr2) {
-
-}
-
-
 
 console.log(diffArrayLen(a1, a2)); // => false
 console.log(diffArrayLen(a1, a3)); // => true
