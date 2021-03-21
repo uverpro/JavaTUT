6750cc9bcd07a98563e00951c1738f28750b3243
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-20 20:26:30 -0400

    Update 05-dynamic-adder.js

--- App-Academy/W2/W2 - Assessment/Problems1/05-dynamic-adder.js
+++ App-Academy/W2/W2 - Assessment/Problems1/05-dynamic-adder.js
@@ -2,25 +2,23 @@
 Write a function called `dynamicAdder(num)`. The dynamicAdder function will
 return a new function that will allow us to create new separate custom
 adding functions.
-
-
-Look below to see how this function is invoked:
-
-const addTwo = dynamicAdder(2); // returns a function
-addTwo(5); // returns 7
-
-const addTen = dynamicAdder(10);
-addTen(5); // returns 15
-
-const addNinety = dynamicAdder(90);
-addNinety(5); // returns 95
-
-
 ***********************************************************************/
 
-function dynamicAdder(num) {
-    // Your code here
-}
+// function dynamicAdder(num) {
+//     return el => (el + num)
+// };
 
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-module.exports = dynamicAdder;
+let dynamicAdder = function (num) {
+    return function (el) {
+        el + num;
+    }
+};
+
+const addTwo = dynamicAdder(2); // returns a function
+console.log(addTwo(5)); // returns 7
+
+const addTen = dynamicAdder(10);
+console.log(addTen(5)); // returns 15
+
+const addNinety = dynamicAdder(90);
+console.log(addNinety(5)); // returns 95
