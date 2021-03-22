e80acd7b534a7bb3110d50b1eca3929c0536581e
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 06:13:18 -0400

    Update 14-shortest-word.js

--- App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/14-shortest-word.js
+++ App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/14-shortest-word.js
@@ -5,22 +5,19 @@ return the word that appears later in the sentence.
 
 Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
 methods.
+*/
 
-Examples:
+let shortestWord = function(sentence) {
+  let words = sentence.split(' ');
+  let shortest = words[0];
+  words.forEach(function(word) {
+    if (word.length <= shortest.length) {
+        shortest = word;
+    }
+  });
+  return shortest;
+};
 
 console.log(shortestWord('what a wonderful life'));     // 'a'
 console.log(shortestWord('the quick brown fox jumps')); // 'fox'
 console.log(shortestWord('do what you enjoy'));         // 'do'
-
-*/
-
-// your code here
-
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-
-try {
-  module.exports = shortestWord;
-} catch (e) {
-  module.exports = null;
-}
