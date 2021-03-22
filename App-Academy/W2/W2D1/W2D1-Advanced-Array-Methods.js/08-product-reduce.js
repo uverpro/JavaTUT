ab98c0166edf8c39ce8f3e0359657000cda4bb0c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 04:42:45 -0400

    Update 08-product-reduce.js

--- App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/08-product-reduce.js
+++ App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/08-product-reduce.js
@@ -1,5 +1,4 @@
 /*
-
 Write a function `productWithReduce(nums)` that takes in an array of numbers.
 The function should return the total product of multiplying all numbers of the
 array together.
@@ -7,22 +6,13 @@ array together.
 You can assume that `nums` will not be an empty array.
 
 Solve this using Array's `reduce()` method.
+*/
 
-Examples:
+productWithReduce = (nums) => {
+  return nums.reduce((accum, num) => {
+      return accum * num;
+  });
+};
 
 console.log(productWithReduce([10, 3, 5, 2])); // 300
 console.log(productWithReduce([4, 3])); // 12
-
-*/
-
-
-// your code here
-
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-
-try {
-  module.exports = productWithReduce;
-} catch (e) {
-  module.exports = null;
-}
