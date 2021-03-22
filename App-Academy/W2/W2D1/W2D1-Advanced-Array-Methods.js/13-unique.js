cb0f22f4f466a49ffbf4056629094c6c4312cfeb
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 06:06:28 -0400

    Update 13-unique.js

--- App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/13-unique.js
+++ App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/13-unique.js
@@ -7,7 +7,15 @@ Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
 methods.
 */
 
-
+let unique = function(array) {
+    let uniques = [];
+    array.forEach(function(el) {
+        if (!uniques.includes(el)) {
+            uniques.push(el);
+        }
+    });
+    return uniques;
+};
 
 console.log(unique([1, 1, 2, 3, 3])); // [1, 2, 3]
 console.log(unique([11, 7, 8, 10, 8, 7, 7])); // [11, 7, 8, 10]
