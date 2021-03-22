41147f34ac15f8e6638fd63de849a1c89791c4b5
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 00:09:59 -0400

    Update ΩW2D4-Learning-Objectives.js

--- App-Academy/W2/W2D4/ΩW2D4-Learning-Objectives.js
+++ App-Academy/W2/W2D4/ΩW2D4-Learning-Objectives.js
@@ -22,9 +22,65 @@ var stupidVarVar = 'why are you the way that you are var?'
 console.log(stupidVarVar) // why are you the way that you are var?
 
 
-
 // 3. Predict the evaluation of code that utilizes function scope, block scope,
 // lexical scope, and scope chaining
+
+var greeting = 'Hello World!'; // scope? Global
+function greet() {
+  console.log(greeting);
+}
+greet(); // output? 'Hello World!'
+
+function greet() {
+  var greeting = 'Hello World!'; // scope? function scoped
+  console.log(greeting);
+}
+greet(); // output? 'Hello World'
+console.log(greeting); // output? *ERROR, function scoped*
+
+{
+  let greeting = 'Hello World!'; // scope? - block scope
+  var lang = 'English'; // scope? -function/ global
+  console.log(greeting); // output? 'Hello World'
+}
+console.log(lang); // output? 'English'
+console.log(greeting); // output? *ERROR
+
+var name = 'Peter'; // scope? global
+function greet() {
+  var greeting = 'Hello'; // scope? function
+  {
+    let lang = 'English'; // scope? block
+    'this'
+    "these"
+    `THIS IS ALSO A STRING`
+    `${lang}`
+    // console.log(`${lang}: ${greeting} ${name}`);
+    console.log(lang, ':', greeting, name)
+  }
+}
+greet(); // output? English: Hello Peter
+
+let number = 42; // scope? global
+function printNumber() {
+  console.log(number);
+}
+function log() {
+  let number = 54; // scope? block
+  printNumber();
+}
+log(); // output? 42
+​
+let foo = 'foo'; // scope? global
+function bar() {
+  let baz = 'baz'; // scope? block
+  console.log(baz); // 'baz'
+  console.log(foo);// 'foo'
+  number = 42; // scope? global
+  console.log(number); // 42
+}
+bar(); // output?
+
 
 // 4. Define an arrow function
 
