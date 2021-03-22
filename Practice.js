fdfbcf5450f6698e777e2e90bcec3d7e488d10d8
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 04:38:53 -0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -8,8 +8,14 @@ Solve this using Array's `reduce()` method.
 */
 
 maxWithReduce = (nums) => {
-
-}
+    let highestNumber = 0;
+    nums.reduce((sum, num) => {
+        if (num > highestNumber) {
+            highestNumber = num;
+        }
+    }, 0);
+    return highestNumber;
+};
 
 console.log(maxWithReduce([4, 6, 3, 5, 42, 4])); // 42
 console.log(maxWithReduce([-2, -3, -7, 3])); // 3
