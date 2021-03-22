c624abe07c99f2499bee50c1b69cca7e7c594e89
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 20:32:05 -0400

    Update ☭W2D1-Array-Looping-Methods.js

--- App-Academy/W2/W2D1/☭W2D1-Array-Looping-Methods.js
+++ App-Academy/W2/W2D1/☭W2D1-Array-Looping-Methods.js
@@ -1,3 +1,22 @@
+// REVIEW:
+
+// Analyze the for.loop
+// Assume you've populated a variable named 'myArray'
+const myArray = [1, 2, 3, 4, 5];
+
+// Now loop through the entire array
+for (let i=0; i<myArray.length; i++) {
+    const element = myArray[i];
+    // Do stuff with each element
+}
+/*
+Declare a counter variable starting at zero (let i = 0)
+Provided a condition (i < my_array.length),
+and finally incremented the counter variable (i++).
+Often, as shown above, you also want to use a variable (or constant) to make it easier to reference the current element within the code block.
+*/
+// Introduction forEach
+
 // The initial value for total sales is zero
 let totalSales = 0;
 
@@ -18,3 +37,9 @@ monthlySales.forEach(addToTotalSales);
 // Output the total to the console
 console.log('Total Sales are', totalSales);
 // Expected result: Total Sales are 17280
+
+// monthlySales.forEach(function (value) {
+//     totalSales += value;
+// });
+
+// console.log('Total Sales are', totalSales);
