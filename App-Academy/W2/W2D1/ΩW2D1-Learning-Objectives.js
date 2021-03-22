93322fce00f520e3e15550e5b8b6b15fb055211c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-21 22:17:53 -0400

    Update ΩW2D1-Learning-Objectives.js

--- App-Academy/W2/W2D1/ΩW2D1-Learning-Objectives.js
+++ App-Academy/W2/W2D1/ΩW2D1-Learning-Objectives.js
@@ -7,21 +7,77 @@ let bool = true // Primitive (Not mutable)
 let nullVal = null // Primitive (Not mutable)
 let nanVal = NaN // Primitive (Not mutable)
 let undefinedVal = undefined // Primitive (Not mutable)
+let arr = ['sup', 'why tho', 1, ['What about me?']] // Reference (Mutable)
+let obj = {
+    'key': "value",
+    'otherKey': 2
+} // Reference (Mutable)
 
 // 2. Identify when to use . vs [] when accessing values of an object
 
-const object = { a: 1, b: 2, c: 3 };
+const object = { A: 1, B: 2, C: 3 };
 
 for (const value in object) {
   console.log(`${value}: ${object[value]}`);
 }
+// A: 1
+// B: 2
+// C: 3
+
+let notationPractice = {
+  key1: 'Miah',
+  key2: 'Mylo',
+  key3: 'Zac',
+  name: 'Carlos',
+  chad: 'Adrian'
+};
+let Miah = 'Miah';
+let key1 = 'key2';
+let name = 'chad';
+console.log(notationPractice[Miah]) // undefined
+console.log(notationPractice.name) // Carlos
+console.log(notationPractice['name']) // Carlos
+console.log(notationPractice[name]) // Adrian
+console.log(notationPractice.chad) // Adrian
+console.log(notationPractice.key1) // Miah === notationPractice['key1']
+console.log(notationPractice[key1]) // Mylo
+
 
 // 3. Use the obj[key] !== undefined pattern
 // to check if a given variable that contains a key exists in an object
+console.log(notationPractice[Miah] !== undefined) // false
+console.log(notationPractice.chad !== undefined) // true
+console.log(notationPractice.key3 !== undefined) // true
+console.log(notationPractice.warren !== undefined) // false
+function changeToRich(obj) {
+  if(obj.key1 === undefined){
+    obj.key1 = 'Rich'
+  }
+}
+changeToRich(notationPractice);
+console.log(notationPractice)
 
 
 
 // 4. Utilize Object.keys and Object.values in a function
+let notationPractice = {
+  'key1': 'Miah',
+  'key2': 'Mylo',
+  'key3': 'Zac',
+  'name': 'Carlos',
+  'chad': 'Adrian'
+};
+let functionUsingObjStuff = (obj) => {
+  let keys = Object.keys(obj);
+  console.log("This is what Object.keys gives us:", keys)
+  let values = Object.values(obj);
+  console.log("This is what Object.values gives us", values)
+  keys.forEach((key, i) => console.log(key, 'key at index', i))
+  values.forEach((value, i) => console.log(value, 'value at index', i))
+}
+functionUsingObjStuff(notationPractice);
+
+
 
 // 5. Iterate through an object using a for...in loop
 
