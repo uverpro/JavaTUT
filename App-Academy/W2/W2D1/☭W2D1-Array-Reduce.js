8785dedf7c03debeb8147f380f5b3979ec131d54
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 02:28:06 -0400

    Update ☭W2D1-Array-Reduce.js

---
+++ App-Academy/W2/W2D1/☭W2D1-Array-Reduce.js
@@ -0,0 +1,10 @@
+// Array Reduce
+// Array reduce is helpful when you have an array of multiple numbers or elements
+// and you want to reduce the array to a single value.
+// For example, you can use reduce to find a mathematical sum
+// or even compare word lengths to return the longest word of an array.
+
+
+// List the arguments that should be used with the Array reduce method
+// Explain the difference between the accumulator and initialValue parameters
+// Use the reduce function to change an array into a single value
