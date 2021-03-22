9ff95deb827cf8b1f860f6aa8be0451fc2f517c6
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 05:24:43 -0400

    Update 10-yeller-map.js

--- App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/10-yeller-map.js
+++ App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/10-yeller-map.js
@@ -4,23 +4,14 @@ The function should return a new array where each element of the original array
 is yelled.
 
 Solve this using Array's `map()` method.
-
-Examples:
-
-console.log(yeller(['hello', 'world'])); // [ 'HELLO!', 'WORLD!' ]
-console.log(yeller(['kiwi', 'orange', 'mango'])); // [ 'KIWI!', 'ORANGE!', 'MANGO!' ]
-
 */
 
 
-// your code here
+yeller = (words) => {
+  return words.map((word) => {
+      return `${word.toUpperCase()}!`
+  })
+};
 
-
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-
-try {
-  module.exports = yeller;
-} catch (e) {
-  module.exports = null;
-}
+console.log(yeller(['hello', 'world'])); // [ 'HELLO!', 'WORLD!' ]
+console.log(yeller(['kiwi', 'orange', 'mango'])); // [ 'KIWI!', 'ORANGE!', 'MANGO!' ]
