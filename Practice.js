aff9fe20fd6a74beaa9fa02ef5a1f1967fe967e4
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 05:24:50 -0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -6,7 +6,11 @@ is yelled.
 Solve this using Array's `map()` method.
 */
 
-
+yeller = (words) => {
+    return words.map((word) => {
+        return `${word.toUpperCase()}!`
+    })
+};
 
 console.log(yeller(['hello', 'world'])); // [ 'HELLO!', 'WORLD!' ]
 console.log(yeller(['kiwi', 'orange', 'mango'])); // [ 'KIWI!', 'ORANGE!', 'MANGO!' ]
