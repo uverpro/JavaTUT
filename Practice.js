426528cd8c4d845b8cdb3407959fd1989b991a65
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 04:27:26 -0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -5,7 +5,11 @@ The function should return the total sum of all numbers in the array.
 Solve this using Array's `reduce()` method.
 */
 
-
+sumWithReduce = (nums) => {
+    return nums.reduce(function(accum, el) {
+        return accum + el;
+    }, 0)
+};
 
 console.log(sumWithReduce([10, 3, 5, 2])); // 20
 console.log(sumWithReduce([])); // 0
