68e27bf69f7ba895028392ee8806888949a51744
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 04:38:47 -0400

    Update 07-max-reduce.js

--- App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/07-max-reduce.js
+++ App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/07-max-reduce.js
@@ -14,14 +14,25 @@ console.log(maxWithReduce([-2, -3, -7, 3])); // 3
 
 */
 
+let maxWithReduce = function(nums) {
+  return nums.reduce(function(max, num) {
+    if (num > max) {
+        return num;
+    } else {
+        return max;
+    }
+  }, 0);
+};
 
-// your code here
+console.log(maxWithReduce([4, 6, 3, 5, 42, 4])); // 42
+console.log(maxWithReduce([-2, -3, -7, 3])); // 3
 
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-
-try {
-  module.exports = maxWithReduce;
-} catch (e) {
-  module.exports = null;
-}
+// maxWithReduce = (nums) => {
+//   let highestNumber = 0;
+//   nums.reduce((sum, num) => {
+//       if (num > highestNumber) {
+//           highestNumber = num;
+//       }
+//   }, 0);
+//   return highestNumber;
+// };
