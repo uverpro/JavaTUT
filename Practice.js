ecc72e4091e0566731f00ee4aff1eb8fdf7adf00
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 04:32:07 -0400

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,15 @@
+/*
+Write a function `maxWithReduce(nums)` that takes in an array of numbers.
+The function should return the largest number of the array.
+
+You can assume that the array will not be empty.
+
+Solve this using Array's `reduce()` method.
+*/
+
+maxWithReduce = (nums) => {
+
+}
+
+console.log(maxWithReduce([4, 6, 3, 5, 42, 4])); // 42
+console.log(maxWithReduce([-2, -3, -7, 3])); // 3
