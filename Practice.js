dd10a7b9f2a8486c8d97e5c89a614933e9feefa2
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 04:22:14 -0400

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,11 @@
+/*
+Write a function `sumWithReduce(nums)` that takes in an array of numbers.
+The function should return the total sum of all numbers in the array.
+
+Solve this using Array's `reduce()` method.
+*/
+
+
+
+console.log(sumWithReduce([10, 3, 5, 2])); // 20
+console.log(sumWithReduce([])); // 0
