6c817b98b484f4256ac971eb7ebadd92b5c5494b
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 04:47:49 -0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -5,15 +5,11 @@ with underscores(`_`). PascalCase is a string where the first char of each word
 is capital, all other chars lowercase.
 
 Solve this using `array.map()`.
+*/
 
-Examples:
+
 
 console.log(snakeToCamel('snakes_go_hiss')); // 'SnakesGoHiss'
 console.log(snakeToCamel('say_hello_world')); // 'SayHelloWorld'
 console.log(snakeToCamel('app_academy_is_cool')); // 'AppAcademyIsCool'
 console.log(snakeToCamel('APp_ACADEMY_iS_cOol')); // 'AppAcademyIsCool'
-
-*/
-
-
-// your code here
