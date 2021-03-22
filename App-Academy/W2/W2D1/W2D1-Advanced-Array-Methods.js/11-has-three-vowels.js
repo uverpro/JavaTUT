ed4573aa1006bcc1b70c98ca5db771a2ebcfa6c4
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 05:28:32 -0400

    Update 11-has-three-vowels.js

--- App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/11-has-three-vowels.js
+++ App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/11-has-three-vowels.js
@@ -5,25 +5,40 @@ contains at least three different vowels.
 
 Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
 methods.
-
-Examples:
-
-console.log(hasThreeVowels('delicious'));       //  true
-console.log(hasThreeVowels('bootcamp prep'));   //  true
-console.log(hasThreeVowels('bootcamp'));        //  false
-console.log(hasThreeVowels('dog'));             //  false
-console.log(hasThreeVowels('go home'));         //  false
-
 */
 
+let hasThreeVowels = function(string) {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = vowels.reduce(function(acc, vowel) {
+        if (string.includes(vowel)) {
+            acc++;
+        }
+        return acc;
+    }, 0);
 
-// your code here
+    return count >= 3;
+};
 
 
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+// using forEach
+// let hasThreeVowels = function(string) {
+//     let vowels = ['a', 'e', 'i', 'o', 'u'];
+//     let count = 0;
+//     vowels.forEach(function(vowel) {
+//         if (string.includes(vowel)) {
+//             count++;
+//         }
+//     });
 
-try {
-    module.exports = hasThreeVowels;
-} catch (e) {
-    module.exports = null;
-}
+//     return count >= 3;
+// };
+
+// using filter
+// let hasThreeVowels = function(string) {
+//     let vowels = ['a', 'e', 'i', 'o', 'u'];
+//     let vowelWords = vowels.filter(function(vowel) {
+//         return (string.includes(vowel));
+//     });
+
+//     return vowelWords.length >= 3;
+// };
