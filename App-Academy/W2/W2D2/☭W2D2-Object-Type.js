c1a9294004311d206a40ee1d8287b0d9c65d9afa
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 07:11:04 -0400

    Update ☭W2D2-Object-Type.js

---
+++ App-Academy/W2/W2D2/☭W2D2-Object-Type.js
@@ -0,0 +1,11 @@
+// THE OBJECT TYPE
+
+// Objects are a Data Type, like string or arrays
+// they store other data like an array stores elements
+
+// Objects store VALUE that is associated with a KEY
+// KEYS are almost always strings
+// VALUES can be any data type: numbers, strings, arrays, other objects, etc.
+
+// In other programming languages similar data structures are
+// referred to as "dictionaries", "maps", or "associative arrays"
