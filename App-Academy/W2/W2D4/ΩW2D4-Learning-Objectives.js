052cb244bd31aa11885f7bb917c20a662593a08e
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 00:02:05 -0400

    Update ΩW2D4-Learning-Objectives.js

--- App-Academy/W2/W2D4/ΩW2D4-Learning-Objectives.js
+++ App-Academy/W2/W2D4/ΩW2D4-Learning-Objectives.js
@@ -2,6 +2,12 @@
 
 // 1. Identify the difference between const, let, and var declarations
 
+const constVar = 'something I want to be "constant"';
+// Block Scoped - in a block of code aka {in here}
+// Cannot be reassigned
+// Cannot be re-declared
+
+
 // 2. Explain the difference between const, let, and var declarations
 
 // 3. Predict the evaluation of code that utilizes function scope, block scope,
