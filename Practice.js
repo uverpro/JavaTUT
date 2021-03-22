87a69fc8e3ebbc27cf41a5be91f2e88b16841aef
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 03:26:14 -0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,20 +1,18 @@
-// The initial value for total sales is zero
-let totalSales = 0;
+/*
+Write a function `sumForEach(arr)` that accepts an array as an arg.
+The function should return the total sum of all values in the array.
 
-// Function to add a value to the total sales
-const addToTotalSales = function (value) {
-   totalSales += value;
-}
+Solve this using Array's `forEach()` method.
 
-// Some sales numbers to experiment with
-const monthlySales = [1234, 2345, 3456, 4567, 5678];
+Examples:
 
-// Loop through all sales numbers to add them to the total
-monthlySales.forEach(addToTotalSales);
-// for (let i=0; i<monthlySales.length; i++) {
-//     addToTotalSales(monthlySales[i]);
-// }
+console.log(sumForEach([4, 3, -1, 10])); // 16
+console.log(sumForEach([6, 7, 2])); // 15
+console.log(sumForEach([])); // 0
+*/
 
-// Output the total to the console
-console.log('Total Sales are', totalSales);
-// Expected result: Total Sales are 17280
+
+
+console.log(sumForEach([4, 3, -1, 10])); // 16
+console.log(sumForEach([6, 7, 2])); // 15
+console.log(sumForEach([])); // 0
