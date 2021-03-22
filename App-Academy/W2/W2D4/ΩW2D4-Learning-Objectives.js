e0441d2e71238eec2571ca5f1eafa89bac8eb5dc
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 00:06:31 -0400

    Update ΩW2D4-Learning-Objectives.js

--- App-Academy/W2/W2D4/ΩW2D4-Learning-Objectives.js
+++ App-Academy/W2/W2D4/ΩW2D4-Learning-Objectives.js
@@ -1,14 +1,27 @@
 // Scope Lesson Learning Objectives
 
 // 1. Identify the difference between const, let, and var declarations
+// 2. Explain the difference between const, let, and var declarations
 
 const constVar = 'something I want to be "constant"';
 // Block Scoped - in a block of code aka {in here}
 // Cannot be reassigned
 // Cannot be re-declared
 
+let letVar = 'something I want to be reassignable'
+// Block Scoped
+// can be reassigned
+// cannot be re-declared
 
-// 2. Explain the difference between const, let, and var declarations
+var stupidVarVar = 'don\'t do this, I will find you'
+// function scoped - available in the function it is declared,
+// or globally if not declared in a function
+// can be reassigned
+// can be re-declared <---- a problem
+var stupidVarVar = 'why are you the way that you are var?'
+console.log(stupidVarVar) // why are you the way that you are var?
+
+
 
 // 3. Predict the evaluation of code that utilizes function scope, block scope,
 // lexical scope, and scope chaining
