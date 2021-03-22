1d6e53d323c4ce92a34e604a34d2f1fef3a44cd9
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 03:51:32 -0400

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,12 @@
+/*
+Write a function `longWords(words)` that takes in an array of words.
+The function should return an array containing only the words that are longer
+than 5 characters.
+
+Solve this using Array's `filter()` method.
+*/
+
+longWords = (words) =>
+
+console.log(longWords(['bike', 'skateboard','scooter', 'moped'])); // [ 'skateboard', 'scooter' ]
+console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // [ 'couscous', 'ceviche', 'solyanka' ]
