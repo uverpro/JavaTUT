e7d255e826a3efb5acb8998d9128544654610cb2
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 06:23:41 -0400

    Update 15-choose-primes.js

--- App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/15-choose-primes.js
+++ App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/15-choose-primes.js
@@ -18,12 +18,31 @@ console.log(choosePrimes([5, 6, 4, 11, 2017])); // [ 5, 11, 2017 ]
 
 */
 
-choosePrimes = (nums) => {
-    let result = nums.filter((num) => {
-        return (num % 2 !== 0 || num % 3 !== 0)
-    })
-    return result;
+let isPrime = function(num) {
+    if (num < 2) return false;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
 };
+
+let choosePrimes = function(nums) {
+    let primes = [];
+    nums.forEach(function(num) {
+        if (isPrime(num)) {
+            primes.push(num);
+        }
+    });
+    return primes;
+}
+
+// alternative solution using Array.filter
+// let choosePrimes = function(nums) {
+//     return nums.filter(isPrime);
+// }
+
 
 console.log(choosePrimes([36, 48, 9, 13, 19])); // [ 13, 19 ]
 console.log(choosePrimes([5, 6, 4, 11, 2017])); // [ 5, 11, 2017 ]
