1a67ac5aa8160729e627283182da552c2e5b0f0d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 03:57:20 -0400

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,12 @@
+/*
+Write a function `removeEWords(sentence)` that accepts a sentence string as an
+arg. The function should return a new string, containing only the words that
+don't have the letter "e" in them.
+
+Solve this using Array's `filter()` method.
+*/
+
+
+
+console.log(removeEWords('What time is it everyone?')); // 'What is it'
+console.log(removeEWords('Enter the building')); // 'building'
