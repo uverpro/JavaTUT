56204313e842c056895eb0e5ca55f8b51bd9e007
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 05:10:47 -0400

    Update 09-snake-to-camel-map.js

--- App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/09-snake-to-camel-map.js
+++ App-Academy/W2/W2D1/W2D1-Advanced-Array-Methods.js/09-snake-to-camel-map.js
@@ -5,24 +5,17 @@ with underscores(`_`). PascalCase is a string where the first char of each word
 is capital, all other chars lowercase.
 
 Solve this using `array.map()`.
+*/
 
-Examples:
+snakeToCamel = (string) => {
+    let words = string.split("_");
+    let nuWords = words.map((word) => {
+        return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    });
+    return nuWords.join('');
+};
 
 console.log(snakeToCamel('snakes_go_hiss')); // 'SnakesGoHiss'
 console.log(snakeToCamel('say_hello_world')); // 'SayHelloWorld'
 console.log(snakeToCamel('app_academy_is_cool')); // 'AppAcademyIsCool'
 console.log(snakeToCamel('APp_ACADEMY_iS_cOol')); // 'AppAcademyIsCool'
-
-*/
-
-
-// your code here
-
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-
-try {
-    module.exports = snakeToCamel;
-} catch (e) {
-    module.exports = null;
-};
