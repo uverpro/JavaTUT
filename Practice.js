c570a9a5a77c10217943b926956f0383c73f26f3
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 04:40:41 -0400

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,14 @@
+/*
+Write a function `productWithReduce(nums)` that takes in an array of numbers.
+The function should return the total product of multiplying all numbers of the
+array together.
+
+You can assume that `nums` will not be an empty array.
+
+Solve this using Array's `reduce()` method.
+*/
+
+productWithReduce = (nums) => {};
+
+console.log(productWithReduce([10, 3, 5, 2])); // 300
+console.log(productWithReduce([4, 3])); // 12
