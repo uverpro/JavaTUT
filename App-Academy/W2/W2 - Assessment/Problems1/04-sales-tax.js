6655d2ce0b3df9dfd706f856663a4caf42129acf
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 12:39:14 -0400

    Update 04-sales-tax.js

--- App-Academy/W2/W2 - Assessment/Problems1/04-sales-tax.js
+++ App-Academy/W2/W2 - Assessment/Problems1/04-sales-tax.js
@@ -4,19 +4,17 @@ number of arguments as prices, adds them up and returns a string containing the
 sales tax with a dollar sign in front. Use a tax percentage of 9%.
 
 Hint: Look up `toFixed()` on MDN
-
-Examples:
-
-salesTax(30, 70); // '$9.00'
-salesTax(1.12, 2.23, 3.34, 4.45); // '$1.00'
-salesTax(4.99, 9.99, 19.99); // '$3.15'
 ***********************************************************************/
 
-// your code here
+salesTax = (...nums) => {
+    sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        sum += num;
+    }
+    return `$${(sum * .09).toFixed(2)}`
+};
 
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-try {
-    module.exports = salesTax;
-} catch(e) {
-    module.exports = null;
-}
+console.log(salesTax(30, 70)); // '$9.00'
+console.log(salesTax(1.12, 2.23, 3.34, 4.45)); // '$1.00'
+console.log(salesTax(4.99, 9.99, 19.99)); // '$3.15'
