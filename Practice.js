2bb91c7c7e4239c163c8d6f7dfa3d6455943fd0b
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-22 04:42:51 -0400

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -8,7 +8,11 @@ You can assume that `nums` will not be an empty array.
 Solve this using Array's `reduce()` method.
 */
 
-productWithReduce = (nums) => {};
+productWithReduce = (nums) => {
+    return nums.reduce((accum, num) => {
+        return accum * num;
+    });
+};
 
 console.log(productWithReduce([10, 3, 5, 2])); // 300
 console.log(productWithReduce([4, 3])); // 12
