319d00dce0262fb8b407e83d0df1d7a931474e16
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 13:28:36 -0400

    Update ∆A-Recursion-Debugging-Video-Notes.js

--- App-Academy/W3/W3D1/W3D1-Videos/∆A-Recursion-Debugging-Video-Notes.js
+++ App-Academy/W3/W3D1/W3D1-Videos/∆A-Recursion-Debugging-Video-Notes.js
@@ -1,6 +1,9 @@
 // RECURSION: "DEBUGGING" SOLUTION
 // Coding Demo
 
+// BASE CASE: Empty Array
+// RECURSIVE STEP: Removing Elements from the collection (.slice(1))
+
 function doForAll(array, action) {
     return [action(array[0]), ...doForAll(array.slice(1), action)];
 };
@@ -8,3 +11,16 @@ function doForAll(array, action) {
 doForAll([], (x) => x * 2); // => []
 doForAll([1, 2, 3], (x) => x + 1); // => [2, 4, 6]
 doForAll(["a", "b", "c"], (x) => x.toUpperCase()); // => ["A", "B", "C"]
+
+// const halve = (x) => x/2;
+// doForAll([2, 4], halve); => [1, 2]
+// - [1, ...doForAll([4], halve)]
+// -- [1, ...[2, ...doForAll([], halve)]]
+// --- [1, 2, ...[0, ...doForAll([], halve)]]
+// ---- [1, 2, 0, ...[0, ...doForAll([], halve)]] <== will continue forever
+
+// BASE CASE: Empty Array
+// RECURSIVE STEP: Removing Elements from the collection (.slice(1))
+function doForAll(array, action) {
+    return [action(array[0]), ...doForAll(array.slice(1), action)];
+};
