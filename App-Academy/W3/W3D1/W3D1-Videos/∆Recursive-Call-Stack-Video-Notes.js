9bfee7f74e4eaddeb554188c45ee3d7c095e36a5
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 11:53:32 -0400

    Update ∆Recursive-Call-Stack-Video-Notes.js

--- App-Academy/W3/W3D1/W3D1-Videos/∆Recursive-Call-Stack-Video-Notes.js
+++ App-Academy/W3/W3D1/W3D1-Videos/∆Recursive-Call-Stack-Video-Notes.js
@@ -2,7 +2,7 @@
 
 function echo(word) {
     if (word.length > 1) {
-        console.log(word.toUpperCase());
+        // console.log(word.toUpperCase());
 
         let response = echo(word.slice(1));
 
@@ -10,22 +10,21 @@ function echo(word) {
 
         return word;
     } else {
-        console.log(word.toUpperCase());
+        // console.log(word.toUpperCase());
         return word;
     }
 };
 
 echo("howdy");
-
 // HOWDY   // 5    <= this console.log is UNINTERRUPTIVE
-// OWDY    // 7
-// WDY     // 7
-// DY      // 7
-// Y       // 13   <= word.length is less than 1, so running from line 12, the else condtional
+// OWDY    // 9-7
+// WDY     // 9-7
+// DY      // 9-7
+// Y       // 13-12   <= word.length is less than 1, so running from line 12, the else condtional
 /* Here's where things turn around for stack. We've made all our recursive calls.
 The "else" conditional is base case. We've stopped recursion
 and now we have to clear existing stack frames.*/
-// y       // 14
-// dy      // 11
-// wdy     //
-// owdy    //
+// y       // 9-14
+// dy      // 9-11
+// wdy     // 9-11
+// owdy    // 9-11
