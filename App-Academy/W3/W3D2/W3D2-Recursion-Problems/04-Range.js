c26476471aafe085ca2c27530d913be55977900c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 17:28:56 -0400

    Create 04-Range.js

---
+++ App-Academy/W3/W3D2/W3D2-Recursion-Problems/04-Range.js
@@ -0,0 +1,10 @@
+/***********************************************************************
+Write a recursive function, `range`, that takes a start and an end and returns
+an array of all numbers in that range, exclusive. If the end number is less than
+the start, return an empty array.
+***********************************************************************/
+
+
+range(1, 5); // [1, 2, 3, 4]
+range(3, 4); // [3]
+range(7, 6); // []
