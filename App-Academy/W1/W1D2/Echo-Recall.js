285962d1a07c9bfd2c353a4b1df88c3146c41fd8
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 16:37:24 -0400

    Delete Echo-Recall.js

--- App-Academy/W1/W1D2/Echo-Recall.js
+++
@@ -1,7 +0,0 @@
-function echo (string) {
-    console.log(`${string.toUpperCase()} ... ${string} ... ${string.toLowerCase()}`);
-  };
-
-  echo("Mom!"); // => prints "MOM! ... Mom! ... mom!"
-  echo("hey"); // => prints "HEY ... hey ... hey"
-  echo("JUMp"); // => prints "JUMP ... JUMp ... jump"
