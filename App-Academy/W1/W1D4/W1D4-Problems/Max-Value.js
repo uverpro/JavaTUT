e05b764f8e0aa752e4887dbcb4783fbb87718dd7
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 17:06:55 -0400

    Create Max-Value.js

---
+++ App-Academy/W1/W1D4/W1D4-Problems/Max-Value.js
@@ -0,0 +1,37 @@
+// Max Value
+// Write a function maxValue(nums) that takes in an array of numbers as an arg.
+// The function should return the largest number of the array.
+// If the array is empty, the function should return null.
+
+let maxValue = function (nums) {
+    let largestNum = 0;
+    if (nums.length === 0) {
+        return null;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        if (num > largestNum) {
+            largestNum = num;
+        }
+    }
+    return largestNum;
+};
+
+console.log(maxValue([4, 6, 3, 5, 42, 4])); // 42
+console.log(maxValue([-2, -3, -7, 3 ])); // 3
+console.log(maxValue([])); // null
+
+/*
+let maxValue = function(nums) {
+    let max = null;
+
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        if (max === null || num > max) {
+            max = num;
+        }
+    }
+
+    return max;
+};
+*/
