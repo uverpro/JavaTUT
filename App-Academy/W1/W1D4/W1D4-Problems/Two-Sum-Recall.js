b866781cd384600aae89755bc237ad1c1bf4b211
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 17:42:16 -0400

    Create Two-Sum-Recall.js

---
+++ App-Academy/W1/W1D4/W1D4-Problems/Two-Sum-Recall.js
@@ -0,0 +1,6 @@
+// Two Sum Recall
+// Write a function twoSum(arr, target)
+// that accepts an array and a target number as args.
+// The function should a return a boolean
+// indicating if two distinct numbers of the array add up to the target value.
+// You can assume that input array contains only unique numbers.
