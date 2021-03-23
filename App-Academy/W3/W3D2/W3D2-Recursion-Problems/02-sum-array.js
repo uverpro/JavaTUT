b220ac48377ee88c69758077efc4ac60d6d25bdb
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 15:44:09 -0400

    Update 02-sum-array.js

--- App-Academy/W3/W3D2/W3D2-Recursion-Problems/02-sum-array.js
+++ App-Academy/W3/W3D2/W3D2-Recursion-Problems/02-sum-array.js
@@ -1,3 +1,9 @@
+/***********************************************************************
+Write a recursive function called `sumArray` that takes an array of integers
+and returns the value of all the integers added together. Your array may
+include a mix of positive and negative integers!
+***********************************************************************/
+
 
 
 console.log(sumArray([1, 2, 3])); //  6
