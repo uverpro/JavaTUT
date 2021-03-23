dfa5107bf15d740f7eb41e37f55866f2eb711c8c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 16:19:09 -0400

    Create 01-Sum-to-n.js

---
+++ App-Academy/W3/W3D2/W3D2-Recursion-Problems/01-Sum-to-n.js
@@ -0,0 +1,17 @@
+/***********************************************************************
+Write a recursive function called `sumToN` that takes in a number and returns
+the sum of all the numbers from 0 to that number. Return null for any input
+number below 0.
+***********************************************************************/
+
+function sumToN(n) {
+    if (n < 0) return null;
+    if (n <= 1) return n; // BASE CASE // n === 0, RECURSIVE CASE
+
+    return n + sumToN(n - 1); // RECURSIVE STEP
+  }
+
+console.log(sumToN(5)); // returns 15
+console.log(sumToN(1));  // returns 1
+console.log(sumToN(9));  // returns 45
+console.log(sumToN(-8));  // returns null
