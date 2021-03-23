a53c39afcc27d7ebb6f6d4bf246fea2121daf071
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 17:58:08 -0400

    Update Reverse-Sentence.js

--- App-Academy/W1/W1D4/W1D4-Problems/Reverse-Sentence.js
+++ App-Academy/W1/W1D4/W1D4-Problems/Reverse-Sentence.js
@@ -5,6 +5,12 @@
 
 reverseSentence = (sentence) => {
     let newSentence = sentence.split(' ');
+    let reversedSentence = [];
+    for (let i = newSentence.length; i >= 0; i--) {
+        let word = newSentence[i];
+        reversedSentence.push(word);
+    }
+    return reversedSentence.join(" ");
 };
 
 console.log(reverseSentence('I am pretty hungry')); // 'hungry pretty am I'
