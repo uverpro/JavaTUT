87d56295d42acef78077dce97b4abda0a21da0af
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 16:46:58 -0400

    Create Call-This-Function.js

---
+++ App-Academy/W1/W1D2/W1D2-Problems/Call-This-Function.js
@@ -0,0 +1,14 @@
+// We have provided a function named isCool.
+// The isCool function will take a string argument
+// and return a sentence saying that the given string "is cool!".
+// Call the isCool function with the arguments listed below.
+
+function isCool(string) {
+    let coolSentence = string + " is cool!"
+    console.log(coolSentence);
+  }
+
+  // Call the above function passing in the below arguments:
+  isCool("Music");
+  isCool("JavaScript");
+  isCool("The world");
