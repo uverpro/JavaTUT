50fd2ce86f0a1e8262e5bc565999730162f2a07c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 16:52:57 -0400

    Create Numbers-Exercise.js

---
+++ App-Academy/W1/W1D1/W1D1-Problems/Numbers-Exercise.js
@@ -0,0 +1,8 @@
+// Numbers Exercise
+// Write 5 examples of expressions using each of the operations: +, -, *, /, and %. Print out the result of your expressions using console.log
+
+console.log(5 + 5) // 10
+console.log(5 - 5) // 0
+console.log(5 * 5) // 25
+console.log(5 / 5) // 1
+console.log(5 % 5) // 0
