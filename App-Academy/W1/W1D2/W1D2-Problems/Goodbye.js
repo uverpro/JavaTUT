f0da4310b04f16bf017059c3f284e6292bb9d455
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 16:49:26 -0400

    Create Goodbye.js

---
+++ App-Academy/W1/W1D2/W1D2-Problems/Goodbye.js
@@ -0,0 +1,8 @@
+// Write a function goodbye(name)
+// that takes in a string name and returns a string saying bye to that name.
+
+function goodbye (name) {
+    return "Bye " + name
+};
+
+console.log(goodbye("Felicia"));
