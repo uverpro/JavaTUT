9f8d95b2181a5d34347b6467f9898473548236ff
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 11:12:01 -0400

    Update ∆CallStack-Video-Notes.js

--- App-Academy/W3/W3D1/W3D1-Videos/∆CallStack-Video-Notes.js
+++ App-Academy/W3/W3D1/W3D1-Videos/∆CallStack-Video-Notes.js
@@ -3,3 +3,8 @@
 // What is the Call Stack?
 // The call stack is the structure used in the JavaScript runtime
 // to track the execution of function calls.
+
+// Stack Data Structure
+// It's like a pile of many items
+// New items must be place on the top of the pile (push)
+// Only the top item of the pile can be removed (pop)
