a46b43a08a6915217fed018e31fcdd35bde42a6e
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 16:36:58 -0400

    Delete Average-of-Two.js

--- App-Academy/W1/W1D2/Average-of-Two.js
+++
@@ -1,9 +0,0 @@
-// Write a method averageOfTwo(num1, num2) that returns the average of two numbers.
-
-function averageOfTwo (num1, num2) {
-    return (num1 + num2) / 2;
-  }
-
-  console.log(averageOfTwo(3, 7)); // => 5.0
-  console.log(averageOfTwo(16, 5)); // => 10.5
-  console.log(averageOfTwo(2, 7.5)); // => 4.75
