456b3951a9512951abe8773db8bb305d62ab49fc
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 17:03:56 -0400

    Create Pit-Pat.js

---
+++ App-Academy/W1/W1D3/W1D3-Problems/Pit-Pat.js
@@ -0,0 +1,31 @@
+// Pit Pat
+// Write a function pitPat(max) that accepts a number as an arg.
+// The function should return an array containing all positive numbers
+// less than or equal to max that are divisible by either 4 or 6, but not both.
+
+let pitPat = function (max) {
+    let nuArray = [];
+    for (let i = 0; i <= max; i++) {
+        if (((i % 4 === 0) || (i % 6 === 0))
+            && !(i % 4 === 0 && i % 6 === 0)) {
+                nuArray.push(i);
+            }
+    }
+    return nuArray;
+};
+
+console.log(pitPat(18)); // [ 4, 6, 8, 16, 18 ]
+console.log(pitPat(30)); // [ 4, 6, 8, 16, 18, 20, 28, 30 ]
+
+// AA:
+// let pitPat = function(max) {
+//     let nums = [];
+
+//     for (let i = 1; i <= max; i++) {
+//         if ((i % 4 === 0 || i % 6 === 0) && i % 12 !== 0) {
+//             nums.push(i);
+//         }
+//     }
+
+//     return nums;
+// };
