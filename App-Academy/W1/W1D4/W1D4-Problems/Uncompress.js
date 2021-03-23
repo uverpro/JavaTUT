864a78eea8fece226529307257a94fd84cd9d750
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 17:07:29 -0400

    Create Uncompress.js

---
+++ App-Academy/W1/W1D4/W1D4-Problems/Uncompress.js
@@ -0,0 +1,48 @@
+/* Uncompress
+Write a function uncompress(str) that takes in a "compressed" string as an arg.
+A compressed string consists of a character
+immediately followed by the number of times it appears in the "uncompressed" form.
+The function should return a the uncompressed version of the string. See the examples.
+
+Hint: you can use the built-in Number function should
+convert a numeric string into the number type.
+For example. Number("4") // => 4 */
+
+let uncompress = function (str) {
+    let uncompressedStr = "";
+    for (let i = 0; i < str.length; i++) {
+        let currentEl = str[i];
+        let is_NaN = Number(currentEl);
+        if (is_NaN !== NaN) {
+            let number = Number(currentEl)
+            for (let j = 0; j < number; j++) {
+                let letter = str[i-1];
+                uncompressedStr += letter;
+            }
+        }
+    }
+    return uncompressedStr;
+};
+
+console.log(uncompress('x3y4z2')); // 'xxxyyyyzz'
+console.log(uncompress('a5b2c4z1')); // 'aaaaabbccccz'
+console.log(uncompress('b1o2t1')); // 'boot'
+
+let uncompress = function(str) {
+    let newStr = '';
+
+    for (let i = 0; i < str.length; i += 2) {
+        let char = str[i];
+        let num = Number(str[i + 1]);
+
+        for (let times = 0; times < num; times++) {
+            newStr += char;
+        }
+    }
+
+    return newStr;
+};
+
+console.log(uncompress('x3y4z2')); // 'xxxyyyyzz'
+console.log(uncompress('a5b2c4z1')); // 'aaaaabbccccz'
+console.log(uncompress('b1o2t1')); // 'boot'
