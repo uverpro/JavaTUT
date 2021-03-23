520f998cd8dafbe0539d264952df7dbf2d4828c5
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 16:32:11 -0400

    Update 03-Ice-Cream-Shop.js

--- App-Academy/W3/W3D2/W3D2-Recursion-Problems/03-Ice-Cream-Shop.js
+++ App-Academy/W3/W3D2/W3D2-Recursion-Problems/03-Ice-Cream-Shop.js
@@ -5,10 +5,17 @@ user's favorite ice cream flavor. Recursively find out whether or not the shop
 offers their favorite flavor.
 ***********************************************************************/
 
+iceCreamShop = (flavors, favorite) => {
+    // base cases
+    if (!flavors.length) return false;
+    if (flavors.pop() === favorite) return true;
 
+    // recursive step
+    return iceCreamShop(flavors, favorite);
+}
 
 console.log(iceCreamShop(['vanilla', 'strawberry'], 'blue moon')); // false
-iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea'); // true
-iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio'); // false
-iceCreamShop(['moose tracks'], 'moose tracks'); // true
-iceCreamShop([], 'honey lavender'); // false
+console.log(iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')); // true
+console.log(iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')); // false
+console.log(iceCreamShop(['moose tracks'], 'moose tracks')); // true
+console.log(iceCreamShop([], 'honey lavender')); // false
