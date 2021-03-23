bbb22db72126ba733ff10b97b55c6f2930505274
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 18:05:22 -0400

    Update Initials.js

--- App-Academy/W1/W1D4/W1D4-Problems/Initials.js
+++ App-Academy/W1/W1D4/W1D4-Problems/Initials.js
@@ -2,7 +2,16 @@
 // Write a function initials(name) that accepts a full name as an arg.
 // The function should return the initials for that name.
 
-
+initials = (name) => {
+    let words = name.split(" ");
+    let initials = [];
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        let initial = word[0];
+        initials.push(initial.toUpperCase());
+    }
+    return initials.join();
+};
 
 console.log(initials('anna paschall')); // 'AP'
 console.log(initials('Mary La Grange')); // 'MLG'
