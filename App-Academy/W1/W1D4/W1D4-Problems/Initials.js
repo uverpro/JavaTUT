6abe1087723b464a4c2a70be8ac23349c8b14919
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 17:58:01 -0400

    Create Initials.js

---
+++ App-Academy/W1/W1D4/W1D4-Problems/Initials.js
@@ -0,0 +1,10 @@
+// Initials
+// Write a function initials(name) that accepts a full name as an arg.
+// The function should return the initials for that name.
+
+
+
+console.log(initials('anna paschall')); // 'AP'
+console.log(initials('Mary La Grange')); // 'MLG'
+console.log(initials('brian crawford scott')); // 'BCS'
+console.log(initials('Benicio Monserrate Rafael del Toro Sánchez')); // 'BMRDTS'
