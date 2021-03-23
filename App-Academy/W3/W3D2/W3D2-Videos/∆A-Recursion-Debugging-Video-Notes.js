e5738a5bfb0bcdbeeeb3383c23ff8974ac6a8e64
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 14:35:02 -0400

    Create ∆A-Recursion-Debugging-Video-Notes.js

---
+++ App-Academy/W3/W3D2/W3D2-Videos/∆A-Recursion-Debugging-Video-Notes.js
@@ -0,0 +1,38 @@
+// RECURSION: "DEBUGGING" SOLUTION
+// Coding Demo
+
+// BASE CASE: Empty Array
+// RECURSIVE STEP: Removing Elements from the collection (.slice(1))
+// RECURSIVE CASE: NONE
+
+function doForAll(array, action) {
+    return [action(array[0]), ...doForAll(array.slice(1), action)];
+};
+
+doForAll([], (x) => x * 2); // => []
+doForAll([1, 2, 3], (x) => x + 1); // => [2, 3, 4]
+doForAll(["a", "b", "c"], (x) => x.toUpperCase()); // => ["A", "B", "C"]
+
+const halve = (x) => x/2;
+doForAll([2, 4], halve); // [1, 2]
+// - [1, ...doForAll([4], halve)]
+// -- [1, ...[2, ...doForAll([], halve)]]
+// --- [1, 2, ...[0, ...doForAll([], halve)]]
+// ---- [1, 2, 0, ...[0, ...doForAll([], halve)]] <== will continue forever
+
+
+// BASE CASE: Empty Array ()
+// RECURSIVE STEP: Removing Elements from the collection (.slice(1))
+// RECURSIVE CASE:
+function doForAll(array, action) {
+    if (array.length === 0) {
+        return [];
+    }
+    return [action(array[0]), ...doForAll(array.slice(1), action)];
+};
+
+console.log(doForAll([], (x) => x * 2)); // => []
+console.log(doForAll([1, 2, 3], (x) => x + 1)); // => [2, 4, 6]
+console.log(doForAll(["a", "b", "c"], (x) => x.toUpperCase())); // => ["A", "B", "C"]
+const halve = (x) => x/2;
+console.log(doForAll([2, 4], halve)); // [1, 2]
