9485c66ad69feef489199c26257c3d354875eb04
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 16:37:58 -0400

    Delete Goodbye.js

--- App-Academy/W1/W1D2/Goodbye.js
+++
@@ -1,8 +0,0 @@
-// Write a function goodbye(name)
-// that takes in a string name and returns a string saying bye to that name.
-
-function goodbye (name) {
-    return "Bye " + name
-};
-
-console.log(goodbye("Felicia"));
