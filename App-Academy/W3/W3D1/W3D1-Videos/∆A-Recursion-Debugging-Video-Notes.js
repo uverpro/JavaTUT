664c68cbcd30ebaf9badc1fb56b8ff03e1651414
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 13:19:44 -0400

    Update ∆A-Recursion-Debugging-Video-Notes.js

--- App-Academy/W3/W3D1/W3D1-Videos/∆A-Recursion-Debugging-Video-Notes.js
+++ App-Academy/W3/W3D1/W3D1-Videos/∆A-Recursion-Debugging-Video-Notes.js
@@ -2,5 +2,9 @@
 // Coding Demo
 
 function doForAll(array, action) {
-    return [action(array[0]), ...doForAll(arr.slice(1), action)];
+    return [action(array[0]), ...doForAll(array.slice(1), action)];
 };
+
+doForAll([], (x) => x * 2); // => []
+doForAll([1, 2, 3], (x) => x + 1); // => [2, 4, 6]
+doForAll(["a", "b", "c"], (x) => x.toUpperCase()); // => ["A", "B", "C"]
