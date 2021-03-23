aa606dfe58a29fc80979589a5015cdd31ddbf040
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 17:50:24 -0400

    Create Reverse-Sentence.js

---
+++ App-Academy/W1/W1D4/W1D4-Problems/Reverse-Sentence.js
@@ -0,0 +1,11 @@
+// Reverse Sentence
+// Write a function reverseSentence(sentence) that takes in a sentence as an arg.
+// The function should return a new sentence where the order of the words is reversed.
+// Note that you should reverse the order among words, not the order among characters.
+
+reverseSentence = (sentence) => {
+
+};
+
+console.log(reverseSentence('I am pretty hungry')); // 'hungry pretty am I'
+console.log(reverseSentence('follow the yellow brick road')); // 'road brick yellow the follow'
