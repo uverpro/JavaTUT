261822687f9bd3ba14710765df1c2e349acc87d0
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 16:47:23 -0400

    Create Divide-by-Three.js

---
+++ App-Academy/W1/W1D2/W1D2-Problems/Divide-by-Three.js
@@ -0,0 +1,10 @@
+// Write a function divideByThree(num)
+// that returns the passed in number argument divided by three.
+
+function divideByThree (num) {
+    return num / 3;
+};
+
+console.log(divideByThree(9)); // => 3
+console.log(divideByThree(15)); // => 5.0
+console.log(divideByThree(98)); // => 32.666666666666664
