4f3702fbef1d6a847eac9da79978b60278d1ce98
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 13:51:23 -0400

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,15 @@
+exercise(bottle) {
+  console.log("Just a few more reps!");)
+  drinkWater(bottle);
+}
+
+drinkWater(bottle) {
+  if (bottle.water > 0) {
+      exercise({ water: bottle.water - 1 });
+  } else {
+      console.log("Whew! Good workout.");
+      return;
+  }
+}
+
+exercise({ water: 5 });
