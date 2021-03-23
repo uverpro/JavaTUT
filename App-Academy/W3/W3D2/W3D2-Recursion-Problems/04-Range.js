f476c3ff35c89ad8e62bd1be1212f8ff4800a56f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-23 18:39:10 -0400

    Update 04-Range.js

--- App-Academy/W3/W3D2/W3D2-Recursion-Problems/04-Range.js
+++ App-Academy/W3/W3D2/W3D2-Recursion-Problems/04-Range.js
@@ -11,6 +11,6 @@ function range(start, end) {
     return [start].concat(range(start + 1, end));
 }
 
-range(1, 5); // [1, 2, 3, 4]
-range(3, 4); // [3]
-range(7, 6); // []
+console.log(range(1, 5)); // [1, 2, 3, 4]
+console.log(range(3, 4)); // [3]
+console.log(range(7, 6)); // []
