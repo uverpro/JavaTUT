3529a9b34741d461814117062d2ec88503ddbb27
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-24 19:32:37 -0400

    Update 05-reverse.js

--- App-Academy/W3/W3D2/W3D2-Recursion-Problems/05-reverse.js
+++ App-Academy/W3/W3D2/W3D2-Recursion-Problems/05-reverse.js
@@ -3,11 +3,28 @@ Write a recursive function reverse(string) that takes in a string and returns
 it reversed.
 ***********************************************************************/
 
+const reverse = str => {
+    if (!str.length) {
+        return str; // or "";
+    }
+    let reversed = str.slice(-1) + reverse(str.slice(0, -1));
+    return reversed;
+};
 
+console.log(reverse("house")); // "esuoh"
+// reverse("dog"); // "god"
+// reverse("atom"); // "mota"
+// reverse("q"); // "q"
+// reverse("id"); // "di"
+// reverse(""); // ""
 
-reverse("house"); // "esuoh"
-reverse("dog"); // "god"
-reverse("atom"); // "mota"
-reverse("q"); // "q"
-reverse("id"); // "di"
-reverse(""); // ""
+// Input: String.
+// Output: Reverse string.
+
+// BASE CASE - empty string
+// RECURSIVE CASE - letters exist
+// RECURSIVE STEP - approach base case
+
+// by removing one letter from end at a time
+// string method - slice
+// turn string into array - pop()
