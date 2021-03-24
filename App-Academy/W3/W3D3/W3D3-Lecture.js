51ff89e41806d2df7f3ebefead67a8416de07446
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-24 12:28:08 -0400

    Update W3D3-Lecture.js

--- App-Academy/W3/W3D3/W3D3-Lecture.js
+++ App-Academy/W3/W3D3/W3D3-Lecture.js
@@ -14,3 +14,21 @@ let result = (function () {
 console.log(result);
 
 // Evoking the return value of the function, not the function itself
+
+
+
+let arr = [1];
+console.log(arr.length);
+if (!arr.length) {
+    console.log("Empty")
+} else {
+    console.log("Not empty")
+}
+
+
+
+let obj = { age: 70};
+
+if (!obj.name) {
+    console.log("Add name");
+}
