22c794290f0d1e645aa8c11285caf8c9c7850616
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-24 12:11:32 -0400

    Update ∆Function-Hoisting-Video-Notes.js

--- App-Academy/W3/W3D3/W3D3-Videos/∆Function-Hoisting-Video-Notes.js
+++ App-Academy/W3/W3D3/W3D3-Videos/∆Function-Hoisting-Video-Notes.js
@@ -5,6 +5,7 @@
 // VARIABLE HOISTING REVIEW:
 
 function hoistVar() {
+    // var dog; is hoisted to the top of the scope but hasn't yet been assigned a value
     console.log(dog);
     var dog = "dog";
 }
