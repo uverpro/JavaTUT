728b78e3b0bee7a8aa127b97ed4c4b96d1cd9247
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-24 15:50:51 -0400

    Update addLib.js

--- App-Academy/W3/W3D1/addLib.js
+++ App-Academy/W3/W3D1/addLib.js
@@ -11,5 +11,3 @@ console.log(makeAddLib("swim", "sparkly", "rainbow"));
 // => "I shall swim to the sparkly rainbow?"
 console.log(makeAddLib("RUN", "FANCY", "ParK"));
 // => "I shall RUN to the FANCY ParK?"
-
-// EDIT
