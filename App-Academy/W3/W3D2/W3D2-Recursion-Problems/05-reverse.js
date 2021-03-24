cd869d108bbb62170e3d4a96327c2232280cbc02
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-24 16:09:42 -0400

    Update 05-reverse.js

--- App-Academy/W3/W3D2/W3D2-Recursion-Problems/05-reverse.js
+++ App-Academy/W3/W3D2/W3D2-Recursion-Problems/05-reverse.js
@@ -1,15 +1,6 @@
 /***********************************************************************
 Write a recursive function reverse(string) that takes in a string and returns
 it reversed.
-
-Examples:
-
-reverse("house"); // "esuoh"
-reverse("dog"); // "god"
-reverse("atom"); // "mota"
-reverse("q"); // "q"
-reverse("id"); // "di"
-reverse(""); // ""
 ***********************************************************************/
 
 
