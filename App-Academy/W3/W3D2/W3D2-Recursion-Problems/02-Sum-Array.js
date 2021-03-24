f278def42deabf6d91a27cde480dadda7f874f12
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-24 17:46:43 -0400

    Update 02-Sum-Array.js

--- App-Academy/W3/W3D2/W3D2-Recursion-Problems/02-Sum-Array.js
+++ App-Academy/W3/W3D2/W3D2-Recursion-Problems/02-Sum-Array.js
@@ -5,12 +5,15 @@ include a mix of positive and negative integers!
 ***********************************************************************/
 
 sumArray = (array) => {
+    debugger;
     if (array.length === 0) {
         return 0;
     }
-    return array[0] + sumArray(array.slice(1));
+    let sum = array[0] + sumArray(array.slice(1));
+    debugger;
+    return sum;
 };
 
 console.log(sumArray([1, 2, 3])); //  6
-sumArray([0, 1, -3]); //  -2
-sumArray([1, 2, 3, 4, 5]); // 15
+// sumArray([0, 1, -3]); //  -2
+// sumArray([1, 2, 3, 4, 5]); // 15
