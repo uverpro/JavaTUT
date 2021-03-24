75b1dfd9350c3927cfa60e1b37d4354b2c04b971
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-24 19:45:19 -0400

    Update 11-flatten.js

--- App-Academy/W3/W3D2/W3D2-Recursion-Problems/11-flatten.js
+++ App-Academy/W3/W3D2/W3D2-Recursion-Problems/11-flatten.js
@@ -9,3 +9,19 @@ flatten([]); // []
 flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
+
+function flatten(array) {
+  debugger;
+  let newArray = [];
+  array.forEach(function (el) {
+    if (Array.isArray(el)) {
+      debugger;
+      let flattenEl = flatten(el);
+      newArray.push(...flattenEl);
+    } else {
+      debugger;
+      newArray.push(el);
+    }
+  });
+  return newArray;
+}
