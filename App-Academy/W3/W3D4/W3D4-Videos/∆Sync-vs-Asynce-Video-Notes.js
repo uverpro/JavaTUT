ea214fa3a500d152a21015c1d64624581dcf31ef
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 12:19:13 -0400

    Update ∆Sync-vs-Asynce-Video-Notes.js

--- App-Academy/W3/W3D4/W3D4-Videos/∆Sync-vs-Asynce-Video-Notes.js
+++ App-Academy/W3/W3D4/W3D4-Videos/∆Sync-vs-Asynce-Video-Notes.js
@@ -1,13 +1,13 @@
 // SYNCHRONOUS vs ASYNCHRONOUS CODE
 
-// WHAT DOES IT MEAN TO BE SYNCHRONOUS?
 
+// WHAT DOES IT MEAN TO BE SYNCHRONOUS?
     // In SYNCHRONOUS programming, we must wait for the current command to complete
 // before moving executing another command.
     // There is an inherent order to synchronous code!
 
-// WHAT DOES IT MEAN TO ASYNCHRONOUS?
 
+// WHAT DOES IT MEAN TO ASYNCHRONOUS?
     // In ASYNCHRONOUS programming, we can continue to execute another command,
 // even if the current command has not yet been completed.
     // There may not be a gauranteed order to asynchronous code!
