1ffa80ca04d2b2ed4aa37e4cc8b077a904d78789
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 12:01:32 -0400

    Update ∆Event-Loop-Video-Notes.js

--- App-Academy/W3/W3D4/W3D4-Videos/∆Event-Loop-Video-Notes.js
+++ App-Academy/W3/W3D4/W3D4-Videos/∆Event-Loop-Video-Notes.js
@@ -2,22 +2,27 @@
 
 // How does Javascript handle events?
 // Events includes:
-  // keys being pressed
-  // buttons being cliked
-  // the page being scrolled
-  // timeouts expired
+    // keys being pressed
+    // buttons being cliked
+    // the page being scrolled
+    // timeouts expired
 
 // What is the event loop?
-  // The event loop is the model of execution that Javascript uses
+    // The event loop is the model of execution that Javascript uses
 
 // What structures comprise the event loop?
-  // Call Stack
-  // Message Que
+    // Call Stack
+    // Message Que
 
 // What is the message que?
-  // The message que is the structure used to track the handling of events
+    // The message que is the structure used to track the handling of events
 
 // Que Data Structure
-  // Like the line for checkout
-  // new items are added to the back of the line (enqueue)
-  // items are removed from the front of the line (dequeue)
+    // Like the line for checkout
+    // new items are added to the back of the line (enqueue)
+    // items are removed from the front of the line (dequeue)
+
+// Manipulating the message queue
+    // the items stored on the message queue are called "messages"
+    // when a new event occurs, but the runtime is busy processing another command
+//
