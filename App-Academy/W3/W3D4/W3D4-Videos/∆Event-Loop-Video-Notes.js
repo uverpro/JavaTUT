da37cbd4f0696659155877e33e8b98f740160256
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 11:55:17 -0400

    Update ∆Event-Loop-Video-Notes.js

--- App-Academy/W3/W3D4/W3D4-Videos/∆Event-Loop-Video-Notes.js
+++ App-Academy/W3/W3D4/W3D4-Videos/∆Event-Loop-Video-Notes.js
@@ -1,1 +1,8 @@
 // THE EVENT LOOP
+
+// How does Javascript handle events?
+// Events includes:
+  // keys being pressed
+  // buttons being cliked
+  // the page being scrolled
+  // timeouts expired
