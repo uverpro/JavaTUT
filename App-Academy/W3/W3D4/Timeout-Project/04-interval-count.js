1e3f18abe8ab9cddd1aa7c1ae1790fb4d3a35b31
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 17:17:09 -0400

    Update 04-interval-count.js

--- App-Academy/W3/W3D4/Timeout-Project/04-interval-count.js
+++ App-Academy/W3/W3D4/Timeout-Project/04-interval-count.js
@@ -1,12 +1,12 @@
 /***********************************************************************
-Write a function `intervalCount` that accepts a callback, a delay in 
-milliseconds, and an amount. The function should set an interval with 
+Write a function `intervalCount` that accepts a callback, a delay in
+milliseconds, and an amount. The function should set an interval with
 the given callback and delay, but clear the interval after the callback
 has been executed 'amount' number of times.
 
 Hint: utilize a 'closure' to your advantage
 
-In addition to Mocha, we recommend that you test your code manually using 
+In addition to Mocha, we recommend that you test your code manually using
 node with the examples below.
 
 Example
@@ -18,6 +18,10 @@ intervalCount(function() {
 
 
 
+
+intervalCount(function() {
+  console.log('hi');
+}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = intervalCount;
