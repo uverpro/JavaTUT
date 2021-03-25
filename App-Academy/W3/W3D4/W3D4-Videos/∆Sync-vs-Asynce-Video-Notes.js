9deb4d841607ead8541d39d92c88e24482b617a7
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 12:25:05 -0400

    Update ∆Sync-vs-Asynce-Video-Notes.js

--- App-Academy/W3/W3D4/W3D4-Videos/∆Sync-vs-Asynce-Video-Notes.js
+++ App-Academy/W3/W3D4/W3D4-Videos/∆Sync-vs-Asynce-Video-Notes.js
@@ -11,3 +11,25 @@
     // In ASYNCHRONOUS programming, we can continue to execute another command,
 // even if the current command has not yet been completed.
     // There may not be a gauranteed order to asynchronous code!
+
+
+// Synchronous:
+console.log("one");
+console.log("two");
+console.log("three");
+
+
+// Asynchronous:
+let foo = function() {
+    console.log("two");
+}
+
+console.log("one");
+foo();
+console.log("three");
+// Although this is predictable, the code "jumps around".
+// It is NOT read in a strictly "top down" fashion
+
+// What does it mean to be asynchronous?
+    // In asynchronous programming, we can cotinue to execute another command,
+// even if the current command has not yet been completed
