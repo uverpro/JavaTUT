e705e9c598012a16c86f740228dea51e4c9fdd69
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 19:50:58 -0400

    Update ∆User-Input-Video-Notes.js

--- App-Academy/W3/W3D4/W3D4-Videos/∆User-Input-Video-Notes.js
+++ App-Academy/W3/W3D4/W3D4-Videos/∆User-Input-Video-Notes.js
@@ -6,21 +6,21 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('What is your name?', (answer)=> {
-    console.log(`Hello, ${answer}`)
-    // rl.close();
-    // console.log("after the question") this works
-    rl.question('Where how you from?', (answer)=> {
-            console.log(`${answer} is a beautiful place.`)
+// rl.question('What is your name?', (answer)=> {
+//     console.log(`Hello, ${answer}`)
+//     // rl.close();
+//     // console.log("after the question") this works
+//     rl.question('Where how you from?', (answer)=> {
+//             console.log(`${answer} is a beautiful place.`)
 
-            rl.question("How are you?", thirdAnswer);
+//             rl.question("How are you?", thirdAnswer);
 
-                rl.question('How did you get here?', handleFourthQuestion); // (answer)=> {
-        //         console.log(`${answer}? Well, glad you found me.`)
-        //         rl.close();
-        // });
-    });
-}); // CB HELL
+//                 rl.question('How did you get here?', handleFourthQuestion); // (answer)=> {
+//         //         console.log(`${answer}? Well, glad you found me.`)
+//         //         rl.close();
+//         // });
+//     });
+// }); // CB HELL
 
 // CB CHAIN:
     // console.log("after the question") because rl is asynch, it will print before the answer
@@ -32,12 +32,25 @@ rl.question('What is your name?', (answer)=> {
     // This will not be asked in order b/c it's the same interface
     // You need to include this question in the same callback
 
-const thirdAnswer = (answer)=> {
-    console.log(`That's good, I guess...`)
-    rl.close();
-});
 
-const handleFourthQuestion = (answer)=> {
+const firstQuestion = (answer) => {
+    console.log(`Hello, ${answer}`);
+    rl.question("Where are you from?", secondAnswer);
+}
+
+const secondAnswer = (answer) => {
+    console.log(`${answer} is a beautiful place.`)
+    rl.question("How are you today?", thirdQuestion);
+}
+
+const thirdQuestion = (answer) => {
+    console.log(`${answer}? That's interesting...`)
+    rl.question("How did you get here?", fourthQuestion)
+}
+
+const fourthQuestion = (answer) => {
     console.log(`${answer}? Well, glad you found me.`)
     rl.close();
-};
+}
+
+rl.question("What is your name?", firstQuestion);
