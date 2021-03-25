dddcf9dfc73507f2dd9ec18a23ae9a3347420317
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 19:03:51 -0400

    Update ∆User-Input-Video-Notes.js

--- App-Academy/W3/W3D4/W3D4-Videos/∆User-Input-Video-Notes.js
+++ App-Academy/W3/W3D4/W3D4-Videos/∆User-Input-Video-Notes.js
@@ -8,5 +8,20 @@ const rl = readline.createInterface({
 
 rl.question('What is your name?', (answer)=> {
     console.log(`Hello, ${answer}`)
-    rl.close();
+    // rl.close();
+    // console.log("after the question") this works
+    rl.question('Where how you from?', (answer)=> {
+            console.log(`${answer} is a beautiful place.`)
+            rl.close();
+        });
 });
+
+// CB CHAIN:
+    // console.log("after the question") because rl is asynch, it will print before the answer
+
+// rl.question('Where how you from?', (answer)=> {
+//     console.log(`${answer} is a beautiful place.`)
+//     rl.close();
+// });
+    // This will not be asked in order b/c it's the same interface
+    // You need to include this question in the same callback
