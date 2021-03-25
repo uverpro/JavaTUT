589122f7a5e51e70ec1b0f1f5b4a26c013c8948b
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 18:51:53 -0400

    Update ∆User-Input-Video-Notes.js

--- App-Academy/W3/W3D4/W3D4-Videos/∆User-Input-Video-Notes.js
+++ App-Academy/W3/W3D4/W3D4-Videos/∆User-Input-Video-Notes.js
@@ -1,2 +1,15 @@
 const readline = require("readline");
-console.log(readline)
+// console.log(readline)
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('What is your name?', (answer)=> {
+    console.log(`Hello, ${answer}`)
+});
+
+rl.question("What do you think of Node?", (answer)=> {
+    console.log(`I couldn't agree more that ${answer}`)
+});
