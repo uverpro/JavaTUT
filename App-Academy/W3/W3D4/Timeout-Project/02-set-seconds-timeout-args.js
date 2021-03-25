3e84214934b3bc172efbb00e9928f81c1fbbe224
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 17:41:38 -0400

    Update 02-set-seconds-timeout-args.js

--- App-Academy/W3/W3D4/Timeout-Project/02-set-seconds-timeout-args.js
+++ App-Academy/W3/W3D4/Timeout-Project/02-set-seconds-timeout-args.js
@@ -34,10 +34,3 @@ setSecondsTimeoutArgs(printSum, 0.25, 5, 1, 4); // should print '10' after 250ms
 setSecondsTimeoutArgs(function(arg1, arg2) {
   console.log(arg1 + '-' + arg2);
 }, 0.7, 'hello', 'world'); // should print 'hello-world' after 700ms
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-try {
-  module.exports = setSecondsTimeoutArgs;
-} catch {
-  module.exports = null;
-}
