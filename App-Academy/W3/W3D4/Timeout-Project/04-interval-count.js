56c4113bfd37fcc2ed24ac7c96f955d911c8be1b
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 17:41:06 -0400

    Update 04-interval-count.js

--- App-Academy/W3/W3D4/Timeout-Project/04-interval-count.js
+++ App-Academy/W3/W3D4/Timeout-Project/04-interval-count.js
@@ -8,12 +8,6 @@ Hint: utilize a 'closure' to your advantage
 
 In addition to Mocha, we recommend that you test your code manually using
 node with the examples below.
-
-Example
-
-intervalCount(function() {
-    console.log('hi');
-}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
 ***********************************************************************/
 
 intervalCount = (cb, delay, amount) => {
@@ -30,10 +24,3 @@ intervalCount = (cb, delay, amount) => {
 intervalCount(function() {
   console.log('hi');
 }, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-try {
-  module.exports = intervalCount;
-} catch {
-  module.exports = null;
-}
