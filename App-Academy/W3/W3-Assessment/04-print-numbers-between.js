624a626c2b482a029636a81cb3bdda80d2407e99
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-24 22:48:57 -0400

    Create 04-print-numbers-between.js

---
+++ App-Academy/W3/W3-Assessment/04-print-numbers-between.js
@@ -0,0 +1,60 @@
+/***********************************************************************
+You are given a recursive function, `printNumbersBetween()`, that takes
+a start and end number and prints all the integers between them in order.
+If the first number is smaller, it should print in increasing order. If
+the first number is larger, it should print in decreasing order.
+
+The code is almost working but unfortunately, there are a few bugs. Use
+the debugging skills you've been practicing and the VSCode debugger to
+identify and solve the bugs.
+
+Examples:
+
+printNumbersBetween(5, 10);
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+
+printNumbersBetween(2, -2);
+// 2
+// 1
+// 0
+// -1
+// -2
+
+***********************************************************************/
+
+
+function printNumbersBetween(startNum, endNum) {
+
+    // Print the number
+    console.log(startNum);
+
+    // Base Case: Stop the program when startNum equals endNum
+    if (startNum = endNum) {
+        return;
+    }
+
+    // If the starting number is greater than the ending
+    // number, decrease it by one. Otherwise, increase
+    // it by one.
+    if (startNum < endNum) {
+        startNum - 1;
+    } else {
+        startNum + 1;
+    }
+
+    // Make a recursive call with the modified number.
+    printNumbersBetween(startNum, endNum);
+
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+  module.exports = printNumbersBetween;
+} catch {
+  module.exports = null;
+}
