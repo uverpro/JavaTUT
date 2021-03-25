5716932172f00bb1e53b2f5343c5e015f6afcf58
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 19:14:54 -0400

    Update ∆User-Input-Video-Notes.js

--- App-Academy/W3/W3D4/W3D4-Videos/∆User-Input-Video-Notes.js
+++ App-Academy/W3/W3D4/W3D4-Videos/∆User-Input-Video-Notes.js
@@ -12,9 +12,15 @@ rl.question('What is your name?', (answer)=> {
     // console.log("after the question") this works
     rl.question('Where how you from?', (answer)=> {
             console.log(`${answer} is a beautiful place.`)
-            rl.close();
-        });
-});
+
+            rl.question("How are you?", thirdAnswer);
+
+                rl.question('How did you get here?', handleFourthQuestion); // (answer)=> {
+        //         console.log(`${answer}? Well, glad you found me.`)
+        //         rl.close();
+        // });
+    });
+}); // CB HELL
 
 // CB CHAIN:
     // console.log("after the question") because rl is asynch, it will print before the answer
@@ -25,3 +31,13 @@ rl.question('What is your name?', (answer)=> {
 // });
     // This will not be asked in order b/c it's the same interface
     // You need to include this question in the same callback
+
+const thirdAnswer = (answer)=> {
+    console.log(`That's good, I guess...`)
+    rl.close();
+});
+
+const handleFourthQuestion = (answer)=> {
+    console.log(`${answer}? Well, glad you found me.`)
+    rl.close();
+};
