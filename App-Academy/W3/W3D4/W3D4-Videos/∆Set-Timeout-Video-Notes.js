e7a2b0cdb2002b630fe8b4aba5c42ea699909858
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 13:14:29 -0400

    Update ∆Set-Timeout-Video-Notes.js

--- App-Academy/W3/W3D4/W3D4-Videos/∆Set-Timeout-Video-Notes.js
+++ App-Academy/W3/W3D4/W3D4-Videos/∆Set-Timeout-Video-Notes.js
@@ -12,6 +12,25 @@ function foo() {
 setTimeout(foo, 1000); // even without the '1000' arg, setTimeOut will set default to one millisecond
 console.log("end");
 
-// setTimeout(foo, 1000); // even without the '1000' arg,
+setTimeout(foo, 1000); // even without the '1000' arg,
 // setTimeOut will set default to one millisecond
-// setTimeout can accept infinite amount of arguments
+// setTimeout can accept infinite amount of arguments:
+function foo(a, b) {
+    console.log("time is up")
+    console.log(a + " " + b);
+}
+setTimeout(foo, 1000, 'potato', 'tomato');
+console.log('end');
+// end
+// time is up
+// potato tomato
+
+
+function foo(a, b) {
+    console.log("time is up")
+    console.log(a + " " + b);
+}
+const result = setTimeout(foo, 1000, 'potato', 'tomato');
+console.log(result);
+console.log('end');
+// prints a "Timeout" object!
