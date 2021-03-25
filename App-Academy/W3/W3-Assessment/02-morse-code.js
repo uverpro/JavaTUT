6052b591d303bc9952445be873214c8ae19338f6
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-24 22:54:06 -0400

    Update 02-morse-code.js

--- App-Academy/W3/W3-Assessment/02-morse-code.js
+++ App-Academy/W3/W3-Assessment/02-morse-code.js
@@ -3,20 +3,6 @@ Write a function `morseCode` that takes an array containing a series
 of either 'dot' or 'dash' strings. Your function should `console.log`
 each string in order, followed by a pause of 100ms after each `dot`
 and 300ms after each `dash`.
-
-
-Example:
-
-let code = ['dot', 'dash', 'dot'];
-
-morseCode(code);
-// print 'dot'
-// pause for 100ms
-// print 'dash'
-// pause for 300ms
-// print 'dot'
-// pause for 100ms
-
 ***********************************************************************/
 
 function morseCode(code) {
