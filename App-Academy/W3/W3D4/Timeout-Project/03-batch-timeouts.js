76bd43d0eec7f2172fe9d97186bd1f022be9888f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 17:41:28 -0400

    Update 03-batch-timeouts.js

--- App-Academy/W3/W3D4/Timeout-Project/03-batch-timeouts.js
+++ App-Academy/W3/W3D4/Timeout-Project/03-batch-timeouts.js
@@ -10,22 +10,6 @@ have the same length.
 
 In addition to Mocha, we recommend that you test your code manually using
 node with the examples below.
-
-Example:
-
-const sayHello = () => console.log('hi');
-const sayGoodbye = () => console.log('bye');
-const shout = () => console.log('WHAT?');
-const tasks = [sayHello, sayGoodbye, shout];
-const delays = [500, 200, 900];
-
-const timeoutObjs = batchTimeouts(tasks, delays);
-// should print:
-//  'bye' after 200 ms
-//  'hi' after 500 ms
-//  'WHAT?' after 900 ms
-
-console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 ***********************************************************************/
 
 function batchTimeouts(tasks, delays) {
