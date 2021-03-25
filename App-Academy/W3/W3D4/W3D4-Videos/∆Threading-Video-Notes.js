b1a92ac881c0d2d96b13e605e3c4ce3674c01600
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 11:18:59 -0400

    Update ∆Threading-Video-Notes.js

--- App-Academy/W3/W3D4/W3D4-Videos/∆Threading-Video-Notes.js
+++ App-Academy/W3/W3D4/W3D4-Videos/∆Threading-Video-Notes.js
@@ -1,7 +1,12 @@
 // JAVASCRIPT THREADING
 
-// SINGLE THREADING
-
 // What is a thread?
 // Not exclusive to JS, a computer science term
 // In programming, the term "THREAD OF EXECUTION" refers to a sequence of commands
+
+
+// SINGLE-THREADED vs MULTI-THREADED EXECUTION
+
+// SINGLE-THREADED EXECUTION: only ONE command can be processed at a time
+
+// MULTI-THREADED EXECUTION: multiple commands can be processed at the same time
