65ba1cc320bca26612998a80d35e291ac2786c57
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 12:16:31 -0400

    Update ∆Sync-vs-Asynce-Video-Notes.js

---
+++ App-Academy/W3/W3D4/W3D4-Videos/∆Sync-vs-Asynce-Video-Notes.js
@@ -0,0 +1,5 @@
+// WHAT DOES IT MEAN TO ASYNCHRONOUS?
+
+    // In asynchronous programming, we can continue to execute another command,
+// even if the current command has not yet been completed.
+    // There may not be a gauranteed order to asynchronous code!
