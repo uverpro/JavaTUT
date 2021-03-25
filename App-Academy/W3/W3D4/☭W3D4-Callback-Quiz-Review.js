154713aaa1f473fff0d8ee4f5b92d83bcaed0499
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-25 14:31:43 -0400

    Update ☭W3D4-Callback-Quiz-Review.js

--- App-Academy/W3/W3D4/☭W3D4-Callback-Quiz-Review.js
+++ App-Academy/W3/W3D4/☭W3D4-Callback-Quiz-Review.js
@@ -1,29 +1,29 @@
 let bar = function(s) {
     return s.toLowerCase() + "...";
-  };
+};
 
-  let foo = function(message, cb1, cb2) {
+let foo = function(message, cb1, cb2) {
     console.log(cb1(message));
     console.log(cb2(message));
-  };
+};
 
-  foo("Hey Programmers", bar, function(s) {
+foo("Hey Programmers", bar, function(s) {
     return s.toUpperCase() + "!";
-  });
-  // hey programmers...
-  // HEY PROGRAMMERS!
+});
+// hey programmers...
+// HEY PROGRAMMERS!
 
 
 
-  let bar = function(mystery) {
+let bar = function(mystery) {
     mystery("sneaky");
-  };
+};
 
-  let foo = function(secret) {
+let foo = function(secret) {
     console.log(secret);
-  };
+};
 
-  bar(foo);
+bar(foo);
 
 
 
