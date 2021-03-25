d12caef482d738d33280cca428ae90f0baf30d4d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-24 22:48:31 -0400

    Create 01-dynamic-rotate.js

---
+++ App-Academy/W3/W3-Assessment/01-dynamic-rotate.js
@@ -0,0 +1,36 @@
+/***********************************************************************
+Write a function named `dynamicRotate(num)`. When invoked the
+`dynamicRotate` function will accept a number to be used as the
+rotation amount and return a function. Positive numbers rotate
+the array to the right and negative numbers rotate to the left.
+
+The function returned by `dynamicRotate` will accept an array to
+be rotated by the amount provided when the `dynamicRotate` was
+first invoked. It will return the original array mutated by
+the given rotation.
+
+Examples:
+
+```js
+let arr = ['a', 'b', 'c', 'd', 'e'];
+rotateRightTwo = dynamicRotate(2);
+rotateRightTwo(arr);
+console.log(arr); // [ 'd', 'e', 'a', 'b', 'c' ]
+
+let animals = ['wombat', 'koala', 'opossum', 'kangaroo'];
+rotateLeftOne = dynamicRotate(-1);
+rotateLeftOne(animals)
+console.log(animals); // [ 'koala', 'opossum', 'kangaroo', 'wombat' ]
+```
+***********************************************************************/
+
+function dynamicRotate(num) {
+    // Your code here
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+  module.exports = dynamicRotate;
+} catch {
+  module.exports = null;
+}
