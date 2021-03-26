179ce871b1a36d62dff518989c57266246b0d3c1
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-26 11:18:38 -0400

    Update Length-of-a-String.js

--- Edabit/Recursion/Very-Easy/Length-of-a-String.js
+++ Edabit/Recursion/Very-Easy/Length-of-a-String.js
@@ -1,10 +1,11 @@
 // Recursion: Length of a String
 // Write a function that returns the length of a string. Make your function recursive.
 
-length("apple") // 5
 
-length("make") // 4
+console.log(length("apple")); // 5
 
-length("a") // 1
+console.log(length("make")) // 4
 
-length("") // 0
+console.log(length("a")) // 1
+
+console.log(length("")) // 0
