604489ec62a8c03993831014963981886d007699
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-27 03:03:58 -0400

    Create Grouping-Operator.js

---
+++ Javascript/Operators & Expressions/Grouping-Operator.js
@@ -0,0 +1,13 @@
+// The grouping operator ( ) controls the precedence of evaluation in expressions.
+
+console.log(1 + 2 * 3); // 1 + 6
+// expected output: 7
+
+console.log(1 + (2 * 3)); // 1 + 6
+// expected output: 7
+
+console.log((1 + 2) * 3); // 3 * 3
+// expected output: 9
+
+console.log(1 * 3 + 2 * 3); // 3 + 6
+// expected output: 9
