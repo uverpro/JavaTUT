0e4dde8dd43e02970efd528dd475e3da9884444e
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-27 03:01:55 -0400

    Create Object.values.js

---
+++ Javascript/Methods/Object Methods/Object.values.js
@@ -0,0 +1,16 @@
+// Object.values()
+
+/*
+The Object.values() method returns an array of a given object's own enumerable property values,
+in the same order as that provided by a for...in loop.
+(The only difference is that a for...in loop enumerates properties in the prototype chain as well.)
+*/
+
+const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+
+  console.log(Object.values(object1));
+  // expected output: Array ["somestring", 42, false]
