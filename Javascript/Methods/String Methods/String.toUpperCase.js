def18f75afba4e311eb3940693609c413acd8c34
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-27 03:00:02 -0400

    Create String.toUpperCase.js

---
+++ Javascript/Methods/String Methods/String.toUpperCase.js
@@ -0,0 +1,10 @@
+// String.prototype.toUpperCase()
+
+// The toUpperCase() method returns the calling string value converted to uppercase
+// (the value will be converted to a string if it isn't one).
+
+
+const sentence = 'the giant walked over the hill hooray';
+
+console.log(sentence.toUpperCase());
+// expected output: "THE GIANT WALKED OVER THE HILL HOORAY"
