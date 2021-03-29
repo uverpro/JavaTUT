d224a1c6cba94ada58a9f36ad5971d755e215e45
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-29 18:15:27 -0400

    Update ∆Request-Response-Video-Notes.js

--- App-Academy/W4/W4D1/W4D1-Videos/∆Request-Response-Video-Notes.js
+++ App-Academy/W4/W4D1/W4D1-Videos/∆Request-Response-Video-Notes.js
@@ -9,4 +9,15 @@
     Client                      Server
     Side                        Side
 
+When someone opens a browser and requests a URL, the request is sent to the server
+    which processes the request and sends it back.
+
+On the the browser side, it parcels all the HTML and CSS and JS sent back
+    by the server. The internet is made up of these requests and responses.
+
+    The browser side doesn't see what's under the hood but occasionally will
+    receive error messages. Developers need to understand these messages in order
+    to trouble shoot problems.
+    We can view these network requests and responses by using
+    DEVELOPERS TOOLS. ===> NETWORK TAB
 */
