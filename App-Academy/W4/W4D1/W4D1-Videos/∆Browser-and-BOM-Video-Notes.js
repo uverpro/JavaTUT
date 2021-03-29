d9b3e44c6ef9ed67ae18a3e29df5e1c77d91ff4c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-29 17:11:52 -0400

    Update ∆Browser-and-BOM-Video-Notes.js

---
+++ App-Academy/W4/W4D1/W4D1-Videos/∆Browser-and-BOM-Video-Notes.js
@@ -0,0 +1,11 @@
+/*
+The DOM (Document Object Model)
+    A heirarchy of object in the DOCUMENT OBJECT,
+    which includes HTML elements.
+
+The BOM (Browser Object Model)
+    A heirarchy of objects in the BROWSER OBJECT,
+    which includes the document object.
+
+The DOM is contained within the BOM.
+*/
