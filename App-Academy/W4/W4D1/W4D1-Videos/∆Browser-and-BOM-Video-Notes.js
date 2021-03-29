55e2eca470a2ecefe5ccefc12c56a4e4aa116b9c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-29 17:14:03 -0400

    Update ∆Browser-and-BOM-Video-Notes.js

--- App-Academy/W4/W4D1/W4D1-Videos/∆Browser-and-BOM-Video-Notes.js
+++ App-Academy/W4/W4D1/W4D1-Videos/∆Browser-and-BOM-Video-Notes.js
@@ -8,4 +8,8 @@ The BOM (Browser Object Model)
     which includes the document object.
 
 The DOM is contained within the BOM.
+
+
+USER INTERFACE: The browser interface, which includes the address bar, back and forward buttons,
+    bookmars menu, etc. Includes everything except for the requested page content.
 */
