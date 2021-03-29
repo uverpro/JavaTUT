e49063d283a841f6343a9cfc550a9f5114d5df55
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-29 12:14:50 -0400

    Update 02-morse-code.js

--- App-Academy/W3/W3-Assessment/02-morse-code.js
+++ App-Academy/W3/W3-Assessment/02-morse-code.js
@@ -6,17 +6,21 @@ and 300ms after each `dash`.
 ***********************************************************************/
 
 function morseCode(code) {
+    if (code.length === 0) {
+      return;
+    }
+    let ele = code[code.length -1];
+    if (ele === "dot") {
+      setTimeout(() => {
+        console.log("dot");
+      }, 100);
+    } else {
+        setTimeout(() => {
+          console.log("dash");
+      }, 300);
+    }
+    return morseCode(code.slice(0, -1));
+  }
+  let code = ["dot", "dash", "dot"];
 
-    // Your code here
-
-}
-
-let code = ['dot', 'dash', 'dot'];
-
-morseCode(code);
-// print 'dot'
-// pause for 100ms
-// print 'dash'
-// pause for 300ms
-// print 'dot'
-// pause for 100ms
+  morseCode(code);
