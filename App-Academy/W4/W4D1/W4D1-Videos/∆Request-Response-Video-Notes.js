590a6eaa30e6fdb007d9daa0237ca88e9c186dbb
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-29 17:44:40 -0400

    Update ∆Request-Response-Video-Notes.js

--- App-Academy/W4/W4D1/W4D1-Videos/∆Request-Response-Video-Notes.js
+++ App-Academy/W4/W4D1/W4D1-Videos/∆Request-Response-Video-Notes.js
@@ -2,4 +2,11 @@
 
     Is what happens when someone makes a network request in a browser.
 
+                Request
+    PC          =============>  Data Center
+    Browser     <============   Server/Data
+                Response
+    Client                      Server
+    Side                        Side
+
 */
