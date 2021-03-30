25559d31e581f0f2173dd7718f9de0bae6d9b4cd
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-30 02:26:35 -0400

    Create Two-Dimensional-Project.js

---
+++ App-Academy/W1/W1D4/W1D4-Problems/Two-Dimensional-Project.js
@@ -0,0 +1,15 @@
+
+
+
+let arr1 = [
+    [6, 4],
+    [5],
+    [3, 1]
+];
+console.log(twoDimensionalProduct(arr1)); // 360
+
+let arr2 = [
+    [11, 4],
+    [2]
+];
+console.log(twoDimensionalProduct(arr2)); // 88
