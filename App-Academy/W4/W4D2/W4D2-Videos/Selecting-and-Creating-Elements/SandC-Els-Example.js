09e594ebc5ba9dc74517f7360848f22f4a59464f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-30 12:04:51 -0400

    Update SandC-Els-Example.js

--- App-Academy/W4/W4D2/W4D2-Videos/Selecting-and-Creating-Elements/SandC-Els-Example.js
+++ App-Academy/W4/W4D2/W4D2-Videos/Selecting-and-Creating-Elements/SandC-Els-Example.js
@@ -1,3 +1,9 @@
+// const printDiv = () => {
+//     const divOfInterest = document.getElementById('catch-me-if-you-can');
+//     console.log(divOfInterest);
+// };
+// window.onload = printDiv;
+
 const printDiv = () => {
     const divOfInterest = document.getElementById('catch-me-if-you-can');
     console.log(divOfInterest);
