d9a6d647b70dcd81bcf953cf19f4c23700000b2f
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-30 02:24:59 -0400

    Update Spiral-Matrix.js

--- App-Academy/W1/W1D4/W1D4-Problems/Spiral-Matrix.js
+++ App-Academy/W1/W1D4/W1D4-Problems/Spiral-Matrix.js
@@ -47,3 +47,16 @@ function spiralOrder(mat) {
     }
     return arr;
   }
+
+  matrix = [[ 1, 2, 3],
+          [ 4, 5, 6],
+          [ 7, 8, 9]]
+
+console.log(spiralOrder(matrix)); // [1,2,3,6,9,8,7,4,5]
+
+matrix = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9,10,11,12]]
+
+
+console.log(spiralOrder(matrix)); // [1,2,3,4,8,12,11,10,9,5,6,7]
