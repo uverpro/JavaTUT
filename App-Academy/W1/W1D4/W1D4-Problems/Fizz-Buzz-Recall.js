b3d99923f5494559c560aa710f0617b557e49efa
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-30 03:27:12 -0400

    Update Fizz-Buzz-Recall.js

--- App-Academy/W1/W1D4/W1D4-Problems/Fizz-Buzz-Recall.js
+++ App-Academy/W1/W1D4/W1D4-Problems/Fizz-Buzz-Recall.js
@@ -2,3 +2,9 @@
 // Write a function fizzBuzz(max) that accepts a number as an arg.
 // The function should return an array containing all positive numbers
 // less than max that are divisible by either 3 or 5, but not both.
+
+
+
+
+console.log(fizzBuzz(12)); // [ 3, 5, 6, 9, 10 ]
+console.log(fizzBuzz(20)); // [ 3, 5, 6, 9, 10, 12, 18 ]
