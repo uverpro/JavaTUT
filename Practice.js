8af1c2689985471af6f6db6457946f0f744230e7
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-30 11:11:08 -0400

    Update Practice.js

---
+++ Practice.js
@@ -0,0 +1,20 @@
+// 1.
+let adder = (num1, num2) => {
+  let add = num1 + num2;
+  return add;
+};
+
+// 2.
+let addFive = num => {
+  let add = num + 5;
+  return add;
+};
+
+// 3.
+let addFive2 = num => num + 5;
+
+// 4.
+const addFive3 = num => {
+  const add = num + 5;
+  add;
+};
