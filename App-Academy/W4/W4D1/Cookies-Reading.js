b8f44f1a1c11621b8d69b89303680b2168d5c7c8
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-31 16:42:27 -0400

    Create Cookies-Reading.js

---
+++ App-Academy/W4/W4D1/Cookies-Reading.js
@@ -0,0 +1,9 @@
+// Cookies
+
+// Cookies are used to store info about a user ie,
+    // browser habits or history, form input info
+
+// Session Cookies are lost once the browser window is closed
+
+// Persistent Cookies are set up with a specified expiration date
+    // such as an e-commerce site tracking a users browsing and buying habits
