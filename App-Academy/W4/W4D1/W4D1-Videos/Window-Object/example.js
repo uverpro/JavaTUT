b0deb2ba272ae7880d8a196ea068762559804fb5
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-31 02:43:53 -0400

    Delete example.js

--- App-Academy/W4/W4D1/W4D1-Videos/Window-Object/example.js
+++
@@ -1,6 +0,0 @@
-// Open a new window
-const newWindow = window.open("https://www.wikipedia.org", "Wikipedia", "width=100, height=100");
-// Resize the new window
-newWindow.resizeTo(500, 500);
-
-//
