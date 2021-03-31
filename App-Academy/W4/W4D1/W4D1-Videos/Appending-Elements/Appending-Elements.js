bea985c971cb1166dc61e281d07b94dda8154373
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-31 16:02:43 -0400

    Update Appending-Elements.js

--- App-Academy/W4/W4D1/W4D1-Videos/Appending-Elements/Appending-Elements.js
+++ App-Academy/W4/W4D1/W4D1-Videos/Appending-Elements/Appending-Elements.js
@@ -1,4 +1,7 @@
 // create addElement function
 const addElements = () => {
     // create new h1 element
+    const newElement = document.createElement('h1');
+    // set new element's id to 'sleeping-giant'
+    newElement.setAttribute('id', 'sleeping-giant');
 }
