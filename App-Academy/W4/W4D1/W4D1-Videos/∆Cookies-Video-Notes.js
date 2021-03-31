378d2297a2fcd01e870a79643ffa65e78ec97594
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-31 16:25:29 -0400

    Update ∆Cookies-Video-Notes.js

--- App-Academy/W4/W4D1/W4D1-Videos/∆Cookies-Video-Notes.js
+++ App-Academy/W4/W4D1/W4D1-Videos/∆Cookies-Video-Notes.js
@@ -1,3 +1,8 @@
 // COOKIES
 
 // Cookies are a way for us to store data in the form of key-value pairs in the browser
+
+// Cookies often contain about a user session or their buying habits
+    // basically anything we wouldn't want to store in a database but
+    // would be helpful to use on the front-end.
+    // For example: in a shopping cart
