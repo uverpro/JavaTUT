56ab5bec349f0d69c47aa603c4d4bb1624cdb142
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-31 02:44:00 -0400

    Create Window-Object-example.js

---
+++ App-Academy/W4/W4D1/W4D1-Videos/Window-Object/Window-Object-example.js
@@ -0,0 +1,6 @@
+// Open a new window
+const newWindow = window.open("https://www.wikipedia.org", "Wikipedia", "width=100, height=100");
+// Resize the new window
+newWindow.resizeTo(500, 500);
+
+//
