1d849f6ffcbd0db82e5c725c867cf767b74fc8ac
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-31 16:36:32 -0400

    Create W4-Learning-Objectives.js

---
+++ App-Academy/W4/W4-Learning-Objectives.js
@@ -0,0 +1,10 @@
+// D1 - Browser Basics Lesson Learning Objectives
+
+// 1. Explain the difference between the BOM (browser object model)
+    // and the DOM(document object model).
+Given a diagram of all the different parts of the browser, identify each part.
+Use the Window API to change the innerHeight of a user's window.
+Identify the context of anonymous functions running in the browser (the window).
+Given a JS file and an HTML file, use a script tag to import the JS file and execute the code therein when all the elements on the page load.
+Explain the browser's main role in the request/response cycle.
+Identify, examine, and delete cookies for a given website using the Chrome Developer Tools.
