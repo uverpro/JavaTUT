57baaf0f94bcc1379f0f86a5eb5fa4c5ed770562
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-31 02:26:39 -0400

    Update Practice-HTML.js

--- ΩPractice-HTML/Practice-HTML.js
+++ ΩPractice-HTML/Practice-HTML.js
@@ -6,24 +6,48 @@ window.addEventListener('DOMContentLoaded', (event) => {
 	const name = document.createElement('h1');
 	const text = document.createTextNode('Joe');
 
-	name.setAttribute('id', 'name');
+	// name.setAttribute('id', 'name');
 	name.appendChild(text);
 	body.appendChild(name);
 
 	const list = document.createElement('ul');
+
 	const listItem1 = document.createElement('li');
-	const listItem1Content = document.createTextNode(
-		'I like to drink iced lattes.'
-	);
-	listItem1.appendChild(listItem1Content);
 
-	// what we're creating
 	list.appendChild(listItem1);
-
-	// show what we're creating
 	body.appendChild(list);
-});
 
-// let p = document.createElement('p');
-// p.innerHTML = 'Hello World!';
-// document.appendChild(p);
+	// list of elements
+	const date = new Date();
+	const hours = date.getHours();
+	const minutes = date.getMinutes();
+
+	const detailsArr = [
+		'<li>I like to drink iced lattes.</li>',
+		'<li>I have two cats and eight kittens.</li>',
+		'<li>My favorite place to get lunch is Chipotle.</li>',
+		'<li>On the weekends, I play flag football.</li>',
+		`<li>I live in SF, CA, and it's currently ${hours}:${minutes}</li>`,
+	];
+
+	// formatting elements in array into string
+	const liString = detailsArr.join(' ');
+	console.log(liString);
+
+	const listEle = document.querySelector('ul');
+
+	// inserting to HTML of list
+	listEle.innerHTML = liString;
+	body.appendChild(listEle);
+
+	// set classes to style in CSS
+	list.setAttribute('class', 'my-details');
+
+	// looping through all list elements, give the class
+	const liEle = document.querySelectorAll('li');
+	liEle.forEach((ele) => {
+		ele.className = 'details';
+	});
+
+	name.setAttribute('class', 'my-name');
+});
