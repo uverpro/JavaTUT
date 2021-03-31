d3b8f336c65d0b2219756f76ee9695ca1d54d3d4
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-31 16:44:13 -0400

    Update Cookies-Reading.js

--- App-Academy/W4/W4D1/Cookies-Reading.js
+++ App-Academy/W4/W4D1/Cookies-Reading.js
@@ -7,3 +7,14 @@
 
 // Persistent Cookies are set up with a specified expiration date
     // such as an e-commerce site tracking a users browsing and buying habits
+
+
+// How to create a cookie in JS:
+    // document interface represents the web page loaded in a user's browser
+    // cookies are stored on a user's browser, so document object allows
+    // to get/set cookies on users broswer:
+const firstCookie = "favoriteCat=million";
+document.cookie = firstCookie;
+const secondCookie = "favoriteDog=bambi";
+document.cookie = secondCookie;
+document.cookie; // Returns "favoriteCat=million; favoriteDog=bambi"
