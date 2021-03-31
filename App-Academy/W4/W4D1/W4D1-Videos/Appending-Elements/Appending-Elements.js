7e8bf0b46c01d3cce4f883722d18342842b46797
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-31 16:08:24 -0400

    Update Appending-Elements.js

--- App-Academy/W4/W4D1/W4D1-Videos/Appending-Elements/Appending-Elements.js
+++ App-Academy/W4/W4D1/W4D1-Videos/Appending-Elements/Appending-Elements.js
@@ -4,4 +4,13 @@ const addElements = () => {
     const newElement = document.createElement('h1');
     // set new element's id to 'sleeping-giant'
     newElement.setAttribute('id', 'sleeping-giant');
+    // create text node 'jell-o, burled!'
+    const newContent = document.createTextNode("Jell-0, Burled!");
+    // append content to element
+    newElement.appendChild(newContent);
+    // append element to document body
+    document.body.appendChild(newElement);
+    // append a seceond element, a div, to the DOM
+
+    // set the div's id to "lickable frog"
 }
