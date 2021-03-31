4449043947b90afa3b0fb8c1a201749a04b701b6
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-03-31 03:38:04 -0400

    Update Window-Object-example.js

--- App-Academy/W4/W4D1/W4D1-Videos/Window-Object/Window-Object-example.js
+++ App-Academy/W4/W4D1/W4D1-Videos/Window-Object/Window-Object-example.js
@@ -3,4 +3,14 @@ const newWindow = window.open("https://www.wikipedia.org", "Wikipedia", "width=1
 // Resize the new window
 newWindow.resizeTo(500, 500);
 
-//
+// (function() {
+//  console.log(this)
+// })
+
+// window.onload = () => {
+    // console.log("This script loaded when all the resources and the DOM were ready.");
+// };
+
+// window.addEventListener('DOMContentLoaded', () => {
+    // console.log("This script loaded when the DOM was ready.");
+// });
