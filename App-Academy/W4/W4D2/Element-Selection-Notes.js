20d78f335b47031ed68a018c2651f6ef702da2c1
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-01 00:47:18 -0400

    Update Element-Selection-Notes.js

--- App-Academy/W4/W4D2/Element-Selection-Notes.js
+++ App-Academy/W4/W4D2/Element-Selection-Notes.js
@@ -4,7 +4,7 @@
     // Allows developers to access the document via programming languages, like Javascript
 
 
-// Referencing the DOM
+// Referencing the DOM -
     // The DOM API is a tool used to reference, create, and update DOM elements
     // How to reference a DOM element in JS:
         // Assume the HTML file includes the following div:
@@ -30,3 +30,40 @@
 // .getElementById() references single element
 // .querySelectorAll("span.cloudy") object.property? refs all elements w/ class name "cloudy"
     // A static NodeList. Can iterate over NodeList with .forEach()
+    // Using .forEach() on a NodeList (JS):
+    const cloudySpans = document.querySelectorAll("span.cloudy");
+
+    cloudySpans.forEach(span => {
+      console.log("Cloudy!");
+    });
+
+
+// Creating New DOM Elements -
+    // Reference the JS file in the HEAD of the HTML file:
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <script type="text/javascript" src="example.js"></script>
+    </head>
+    <body></body>
+</html>
+// Create a new h1 element, assign an id, give content, and arrach to the body of HTML doc:
+const addElement = () => {
+    // create a new div element
+    const newElement = document.createElement("h1");
+
+    // set the h1's id
+    newElement.setAttribute("id", "sleeping-giant");
+
+    // and give it some content
+    const newContent = document.createTextNode("Jell-O, Burled!");
+
+    // add the text node to the newly created div
+    newElement.appendChild(newContent);
+
+    // add the newly created element and its content into the DOM
+    document.body.appendChild(newElement);
+  };
+  // run script when page is loaded
+  window.onload = addElement;
