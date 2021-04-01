16ac35534ef8f34c2df9905f29c6c6957164ad0b
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-01 01:04:24 -0400

    Update 2-Element-Selection-Notes.js

---
+++ App-Academy/W4/W4D2/2-Element-Selection-Notes.js
@@ -0,0 +1,5 @@
+// Inserting Element in JS File and Script Block
+
+// Create a second element, div with an id "lickable-frog"
+// append it to the BODY
+// Update the JS function to append a second element to the page
