2d089cf8d6223ff9b26a07a5a9276cc78bf11184
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-01 00:02:34 -0400

    Update Element-Selection-Notes.js

---
+++ App-Academy/W4/W4D2/Element-Selection-Notes.js
@@ -0,0 +1,12 @@
+// What is DOM?
+    // The Document Object Model, DOM, an object-oriented representation of HTML doc/web page
+    // represented by objects or nodes
+    // Allows developers to access the document via programming languages, like Javascript
+
+
+// Referencing the DOM
+    // The DOM API is a tool used to reference, create, and update DOM elements
+    // How to reference a DOM element in JS:
+        // Assume the HTML file includes the following div:
+        <div id="”catch-me-if-you-can”">HI!</div> // HTML
+        // Since this element is in the HTML, it is avaliable in the DOM to reference/manipulate
