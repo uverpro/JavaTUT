648cce1662a9b8b062bd6cc496a39aa42f3f0633
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-02 04:30:37 -0400

    Update HTML.js

--- App-Academy/W4/W4D1/W4D1-HTML/HTML.js
+++ App-Academy/W4/W4D1/W4D1-HTML/HTML.js
@@ -118,3 +118,18 @@
 
 
 // Anatomy of an HTML document
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>My test page</title>
+  </head>
+  <body>
+    <p>This is my page</p>
+  </body>
+</html>
+</html>
+
+// 1. !DOCTYPE html is the shortest string of chars for a valid doctype. Use it.
+// 2. <html></html>:
