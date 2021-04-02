fa3a9db35880775cf43206f9a66364123e41c61a
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-02 15:48:05 -0400

    Update JSON.js

--- Javascript/Built-in-Objects/JSON/JSON.js
+++ Javascript/Built-in-Objects/JSON/JSON.js
@@ -1,6 +1,16 @@
 // JSON
 
 // The JSON object contains methods for parsing JavaScript Object Notation (JSON)
-// and converting values to JSON.
-// It can't be called or constructed, and aside from its two method properties,
-// it has no interesting functionality of its own.
+    // and converting values to JSON.
+    // It can't be called or constructed, and aside from its two method properties,
+    // it has no interesting functionality of its own.
+
+
+// JavaScript and JSON differences
+// JSON is a syntax for serializing objects, arrays, numbers, strings, booleans, and null.
+    // It is based upon JavaScript syntax but is distinct from it: some JavaScript is not JSON.
+
+Objects and Arrays
+Property names must be double-quoted strings; trailing commas are forbidden.
+Numbers
+Leading zeros are prohibited. A decimal point must be followed by at least one digit. NaN and Infinity are unsupported.
