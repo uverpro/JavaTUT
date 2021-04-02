c9ded9658e5140ded94d1747612f264fda8b7fda
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-02 00:52:26 -0400

    Update HTML.js

--- App-Academy/W4/W4D1/W4D1-HTML/HTML.js
+++ App-Academy/W4/W4D1/W4D1-HTML/HTML.js
@@ -1,3 +1,9 @@
 // HTML
 
 // HTML is a MARKUP LANGUAGE, not a programming language.
+// HTML consists of a series of elements, which are enclosed, wrapped, or marked up
+// The enclosing tags make content into a hyperlink to connect another page,
+    // italicize words, etc. Example:
+        My cat is very grumpy
+        // to make the text stand alone, enclose with paragraph <p> element:
+            <p>My cat is very grumpy</p>
