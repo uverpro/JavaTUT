54540cfb118936e8be5df585befa365d66e7683c
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-02 15:45:45 -0400

    Update JSON.js

---
+++ Javascript/Built-in-Objects/JSON/JSON.js
@@ -0,0 +1,6 @@
+// JSON
+
+// The JSON object contains methods for parsing JavaScript Object Notation (JSON)
+// and converting values to JSON.
+// It can't be called or constructed, and aside from its two method properties,
+// it has no interesting functionality of its own.
