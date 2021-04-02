dc750242f034c1c660c312ca5b41d1fd4c9c2b1b
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-02 01:12:26 -0400

    Update HTML.js

--- App-Academy/W4/W4D1/W4D1-HTML/HTML.js
+++ App-Academy/W4/W4D1/W4D1-HTML/HTML.js
@@ -20,10 +20,6 @@
 // The opening tag: This consists of the name of the element (in this example, p for paragraph),
     // wrapped in opening and closing angle brackets.
     // This opening tag marks where the element begins or starts to take effect.
-    // In this example, it precedes the start of the paragraph text.
 // The content: This is the content of the element. In this example, it is the paragraph text.
 // The closing tag: This is the same as the opening tag, except that it includes
-    // a forward slash before the element name.
-    // This marks where the element ends.
-    // Failing to include a closing tag is a common beginner error that
-    // can produce peculiar results.
+    // a forward slash before the element name. This marks where the element ends.
