1d4f666352af4eb9b948ec9d70eb85311d8b9f4b
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-02 01:23:29 -0400

    Update HTML.js

--- App-Academy/W4/W4D1/W4D1-HTML/HTML.js
+++ App-Academy/W4/W4D1/W4D1-HTML/HTML.js
@@ -28,3 +28,18 @@
 {/* <p>My cat is <strong>very</strong> grumpy.</p> */}
 
 // Block verses Inline Elements
+
+// Block-level elements form a visible block on a page.
+    // A block-level element appears on a new line following the content that precedes it.
+    // Any content that follows a block-level element also appears on a new line.
+    // Block-level elements are usually structural elements on the page.
+    // For example, a block-level element might represent headings, paragraphs, lists,
+    // navigation menus, or footers.
+    // A block-level element wouldn't be nested inside an inline element,
+    // but it might be nested inside another block-level element.
+
+// Inline elements are contained within block-level elements, and surround only small parts
+    // of the document’s content (not entire paragraphs or groupings of content).
+    // An inline element will not cause a new line to appear in the document.
+    // It is typically used with text, for example an <a> element creates a hyperlink,
+    // and elements such as <em> or <strong> create emphasis.
