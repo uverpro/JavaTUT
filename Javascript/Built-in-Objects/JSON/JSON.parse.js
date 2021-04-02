10f23993a0b989461f7b26c4b62529bdc220e533
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-02 15:54:48 -0400

    Create JSON.parse.js

---
+++ Javascript/Built-in-Objects/JSON/JSON.parse.js
@@ -0,0 +1,5 @@
+// JSON.parse()
+    // The JSON.parse() method parses a JSON string, constructing the JavaScript value
+    // or object described by the string.
+    // An optional reviver function can be provided to perform a transformation
+    // on the resulting object before it is returned.
