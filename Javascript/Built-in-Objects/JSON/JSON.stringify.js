4cbdb1c02e6e17171be4bb4c0d339198ede0df8d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-04 16:24:09 -0400

    Create JSON.stringify.js

---
+++ Javascript/Built-in-Objects/JSON/JSON.stringify.js
@@ -0,0 +1,4 @@
+// JSON.stringify()
+// The JSON.stringify() method converts a JavaScript object or value to a JSON string,
+    // optionally replacing values if a replacer function is specified
+    // or optionally including only the specified properties if a replacer array is specified.
