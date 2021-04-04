2bf69f933dc74d966ca72272634d4fe0c9334bd9
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-04 16:58:16 -0400

    Update JSON.parse.js

--- Javascript/Built-in-Objects/JSON/JSON.parse.js
+++ Javascript/Built-in-Objects/JSON/JSON.parse.js
@@ -3,3 +3,12 @@
     // or object described by the string.
     // An optional reviver function can be provided to perform a transformation
     // on the resulting object before it is returned.
+
+const json = '{"result":true, "count":42}';
+const obj = JSON.parse(json);
+
+console.log(obj.count);
+// expected output: 42
+
+console.log(obj.result);
+// expected output: true
