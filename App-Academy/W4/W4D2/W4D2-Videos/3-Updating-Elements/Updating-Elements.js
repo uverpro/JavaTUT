772015e274e4a48aab71eded8bf99693649e5b2d
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-05 04:56:11 -0400

    Update Updating-Elements.js

--- App-Academy/W4/W4D2/W4D2-Videos/3-Updating-Elements/Updating-Elements.js
+++ App-Academy/W4/W4D2/W4D2-Videos/3-Updating-Elements/Updating-Elements.js
@@ -4,4 +4,9 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     // get id of parent element
     const parent = document.getElementById('your-best-friend');
+    // store reference to child nodes
+    const childNodes = parent.childNodes;
+    // loop through node values and console.log each value
+
 });
+// END
