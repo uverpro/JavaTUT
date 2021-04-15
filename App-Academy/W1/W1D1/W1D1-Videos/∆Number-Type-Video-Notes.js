31aa54667acbc5a26382d64c1f3d7809a5aec288
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-15 00:39:59 -0400

    Update ∆Number-Type-Video-Notes.js

---
+++ App-Academy/W1/W1D1/W1D1-Videos/∆Number-Type-Video-Notes.js
@@ -0,0 +1,19 @@
+// Number Data Types
+
+console.log(1 + 1); // 2
+console.log(2.5 - 2); // .5
+console.log(3 * 4); // 12
+console.log(99 / 9); // 11
+
+// Syntax:
+// "+" = addition
+// "-" = subtraction
+// "*" = multiplication
+// "/" = division
+
+console.log(3 + 2 * 4); // 11
+// REMEMBER: Order of operation. Multiplication takes precedent over addition
+console.log((3 + 2) * 4) // 20
+
+
+// Modulo Operation
