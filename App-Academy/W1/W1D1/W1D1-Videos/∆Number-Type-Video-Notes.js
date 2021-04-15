e04fdebc13fc8b4818dd621330db6f79cd423422
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-15 00:41:58 -0400

    Update ∆Number-Type-Video-Notes.js

--- App-Academy/W1/W1D1/W1D1-Videos/∆Number-Type-Video-Notes.js
+++ App-Academy/W1/W1D1/W1D1-Videos/∆Number-Type-Video-Notes.js
@@ -19,4 +19,10 @@ console.log((3 + 2) * 4); // 20
 
 // Modulo Operation (%)
 
-console.log(22 % 5); //
+console.log(20 % 5); // 0
+console.log(21 % 5); // 1
+console.log(22 % 5); // 2
+console.log(23 % 5); // 3
+console.log(24 % 5); // 4
+console.log(25 % 5); // 0
+console.log(26 % 5); // 1
