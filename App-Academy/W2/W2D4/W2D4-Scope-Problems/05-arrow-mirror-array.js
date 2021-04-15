b71ca3704905d55b08f45f621eaf4188869d5505
Cory Pedigo <74264029+uverpro@users.noreply.github.com>
2021-04-15 17:56:25 -0400

    Update 05-arrow-mirror-array.js

--- App-Academy/W2/W2D4/W2D4-Scope-Problems/05-arrow-mirror-array.js
+++ App-Academy/W2/W2D4/W2D4-Scope-Problems/05-arrow-mirror-array.js
@@ -11,10 +11,31 @@ arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b
 ***********************************************************************/
 
 arrowMirrorArray = (array) => {
-  
+  let newArray = array;
+  for (let i = array.length - 1; i >= 0; i--) {
+    let el = array[i];
+    newArray.push(el);
+  }
+  return newArray;
 };
 
 
 console.log(arrowMirrorArray([1,2,3])); // => [ 1, 2, 3, 3, 2, 1 ]
 console.log(arrowMirrorArray(['a', 'b', 'c', 'd']));
 // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
+
+const mirrorArray = (array) => {
+  let newArray = [];
+
+  for (let i = 0; i < array.length; i += 1) {
+    let el = array[i];
+    newArray.push(el);
+  }
+
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    let el = array[i];
+    newArray.push(el);
+  }
+
+  return newArray;
+};
